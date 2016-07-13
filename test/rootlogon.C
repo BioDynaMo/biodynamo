93832e8382f828948945a8ee678931749561e673
Fons Rademakers <Fons.Rademakers@cern.ch>
2016-07-13 11:41:19 +0200

    Use proper interpreter API.

--- test/rootlogon.C
+++ test/rootlogon.C
@@ -1,7 +1,7 @@
 {
-   gROOT->ProcessLine(".I ../include");
-   gROOT->ProcessLine(".I .");
-   gROOT->ProcessLine(".I ../build/gtest/src/gtest/include");
+   gInterpreter->AddIncludePath("../include");
+   gInterpreter->AddIncludePath(".");
+   gInterpreter->AddIncludePath("../build/gtest/src/gtest/include");
    gSystem->Load("../build/libgmp.10.dylib");
    gSystem->Load("../build/libbiodynamo.dylib");
    printf("\nWelcome to the biodynamo test directory...\n");
