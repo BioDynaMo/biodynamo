6c3fc2188b259eaad4a46c7666b092c29f6373c7
Lukas Johannes Breitwieser <lukas.johannes.breitwieser@cern.ch>
2016-08-03 13:56:13 +0200

    Improve runCppLint.sh
     Fix script for staged files
     Enhance to check files that have changed compared to master
    
     Resolves: #13, $14

--- housekeeping/cpplint/runCppLint.sh
+++ housekeeping/cpplint/runCppLint.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
 
-#runs cpplint for all staged source files in dir src/main/cpp
-git diff --name-only --cached | grep "src/main" | grep "\(\.h\)\|\(\.cc\)$" | xargs housekeeping/cpplint/cpplint.py --root=src/main/cpp/include --linelength=120 --filter=-build/c++11
+function help {
+  echo "runCppLint.py <mode>
+  Executes cpplint on a filtered subset of *.h and *.cc files in directory include, source and test
+  <mode> defines the filter condition: options
+     staged: only files that are staged in git are checked
+     master: all files that have been changed compared to the local master HEAD are checked"
+  exit
+}
 
-#runs cpplint for all staged source files in dir src/main/cpp
-git diff --name-only --cached | grep "src/test" | grep "\(\.h\)\|\(\.cc\)$" | xargs housekeeping/cpplint/cpplint.py --root=src/test/cpp/include --linelength=120 --filter=-build/c++11
+git_cmd=""
+if [ "$1" == "staged" ]; then
+  git_cmd="git --no-pager diff --name-only --cached"
+elif [ "$1" == "master" ]; then
+  git_cmd="git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD master)"
+else
+  help
+fi
+
+# run cpplint for all staged source files in dir src/ and include/
+files=$($git_cmd | grep -P "^((src/)|(include/))" | grep "\(\.h\)\|\(\.cc\)$")
+num_files=$(echo "$files" | sed '/^$/d' | wc -l)
+if [ $num_files != "0" ]; then
+  echo "$files" | xargs housekeeping/cpplint/cpplint.py --root=include --linelength=120 --filter=-build/c++11,-legal/copyright
+else
+  echo "Nothing to be checked for directory src and include"
+fi
+
+# run cpplint for all staged source files in dir test
+files=$($git_cmd | grep -P "^test/" | grep "\(\.h\)\|\(\.cc\)$")
+num_files=$(echo "$files" | sed '/^$/d' | wc -l)
+if [ $num_files != "0" ]; then
+  echo "$files" | xargs housekeeping/cpplint/cpplint.py --linelength=120 --filter=-build/c++11,-legal/copyright
+else
+  echo "Nothing to be checked for directory test"
+fi
