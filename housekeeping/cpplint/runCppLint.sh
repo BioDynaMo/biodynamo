a9b822fb1ab2f465fba9f97d431a7e539a7b2cda
Fons Rademakers <Fons.Rademakers@cern.ch>
2016-08-04 16:32:17 +0200

    Add option to also run using a single file name or wildcard.
    Examples:
        housekeeping/cpplint/runCppLint.sh src/param.cc
        housekeeping/cpplint/runCppLint.sh include/*.h

--- housekeeping/cpplint/runCppLint.sh
+++ housekeeping/cpplint/runCppLint.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+SCRIPTPATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 function help {
-  echo "runCppLint.py <mode>
+  echo "runCppLint.py <mode>|<wildcard>
   Executes cpplint on a filtered subset of *.h and *.cc files in directory
   include, source and test.
   <mode> defines the filter condition options:
      staged: only files that are staged in git are checked
      master: all files that have been changed compared to the local
-             master HEAD are checked"
-  exit
+             master HEAD are checked
+  <wildcard> single file name or wildcard like *.cc or *.h"
+  exit 0
 }
 
 git_cmd=""
@@ -16,6 +19,11 @@ if [ "$1" == "staged" ]; then
   git_cmd="git --no-pager diff --name-only --cached"
 elif [ "$1" == "master" ]; then
   git_cmd="git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD master)"
+elif ls $1 1> /dev/null 2>&1; then
+   for f in $1; do
+      $SCRIPTPATH/cpplint.py --root=include --linelength=120 --filter=-build/c++11,-legal/copyright $f
+   done
+   exit 0
 else
   help
 fi
@@ -24,7 +32,7 @@ fi
 files=$($git_cmd | grep "^\(src\|include\)/.*" | grep "\(\.h\|\.cc\)$")
 num_files=$(echo "$files" | sed '/^$/d' | wc -l)
 if [ $num_files != "0" ]; then
-  echo "$files" | xargs housekeeping/cpplint/cpplint.py --root=include --linelength=120 --filter=-build/c++11,-legal/copyright
+  echo "$files" | xargs $SCRIPTPATH/cpplint.py --root=include --linelength=120 --filter=-build/c++11,-legal/copyright
 else
   echo "Nothing to be checked for directory src and include"
 fi
@@ -33,7 +41,9 @@ fi
 files=$($git_cmd | grep "^test/.*" | grep "\(\.h\|\.cc\)$")
 num_files=$(echo "$files" | sed '/^$/d' | wc -l)
 if [ $num_files != "0" ]; then
-  echo "$files" | xargs housekeeping/cpplint/cpplint.py --linelength=120 --filter=-build/c++11,-legal/copyright
+  echo "$files" | xargs $SCRIPTPATH/cpplint.py --linelength=120 --filter=-build/c++11,-legal/copyright
 else
   echo "Nothing to be checked for directory test"
 fi
+
+exit 0
