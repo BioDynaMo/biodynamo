0878d6a5e5027a9b55e3e628f37efb1da4a2a2ca
Fons Rademakers <Fons.Rademakers@cern.ch>
2016-08-26 16:11:55 +0200

    fix typo.

--- travis.sh
+++ travis.sh
@@ -14,7 +14,7 @@ if [ "$TRAVIS_OS_NAME" = "osx" ]; then
   # get latest cmake
   wget https://cmake.org/files/v3.6/cmake-3.6.1-Darwin-x86_64.tar.gz 2> /dev/null
   tar zxf cmake-3.6.1-Darwin-x86_64.tar.gz > /dev/null
-  export PATH="`pwd`/cmake-3.6.0-Darwin-x86_64/CMake.app/Contents/bin":$PATH:
+  export PATH="`pwd`/cmake-3.6.1-Darwin-x86_64/CMake.app/Contents/bin":$PATH:
 fi
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
