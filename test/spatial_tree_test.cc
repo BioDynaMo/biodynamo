041759b890910003d4664790b621176594aea900
Victor Drobny <vdrobny@yandex.ru>
2017-03-10 14:54:36 +0300

    rand was replaced by std::minstd_rand

--- test/spatial_tree_test.cc
+++ test/spatial_tree_test.cc
@@ -88,8 +88,12 @@ bool IsEqual(std::vector<std::pair<int, int>> a,
 bool SearchTest(SpatialTreeNode<int> *tree, int amount) {
   Point *possitions = new Point[amount];
   double gap = 1.0 / (amount + 1);
+  std::minstd_rand simple_rand;
+  simple_rand.seed(42);
+
   for (int i = 0; i < amount; i++) {
-    possitions[i] = Point(gap * i, rand() / RAND_MAX, rand() / RAND_MAX);
+    possitions[i] = Point(gap * i, simple_rand() / simple_rand.max(),
+                          simple_rand() / simple_rand.max());
   }
 
   for (int i = 0; i < amount; i++) {
