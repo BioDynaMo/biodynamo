7a82608b229897d4739995505aa1c67d9dca91a8
ahmad <hesam212@gmail.com>
2017-05-24 21:21:00 +0200

    Use fixed grid for neighbor search

--- demo/octree_bench.cc
+++ demo/octree_bench.cc
@@ -14,6 +14,7 @@ using bdm::NeighborOp;
 using bdm::NeighborNanoflannOp;
 using bdm::NeighborPclOp;
 using bdm::NeighborUnibnOp;
+using bdm::Scalar;
 
 template <typename T, typename Op>
 void RunTest(T* cells, const Op& op, const char* filename) {
@@ -35,19 +36,33 @@ void RunTest(T* cells, const Op& op, const char* filename) {
 int main(int args, char** argv) {
   if(args == 2) {
     omp_set_num_threads(omp_get_max_threads());
-    srand((unsigned int) time(NULL));
+    // srand((unsigned int) time(NULL));
     int num_cells;
+    size_t cells_per_dim = 128;
+    const double space = 20;
 
     std::istringstream(std::string(argv[1])) >> num_cells;
 
     auto cells = Cell<>::NewEmptySoa();
 
     // create array of cells
-    for (int i = 0; i < num_cells; i++) {
-      double x = 64.0f * rand () / (RAND_MAX + 1.0f);
-      double y = 64.0f * rand () / (RAND_MAX + 1.0f);
-      double z = 64.0f * rand () / (RAND_MAX + 1.0f);
-      cells.push_back(Cell<>({x, y, z}));
+    // for (int i = 0; i < num_cells; i++) {
+    //   double x = 64.0f * rand () / (RAND_MAX + 1.0f);
+    //   double y = 64.0f * rand () / (RAND_MAX + 1.0f);
+    //   double z = 64.0f * rand () / (RAND_MAX + 1.0f);
+    //   cells.push_back(Cell<>({x, y, z}));
+    // }
+    for (size_t i = 0; i < cells_per_dim; i++) {
+      for (size_t j = 0; j < cells_per_dim; j++) {
+        for (size_t k = 0; k < cells_per_dim; k++) {
+          Cell<Scalar> cell({i * space, j * space, k * space});
+          cell.SetDiameter(30);
+          cell.SetAdherence(0.4);
+          cell.SetMass(1.0);
+          cell.UpdateVolume();
+          cells.push_back(cell);
+        }
+      }
     }
 
     RunTest(&cells, NeighborOp(), "NeighborOp.txt");
