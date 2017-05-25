f3bcf971c94c88595c17caac99245b7f9be19ddf
Roman Bauer <romanbauer-ncl>
2017-05-25 15:00:14 +0100

    amended exporter with paraview exporter in my branch

--- demo/cell_growth.cc
+++ demo/cell_growth.cc
@@ -1,7 +1,7 @@
-#include <omp.h>
 #include <cmath>
 #include <functional>
 #include <iostream>
+#include <omp.h>
 #include <sstream>
 
 #include "backend.h"
@@ -15,7 +15,6 @@
 #include "scheduler.h"
 #include "timing.h"
 #include "timing_aggregator.h"
-#include "transactional_vector.h"
 // #include <ittnotify.h>
 
 using bdm::Cell;
@@ -25,8 +24,8 @@ using bdm::Timing;
 using bdm::TimingAggregator;
 using bdm::Exporter;
 
-void Execute(size_t cells_per_dim, size_t iterations, size_t threads,
-             size_t repititions, TimingAggregator* statistic,
+void execute(size_t cells_per_dim, size_t iterations, size_t threads,
+             size_t repititions, TimingAggregator *statistic,
              bool with_export) {
   for (size_t r = 0; r < repititions; r++) {
     std::stringstream ss;
@@ -36,7 +35,6 @@ void Execute(size_t cells_per_dim, size_t iterations, size_t threads,
 
     const double space = 20;
 
-    // bdm::TransactionalVector<Cell<Scalar>> cells;
     auto cells = Cell<>::NewEmptySoa();
     cells.reserve(cells_per_dim * cells_per_dim * cells_per_dim);
     {
@@ -55,58 +53,61 @@ void Execute(size_t cells_per_dim, size_t iterations, size_t threads,
       }
     }
 
-    {
-      Timing timing("Find Neighbors", statistic);
-      // bdm::NeighborOp op(700);
-      bdm::NeighborNanoflannOp op(700);
-      op.Compute(&cells);
+    // iterate for all (time) steps
+    Exporter exporter;
+    if (with_export) {
+      exporter.CreatePVDFile("Results4Paraview", iterations, 1.0);
     }
 
-    // __itt_resume();
-
-    {
-      Timing timing("Cell Growth", statistic);
-      bdm::DividingCellOp biology;
-      for (size_t i = 0; i < iterations; i++) {
-        biology.Compute(&cells);
-      }
-    }
-
-    // __itt_pause();
-
-    {
-      Timing timing("Displacement", statistic);
-      bdm::DisplacementOp op;
-      for (size_t i = 0; i < iterations; i++) {
+    for (size_t i = 0; i < iterations; i++) {
+      {
+        Timing timing("Find Neighbors", statistic);
+        bdm::NeighborOp op(700);
         op.Compute(&cells);
       }
-    }
 
-    if (with_export) {
-      Timing timing("Export", statistic);
-      std::cout << "exporting now..." << std::endl;
-      Exporter exporter;
-      exporter.ToFile(cells, "FinalPositions.dat");
-      exporter.ToMatlabFile(cells, "FinalPositions.m");
-      exporter.ToNeuroMLFile(cells, "FinalPositions.xml");
+      // __itt_resume();
+
+      {
+        Timing timing("Cell Growth", statistic);
+        bdm::DividingCellOp biology;
+        biology.Compute(&cells);
+      }
+
+      // __itt_pause();
+
+      {
+        Timing timing("Displacement", statistic);
+        bdm::DisplacementOp op;
+        op.Compute(&cells);
+      }
+
+      if (with_export) {
+        Timing timing("Export", statistic);
+        std::cout << "exporting now..." << std::endl;
+        exporter.ToFile(&cells, "FinalPositions.dat");
+        exporter.ToMatlabFile(&cells, "FinalPositions.m");
+        exporter.ToNeuroMLFile(&cells, "FinalPositions.xml");
+        exporter.ToVTUFile(&cells, "Results4Paraview", i);
+      }
     }
   }
 }
 
-void Scaling(size_t cells_per_dim, size_t iterations, size_t repititions,
-             TimingAggregator* statistic, bool with_export,
-             const std::function<void(int&)> thread_inc =
-                 [](int& i) {  // NOLINT(runtime/references)
+void scaling(size_t cells_per_dim, size_t iterations, size_t repititions,
+             TimingAggregator *statistic, bool with_export,
+             const std::function<void(int &)> thread_inc =
+                 [](int &i) { // NOLINT(runtime/references)
                    i *= 2;
                  },
              const int max_threads = omp_get_max_threads()) {
   for (int i = 1; i <= max_threads; thread_inc(i)) {
     omp_set_num_threads(i);
-    Execute(cells_per_dim, iterations, i, repititions, statistic, with_export);
+    execute(cells_per_dim, iterations, i, repititions, statistic, with_export);
   }
 }
 
-int main(int args, char** argv) {
+int main(int args, char **argv) {
   TimingAggregator statistic;
   size_t repititions = 1;
   bool do_export = false;
@@ -114,37 +115,36 @@ int main(int args, char** argv) {
       (std::string(argv[1]) == "help" || std::string(argv[1]) == "--help")) {
     // clang-format off
     std::cout << "SYNOPSIS\n"
-              << "  ./cell_growth help | --help |\n"
-              << "         [#repititions] | \n"
-              << "         [#cells_per_dim #iterations #threads [#repititions] | \n"
-              << "         --scaling [#repititions] | \n"
-              << "         --detailed-scaling [#repititions] \n"
-              << "\nDESCRIPTION\n"
-              << "  Creates a three dimensional grid of cells, calculates neighbors, simulates \n"
-              << "  cell growth and calculates displacement based on mechanical forces\n"
-              << "  outputs runtime statistic for each operation\n"
-              << "\nOPTIONS\n"
-              << "  help | --help\n"
-              << "    Explains usage of this binary and its command line options\n"
-              << "\n  [#repititions]\n"
-              << "     number of cells per dimension: 8 (-> total number of cells: 8^3 = 512)\n"
-              << "     number of iterations:          1\n"
-              << "     number of threads:             1\n"
-              << "     number of repititions:         according to parameter - 1 if not specified\n"
-              << "\n  --scaling [#repititions]\n"
-              << "     executes the simulation several times with different number of threads\n"
-              << "     number of cells per dimension: 128 (-> total number of cells: 128^3 =~ 2.1M)\n"
-              << "     number of iterations:          1\n"
-              << "     number of threads:             1 - logical CPUs on the system - incremented *= 2\n"
-              << "     number of repititions:         according to parameter - 1 if not specified\n"
-              << "\n  --detailed-scaling [#repititions]\n"
-              << "     executes the simulation several times with different number of threads\n"
-              << "     number of cells per dimension: 128 (-> total number of cells: 128^3 =~ 2.1M)\n"
-              << "     number of iterations:          1\n"
-              << "     number of threads:             1 - logical CPUs on the system - threads incremented += 1\n"
-              << "     number of repititions:         according to parameter - 1 if not specified\n"
-              << std::endl;
-    // clang-format on
+  << "  ./cell_growth help | --help |\n"
+  << "         [#repititions] | \n"
+  << "         [#cells_per_dim #iterations #threads [#repititions] | \n"
+  << "         --scaling [#repititions] | \n"
+  << "         --detailed-scaling [#repititions] \n"
+  << "\nDESCRIPTION\n"
+  << "  Creates a three dimensional grid of cells, calculates neighbors, simulates \n"
+  << "  cell growth and calculates displacement based on mechanical forces\n"
+  << "  outputs runtime statistic for each operation\n"
+  << "\nOPTIONS\n"
+  << "  help | --help\n"
+  << "    Explains usage of this binary and its command line options\n"
+  << "\n  [#repititions]\n"
+  << "     number of cells per dimension: 8 (-> total number of cells: 8^3 = 512)\n"
+  << "     number of iterations:          1\n"
+  << "     number of threads:             1\n"
+  << "     number of repititions:         according to parameter - 1 if not specified\n"
+  << "\n  --scaling [#repititions]\n"
+  << "     executes the simulation several times with different number of threads\n"
+  << "     number of cells per dimension: 128 (-> total number of cells: 128^3 =~ 2.1M)\n"
+  << "     number of iterations:          1\n"
+  << "     number of threads:             1 - logical CPUs on the system - incremented *= 2\n"
+  << "     number of repititions:         according to parameter - 1 if not specified\n"
+  << "\n  --detailed-scaling [#repititions]\n"
+  << "     executes the simulation several times with different number of threads\n"
+  << "     number of cells per dimension: 128 (-> total number of cells: 128^3 =~ 2.1M)\n"
+  << "     number of iterations:          1\n"
+  << "     number of threads:             1 - logical CPUs on the system - threads incremented += 1\n"
+  << "     number of repititions:         according to parameter - 1 if not specified\n"
+  << std::endl;
     return 0;
   } else if (args >= 4) {
     size_t cells;
@@ -157,23 +157,23 @@ int main(int args, char** argv) {
       std::istringstream(std::string(argv[4])) >> repititions;
     }
     omp_set_num_threads(threads);
-    Execute(cells, iterations, threads, repititions, &statistic, do_export);
+    execute(cells, iterations, threads, repititions, &statistic, do_export);
   } else if (args >= 2 && std::string(argv[1]) == "--scaling") {
     if (args == 3) {
       std::istringstream(std::string(argv[2])) >> repititions;
     }
-    Scaling(256, 1, repititions, &statistic, do_export);
+    scaling(256, 1, repititions, &statistic, do_export);
   } else if (args >= 2 && std::string(argv[1]) == "--detailed-scaling") {
     if (args == 3) {
       std::istringstream(std::string(argv[2])) >> repititions;
     }
-    Scaling(256, 1, repititions, &statistic, do_export, [](int& i) { i++; });
+    scaling(256, 1, repititions, &statistic, do_export, [](int& i) { i++; });
   } else {
     omp_set_num_threads(1);
     if (args == 2) {
       std::istringstream(std::string(argv[1])) >> repititions;
     }
-    Execute(8, 1, 1, repititions, &statistic, do_export);
+    execute(8, 1, 1, repititions, &statistic, do_export);
   }
   std::cout << statistic << std::endl;
   return 0;
--- test/exporter_test.cc
+++ test/exporter_test.cc
@@ -20,7 +20,7 @@ TEST(ExportTest, ConductExportToFile) {
   Exporter exporter;
 
   // Test the standard file exporter
-  exporter.ToFile(cells, "TestExporter.dat");
+  exporter.ToFile(&cells, "TestExporter.dat");
   std::ifstream t;
   std::stringstream buffer;
   t.open("TestExporter.dat");
@@ -35,7 +35,7 @@ TEST(ExportTest, ConductExportToFile) {
   remove("TestExporter.dat");
 
   // Test the Matlab file exporter
-  exporter.ToMatlabFile(cells, "TestMatlabExporter.m");
+  exporter.ToMatlabFile(&cells, "TestMatlabExporter.m");
   t.open("TestMatlabExporter.m");
   std::getline(t, line);
   EXPECT_EQ("CellPos = zeros(2,3);", line);
@@ -47,5 +47,34 @@ TEST(ExportTest, ConductExportToFile) {
   EXPECT_EQ("", line);
   t.close();
   remove("TestMatlabExporter.m");
+
+  // Test the Paraview exporter
+  exporter.CreatePVDFile("TestResultsParaview", 1, 1.0);
+  exporter.ToVTUFile(&cells, "TestResultsParaview", 1);
+  t.open("TestResultsParaview.pvd");
+  std::getline(t, line);
+  EXPECT_EQ("<?xml version=\"1.0\"?>", line);
+  std::getline(t, line);
+  EXPECT_EQ(
+      "<VTKFile type=\"Collection\" version=\"0.1\" "
+      "byte_order=\"LittleEndian\">",
+      line);
+  t.close();
+
+  t.open("TestResultsParaview-1.vtu");
+  std::getline(t, line);
+  EXPECT_EQ(
+      "<VTKFile type=\"UnstructuredGrid\" version=\"0.1\" "
+      "byte_order=\"LittleEndian\">",
+      line);
+  std::getline(t, line);
+  EXPECT_EQ("   <UnstructuredGrid>", line);
+  std::getline(t, line);
+  EXPECT_EQ("      <Piece  NumberOfPoints=\"2\" NumberOfCells=\"2\">", line);
+  std::getline(t, line);
+  std::getline(t, line);
+  std::getline(t, line);
+  EXPECT_EQ(" 0.5 1 0 -5 5 0.9", line);
+  t.close();
 }
 }  // namespace bdm
