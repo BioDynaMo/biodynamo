a178c8349cf88592db003865c50d649cf36c6747
Konstantinos Kanellis <kkanelli@hotmail.com>
2017-07-19 17:58:03 +0200

    Integrate visualization into cell_division example
    Integrate `catalyst_adaptor` into scheduler

--- src/scheduler.h
+++ src/scheduler.h
@@ -7,13 +7,26 @@
 #include "op_timer.h"
 #include "resource_manager.h"
 
+#ifdef USE_CATALYST
+#include "param.h"
+#include "visualization/catalyst_adaptor.h"
+#endif
+
 namespace bdm {
 
 class Scheduler {
  public:
-  Scheduler() {}
+  Scheduler() {
+#ifdef USE_CATALYST
+    catalyst::Initialize("../src/visualization/simple_pipeline.py");
+#endif
+  }
 
-  virtual ~Scheduler() {}
+  virtual ~Scheduler() {
+#ifdef USE_CATALYST
+    catalyst::Finalize();
+#endif
+  }
 
   template <typename TCellContainer>
   void Simulate(unsigned steps) {
@@ -23,13 +36,23 @@ class Scheduler {
 
     auto cells = ResourceManager<TCellContainer>::Get()->GetCells();
 
-    while (steps-- > 0) {
+    for (unsigned step = 0; step < steps; step++) {
       neighbor.Compute(cells);
       biology.Compute(cells);
       physics.Compute(cells);
       cells->Commit();
+
+#ifdef USE_CATALYST
+      double time = Param::kSimulationTimeStep * total_steps;
+      catalyst::CoProcess(cells, time, total_steps, step == steps - 1);
+#endif
+
+      total_steps++;
     }
   }
+
+ private:
+  unsigned long total_steps = 0;
 };
 
 }  // namespace bdm
