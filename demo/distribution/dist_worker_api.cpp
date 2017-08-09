30483b30392c7828606e526b5bda48a795892f2d
Konstantinos Kanellis <kkanelli@hotmail.com>
2017-08-09 17:40:01 +0200

    API changes in high-level distributed class
    The class is now responsible for all the ZMQ related objects. No
    socket/pipes objects are visible to the application, whose only
    way to interact with other nodes is via class methods.
    
    In addition, better error handling and graceful termination support
    for this class is implemented.

--- demo/distribution/dist_worker_api.cpp
+++ demo/distribution/dist_worker_api.cpp
@@ -16,13 +16,32 @@ DistWorkerAPI::~DistWorkerAPI () {
     delete thread_;
 }
 
-void DistWorkerAPI::StartThread(const std::string& app_endpoint) {
-    this->info_->app_endpoint_ = app_endpoint;
+bool DistWorkerAPI::Start() {
+    // Create (and bind) parent socket
+    parent_pipe_ = new zmqpp::socket( *(info_->ctx_), zmqpp::socket_type::pair);
+    endpoint_ = "inproc://W_API_" + info_->identity_;
 
+    try {
+        parent_pipe_->bind(endpoint_);
+    }
+    catch (const zmqpp::zmq_internal_exception& ) {
+        std::cout << "E: Endpoint '" << info_->identity_ << "' already taken" << std::endl;
+        return false;
+    }
+
+    // Create child socket
+    child_pipe_ = new zmqpp::socket( *(info_->ctx_), zmqpp::socket_type::pair);
+    child_pipe_->connect(endpoint_);
+
+    // Create background thread
     thread_ = new std::thread(
         std::bind(&DistWorkerAPI::HandleNetwork, this)
     );
     std::cout << "I: started thread with id " << thread_->get_id() << std::endl;
+
+    auto sig = parent_pipe_->wait();
+    assert(sig == zmqpp::signal::ok || sig == zmqpp::signal::ko);
+    return (sig == zmqpp::signal::ok);
 }
 
 void DistWorkerAPI::SetBrokerEndpoint(const std::string& endpoint) {
@@ -37,34 +56,69 @@ void DistWorkerAPI::SetRightNeighbourEndpoint(const std::string& endpoint) {
     this->rworker_endpoint_ = endpoint;
 }
 
-void DistWorkerAPI::WaitForTermination() {
-   thread_->join();
-   thread_ = nullptr;
+void DistWorkerAPI::SendMessage(zmqpp::message& msg) {
+    parent_pipe_->send(msg);
+}
+
+
+void DistWorkerAPI::ReceiveMessage(zmqpp::message *msg) {
+    parent_pipe_->receive( *msg);
+}
+
+bool DistWorkerAPI::Stop(bool wait /* = true */, bool force /* = false */) {
+    if (!parent_pipe_) {
+        return false;
+    }
+
+    if ( !parent_pipe_->send(zmqpp::signal::stop, true) ) {
+        // Cannot deliver signal. Maybe force kill?
+        // TODO: handle force kill
+        return false;
+    }
+
+    auto sig = parent_pipe_->wait();
+    assert(sig == zmqpp::signal::ok || sig == zmqpp::signal::ko);
+    if (sig == zmqpp::signal::ok) {
+        thread_->join();
+        thread_ = nullptr;
+
+        delete parent_pipe_;
+    }
+
+    return (sig == zmqpp::signal::ok);
 }
 
 void DistWorkerAPI::HandleNetwork() {
-    // Create local/application socket
-    info_->app_socket_ = new zmqpp::socket( *(info_->ctx_), zmqpp::socket_type::pair);
-    info_->app_socket_->connect(info_->app_endpoint_);
+
+    try {
+        // Create broker communicator
+        if (!broker_endpoint_.empty()) {
+            broker_comm_ = new BrokerCommunicator(info_, broker_endpoint_);
+        }
+
+        // Create left communicator
+        if (!lworker_endpoint_.empty()) {
+            lworker_comm_ = new WorkerCommunicator(info_, lworker_endpoint_, true);
+        }
+
+        // Create right communicator
+        if (!rworker_endpoint_.empty()) {
+            rworker_comm_ = new WorkerCommunicator(info_, rworker_endpoint_, false);
+        }
+    }
+    catch (...) {
+        eptr_ = std::current_exception();
+        child_pipe_->send(zmqpp::signal::ko);
+
+        std::cout << "E: exception thrown!" << std::endl;
+        goto cleanup;
+    }
+    child_pipe_->send(zmqpp::signal::ok);
 
     // Add app_sock to reactor
-    info_->reactor_->add( *(info_->app_socket_), std::bind(&DistWorkerAPI::HandleAppMessage, this));
+    info_->reactor_->add( *child_pipe_, std::bind(&DistWorkerAPI::HandleAppMessage, this));
 
-    // Create broker communicator
-    if (!broker_endpoint_.empty()) {
-        broker_comm_ = new BrokerCommunicator(info_, broker_endpoint_);
-    }
-
-    // Create left communicator
-    if (!lworker_endpoint_.empty()) {
-        lworker_comm_ = new WorkerCommunicator(info_, lworker_endpoint_, true);
-    }
-
-    // Create right communicator
-    if (!rworker_endpoint_.empty()) {
-        rworker_comm_ = new WorkerCommunicator(info_, rworker_endpoint_, false);
-    }
-
+    std::cout << "I: listening to network..." << std::endl;
     while ( !info_->zctx_interrupted_ ) {
         if ( !info_->reactor_->poll ( HEARTBEAT_INTERVAL.count() ) ) {
             if (broker_comm_) {
@@ -82,7 +136,8 @@ void DistWorkerAPI::HandleNetwork() {
         }
     }
 
-    Terminate();
+cleanup:
+    Cleanup();
     std::cout << "I: Terminated!" << std::endl;
 }
 
@@ -93,7 +148,7 @@ void DistWorkerAPI::HandleAppMessage () {
     // Frame 3: application frame(s)
 
     zmqpp::message msg;
-    if ( !info_->app_socket_->receive(msg)
+    if ( !child_pipe_->receive(msg)
             || msg.is_signal() ) {
         // Interrupted
         info_->zctx_interrupted_ = true;
@@ -137,15 +192,15 @@ void DistWorkerAPI::HandleNetworkMessages() {
 
         if (comm_id == BROKER_COMM) {
             assert(broker_comm_);
-            info_->app_socket_->send(*msg_p);
+            child_pipe_->send(*msg_p);
         }
         else if (comm_id == LEFT_NEIGHBOUR_COMM) {
             assert(lworker_comm_);
-            info_->app_socket_->send(*msg_p);
+            child_pipe_->send(*msg_p);
         }
         else if (comm_id == RIGHT_NEIGHBOUR_COMM) {
             assert(rworker_comm_);
-            info_->app_socket_->send(*msg_p);
+            child_pipe_->send(*msg_p);
         }
         else {
             std::cout << "E: Invalid communicator id: " << comm_id << std::endl;
@@ -155,21 +210,28 @@ void DistWorkerAPI::HandleNetworkMessages() {
     info_->pending_->clear();
 }
 
-void DistWorkerAPI::Terminate() {
-    if (broker_comm_) {
-        delete broker_comm_;
+void DistWorkerAPI::Cleanup() {
+    try {
+        if (broker_comm_) {
+            delete broker_comm_;
+        }
+        if (lworker_comm_) {
+            delete lworker_comm_;
+        }
+        if (rworker_comm_) {
+            delete rworker_comm_;
+        }
+        info_->reactor_->remove( *child_pipe_ );
+
+        // Everything cleaned!
+        child_pipe_->send(zmqpp::signal::ok);
     }
-    if (lworker_comm_) {
-        delete lworker_comm_;
-    }
-    if (rworker_comm_) {
-        delete rworker_comm_;
+    catch (...) {
+        // Error occured; no gratefull exited
+        child_pipe_->send(zmqpp::signal::ko);
     }
 
-    info_->reactor_->remove( *(info_->app_socket_) );
-    info_->app_socket_->close();
-    delete info_->app_socket_;
-
+    delete child_pipe_;
     delete info_->reactor_;
 }
 
