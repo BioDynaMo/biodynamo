87e7d03c9ddec1afc08209887af532e7f09f0ccd
Konstantinos Kanellis <kkanelli@hotmail.com>
2017-08-22 18:28:22 +0200

    Replace plain casts with proper conversion

--- demo/distribution/protocol.h
+++ demo/distribution/protocol.h
@@ -4,12 +4,14 @@
 #include <Rtypes.h>
 #include <TBufferFile.h>
 
+#include <string>
+
 #include "common.h"
 
 namespace bdm {
 
-const std::string protocol_client = "BDM/0.1C";
-const std::string protocol_worker = "BDM/0.1W";
+const std::string PROTOCOL_CLIENT = "BDM/0.1C";
+const std::string PROTOCOL_WORKER = "BDM/0.1W";
 
 // Distributed API commands
 enum class AppProtocolCommand : std::uint8_t {
@@ -50,43 +52,47 @@ enum class WorkerProtocolCmd : std::uint8_t {
   kMinValue = kReady,
   kMaxValue = kDisconnect,
   kCount = kMaxValue
-
 };
 
 class CommandHeader {
  public:
-  CommandHeader() {};
-  CommandHeader(CommunicatorId sender, CommunicatorId receiver) :
-    sender_(sender), receiver_(receiver) { }
-  virtual ~CommandHeader() { }
+  CommandHeader() {}
+  CommandHeader(CommunicatorId sender, CommunicatorId receiver)
+      : sender_(sender), receiver_(receiver) {}
+  virtual ~CommandHeader() {}
 
   CommunicatorId sender_ = CommunicatorId::kUndefined;
   CommunicatorId receiver_ = CommunicatorId::kUndefined;
 
   // Deserialize class
-  template<typename T>
-  static std::unique_ptr<T> FromString(const std::string * str) {
-    TBufferFile buf (TBufferFile::EMode::kRead, str->size(), ((void *)str->c_str()));
+  template <typename T>
+  static std::unique_ptr<T> FromString(const std::string* str) {
+    TBufferFile buf(
+        TBufferFile::EMode::kRead, str->size(),
+        (const_cast<void*>(reinterpret_cast<const void*>(str->c_str()))));
 
-    // find specialized type
     return std::unique_ptr<T>(
-        (T*)buf.ReadObjectAny(T::Class())
-    );
+        reinterpret_cast<T*>(buf.ReadObjectAny(T::Class())));
   }
 
   ClassDef(CommandHeader, 1);
 };
 
 #define optional_arg(class_, type, name, default_value) \
-  public: class_& name(type name) { name##_ = name; return *this; } \
-          type name##_ = default_value
+ public:                                                \
+  class_& name(type name) {                             \
+    name##_ = name;                                     \
+    return *this;                                       \
+  }                                                     \
+  type name##_ = default_value
 
 class ClientCommandHeader : public CommandHeader {
  public:
-  ClientCommandHeader() {};
-  ClientCommandHeader(ClientProtocolCmd cmd, CommunicatorId sender, CommunicatorId receiver) :
-    CommandHeader(sender, receiver), cmd_(cmd) { }
-  virtual ~ClientCommandHeader() {};
+  ClientCommandHeader() {}
+  ClientCommandHeader(ClientProtocolCmd cmd, CommunicatorId sender,
+                      CommunicatorId receiver)
+      : CommandHeader(sender, receiver), cmd_(cmd) {}
+  virtual ~ClientCommandHeader() {}
 
   ClientProtocolCmd cmd_ = ClientProtocolCmd::kInvalid;
 
@@ -96,23 +102,24 @@ class ClientCommandHeader : public CommandHeader {
 
   // Serialize class
   std::unique_ptr<std::string> ToString() {
-    TBufferFile buf (TBufferFile::EMode::kWrite);
-    assert( buf.WriteObjectAny(this, ClientCommandHeader::Class()) == 1 );
-    assert( buf.CheckObject(this, ClientCommandHeader::Class()) );
+    TBufferFile buf(TBufferFile::EMode::kWrite);
+    assert(buf.WriteObjectAny(this, ClientCommandHeader::Class()) == 1);
+    assert(buf.CheckObject(this, ClientCommandHeader::Class()));
 
-    return std::make_unique<std::string>(std::string(buf.Buffer(), buf.Length()));
+    return std::make_unique<std::string>(
+        std::string(buf.Buffer(), buf.Length()));
   }
 
   ClassDef(ClientCommandHeader, 1);
 };
 
-
 class WorkerCommandHeader : public CommandHeader {
  public:
-  WorkerCommandHeader() { };
-  WorkerCommandHeader(WorkerProtocolCmd cmd, CommunicatorId sender, CommunicatorId receiver) :
-    CommandHeader(sender, receiver), cmd_(cmd) { }
-  virtual ~WorkerCommandHeader() { };
+  WorkerCommandHeader() {}
+  WorkerCommandHeader(WorkerProtocolCmd cmd, CommunicatorId sender,
+                      CommunicatorId receiver)
+      : CommandHeader(sender, receiver), cmd_(cmd) {}
+  virtual ~WorkerCommandHeader() {}
 
   WorkerProtocolCmd cmd_ = WorkerProtocolCmd::kInvalid;
 
@@ -122,17 +129,17 @@ class WorkerCommandHeader : public CommandHeader {
 
   // Serialize class
   std::unique_ptr<std::string> ToString() {
-    TBufferFile buf (TBufferFile::EMode::kWrite);
-    assert( buf.WriteObjectAny(this, WorkerCommandHeader::Class()) == 1 );
-    assert( buf.CheckObject(this, WorkerCommandHeader::Class()) );
+    TBufferFile buf(TBufferFile::EMode::kWrite);
+    assert(buf.WriteObjectAny(this, WorkerCommandHeader::Class()) == 1);
+    assert(buf.CheckObject(this, WorkerCommandHeader::Class()));
 
-    return std::make_unique<std::string>(std::string(buf.Buffer(), buf.Length()));
+    return std::make_unique<std::string>(
+        std::string(buf.Buffer(), buf.Length()));
   }
 
   ClassDef(WorkerCommandHeader, 1);
 };
 
-
 }  // namespace bdm
 
-#endif
+#endif  // DEMO_DISTRIBUTION_PROTOCOL_H_
