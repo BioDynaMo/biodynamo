42b72dc6730d3f339f12cb909c9b14c584c42bb4
Konstantinos Kanellis <kkanelli@hotmail.com>
2017-08-24 11:05:00 +0200

    Wrapper over ROOT logging module
    Write a logger class (at `logger.h`) which is a wrapper over ROOT
    logging module. Implemented using variadic templates (instead of
    C-style *printf used in ROOT), to easily pass any object, as long
    as an overloading for the `<<` operator is defined.
    
    At the constructor, `level` argument is passed, which defines the
    minimum level of message types (Info, Error, Fatal etc.) that will be
    finally printed to the console. This provides an easy way to show/hide
    specific message types (e.g during debugging phase).

--- demo/distribution/common.h
+++ demo/distribution/common.h
@@ -18,9 +18,6 @@ namespace bdm {
 typedef std::chrono::time_point<std::chrono::system_clock> time_point_t;
 typedef std::chrono::milliseconds duration_ms_t;
 
-std::ostream& operator<<(std::ostream& out, const zmqpp::message& msg);
-std::string toHex(const std::string& in);
-
 struct DistSharedInfo {
   std::vector<std::unique_ptr<zmqpp::message> >* pending_;
   zmqpp::reactor* reactor_;        // Polling handler
@@ -72,6 +69,60 @@ const duration_ms_t HEARTBEAT_INTERVAL(2500);  //  msecs
 const duration_ms_t HEARTBEAT_EXPIRY = HEARTBEAT_INTERVAL * HEARTBEAT_LIVENESS;
 
 // ---------------------------------------------
+
+// Converts a string to its hex representation
+inline std::string ToHex(const std::string& in) {
+  std::stringstream ss;
+  for (size_t i = 0; i < in.length(); i++) {
+    ss << std::hex << std::setfill('0') << std::setw(2) << std::uppercase
+       << (static_cast<int>(in[i]));
+  }
+  return ss.str();
+}
+
+inline std::ostream& operator<<(std::ostream& out, const zmqpp::message& msg) {
+  size_t frames = msg.parts();
+  if (!frames) {
+    out << std::endl << "*EMPTY_MESSAGE*" << std::endl;
+    return out;
+  }
+
+  std::ostringstream ss;
+
+  std::string part;
+  for (size_t i = 0; i < msg.parts(); i++) {
+    msg.get(part, i);
+
+    // probe message
+    bool is_binary = false;
+    for (size_t j = 0; j < part.size(); j++) {
+      if (part[j] < 9 || part[j] > 127) {
+        is_binary = true;
+        break;
+      }
+    }
+
+    size_t max_size = is_binary ? 35 : 70;
+    std::string ellipses = "";
+
+    if (part.size() > max_size) {
+      part.resize(max_size);
+      ellipses = "...";
+    }
+    ss << "[" << std::setfill('0') << std::setw(3) << part.size() << "] ";
+
+    if (is_binary) {
+      ss << "0x" << ToHex(part);
+    } else {
+      ss << part;
+    }
+    ss << ellipses << std::endl;
+  }
+
+  out << std::endl << ss.str();
+  return out;
+}
+
 }  // namespace bdm
 
 #endif  // DEMO_DISTRIBUTION_COMMON_H__
---
+++ demo/distribution/logger.h
@@ -0,0 +1,138 @@
+#ifndef DEMO_DISTRIBUTION_LOGGER_H_
+#define DEMO_DISTRIBUTION_LOGGER_H_
+
+#include <TError.h>
+#include <cstdio>
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <tuple>
+
+#include "common.h"
+
+namespace bdm {
+
+// ROOT logging levels
+enum class LoggingLevel : Int_t {
+  kUnset = ::kUnset,
+  kPrint = ::kPrint,
+  kDebug = ::kPrint,
+  kInfo = ::kInfo,
+  kWarning = ::kWarning,
+  kError = ::kError,
+  kBreak = ::kBreak,
+  kSysError = ::kSysError,
+  kFatal = ::kFatal
+};
+
+// Wrapper class over ROOT logging module
+class Logger {
+ public:
+  explicit Logger(const std::string& location,
+                  LoggingLevel level = LoggingLevel::kUnset)
+      : level_(level), location_(location) {}
+  ~Logger() {}
+
+  template <typename... Args>
+  void Print(const Args&... parts) const {
+    if (LoggingLevel::kPrint >= level_) {
+      auto message = ConstructMessage(parts...);
+      // Print to stderr (as ROOT does for every message)
+      fprintf(stderr, "%s", message->c_str());
+    }
+  }
+
+  template <typename... Args>
+  void Debug(const Args&... parts) const {
+    if (LoggingLevel::kDebug >= level_) {
+      auto message = ConstructMessage(parts...);
+      // Emulate ROOT logging message
+      fprintf(stderr, "Debug in <%s>: %s\n", location_.c_str(),
+              message->c_str());
+    }
+  }
+
+  template <typename... Args>
+  void Info(const Args&... parts) const {
+    if (LoggingLevel::kInfo >= level_) {
+      auto message = ConstructMessage(parts...);
+      // ROOT function
+      ::Info(location_.c_str(), "%s", message->c_str());
+    }
+  }
+
+  template <typename... Args>
+  void Warning(const Args&... parts) const {
+    if (LoggingLevel::kWarning >= level_) {
+      auto message = ConstructMessage(parts...);
+      // ROOT function
+      ::Warning(location_.c_str(), "%s", message->c_str());
+    }
+  }
+
+  template <typename... Args>
+  void Error(const Args&... parts) const {
+    if (LoggingLevel::kError >= level_) {
+      auto message = ConstructMessage(parts...);
+      // ROOT function
+      ::Error(location_.c_str(), "%s", message->c_str());
+    }
+  }
+
+  template <typename... Args>
+  void Break(const Args&... parts) const {
+    if (LoggingLevel::kBreak >= level_) {
+      auto message = ConstructMessage(parts...);
+      // ROOT function
+      ::Break(location_.c_str(), "%s", message->c_str());
+    }
+  }
+
+  template <typename... Args>
+  void SysError(const Args&... parts) const {
+    if (LoggingLevel::kSysError >= level_) {
+      auto message = ConstructMessage(parts...);
+      // ROOT function
+      ::SysError(location_.c_str(), "%s", message->c_str());
+    }
+  }
+
+  template <typename... Args>
+  void Fatal(const Args&... parts) const {
+    if (LoggingLevel::kFatal >= level_) {
+      auto message = ConstructMessage(parts...);
+      // ROOT function
+      ::Fatal(location_.c_str(), "%s", message->c_str());
+    }
+  }
+
+ private:
+  template <typename... Args>
+  std::unique_ptr<std::string> ConstructMessage(const Args&... parts) const {
+    std::ostringstream message;
+    message << open_str_;
+    ConcatNextPart(&message, parts...);
+    return std::make_unique<std::string>(message.str());
+  }
+
+  // Base case
+  void ConcatNextPart(std::ostringstream* ss) const { *ss << close_str_; }
+
+  // General case
+  template <typename T, typename... Args>
+  void ConcatNextPart(std::ostringstream* ss, const T& arg,
+                      const Args&... parts) const {
+    *ss << arg;
+    ConcatNextPart(ss, parts...);
+  }
+
+  const std::string open_str_ = "";
+  const std::string close_str_ = "";
+
+  LoggingLevel level_;
+  std::string location_;
+};
+}  // namespace bdm
+
+#endif  // DEMO_DISTRIBUTION_LOGGER_H_
