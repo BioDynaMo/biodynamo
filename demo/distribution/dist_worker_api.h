586ebf2cdb51b2d61cade9d97f7911734682c393
Konstantinos Kanellis <kkanelli@hotmail.com>
2017-08-26 23:18:55 +0200

    Zero-copy message sending/receiving
    No intermediate copies of the sending/receiving messages are created
    during the execution of the distributed runtime. To achive this, we
    switch from the ZeroMQ PAIR socket communication (used to exchange
    data between threads) with mutex-protected queues.
    
    One more reason for this change, is that we no longer have to
    serialize the message, to be sent over the pipe. We serialize a single
    time -- when sending the message over the network.

--- demo/distribution/dist_worker_api.h
+++ demo/distribution/dist_worker_api.h
@@ -6,6 +6,7 @@
 #include <condition_variable>
 #include <deque>
 #include <mutex>
+#include <queue>
 #include <string>
 #include <thread>
 #include <vector>
@@ -25,16 +26,26 @@ class DistWorkerAPI {
   bool Start();
 
   void AddBrokerCommunicator(const std::string& endpoint);
+
+  /// \brief Add a connection to a worker positioned left (to this worker)
+  //
+  //  This means that this worker is considered as RIGHT worker (by the other
+  //  worker)
   void AddLeftNeighbourCommunicator(const std::string& endpoint);
+
+  /// \brief Add a connection to a worker positioned right (to this worker)
+  //
+  //  This means that this worker is considered as LEFT worker (by the other
+  //  worker)
   void AddRightNeighbourCommunicator(const std::string& endpoint);
 
   void SendMessage(std::unique_ptr<zmqpp::message> msg, CommunicatorId to);
   bool ReceiveMessage(std::unique_ptr<zmqpp::message>* msg,
-                      duration_ms_t timeout = std::chrono::milliseconds(5000));
+                      duration_ms_t timeout = std::chrono::milliseconds(1000));
   bool ReceiveMessage(std::unique_ptr<zmqpp::message>* msg, CommunicatorId from,
-                      duration_ms_t timeout = std::chrono::milliseconds(5000));
+                      duration_ms_t timeout = std::chrono::milliseconds(1000));
 
-  bool IsConnected(const CommunicatorId comm);
+  bool IsConnected(const CommunicatorId comm) const;
 
   bool Stop(bool wait = true, bool force = false);
 
@@ -45,33 +56,40 @@ class DistWorkerAPI {
   void HandleAppMessage();
 
   void HandleNetworkMessages();
-  void ReceiveAllMessages();
 
   void Cleanup();
 
-  bool IsValidCommunicator(std::uint8_t comm_id);
-  Communicator& GetValidCommunicator(std::uint8_t comm__id);
+  inline bool IsValidCommunicator(CommunicatorId comm_id) const;
+  inline Communicator& GetValidCommunicator(CommunicatorId comm__id) const;
   void ForEachValidCommunicator(
       std::function<void(std::unique_ptr<Communicator>&)> f);
 
   DistSharedInfo info_;
 
+  // Keep all (active) communicator
   std::array<std::unique_ptr<Communicator>,
              ToUnderlying(CommunicatorId::kCount)>
       comms_;
 
+  // Messages waiting to be handled by the communicators
+  // (and then sent over the network)
+  std::queue<MessageMiddlewareHeaderPair> mq_net_deliver_;
+  std::mutex mq_net_deliver_mtx_;
+
+  // Messages ready to be delivered to the application
   std::array<std::deque<std::unique_ptr<zmqpp::message> >,
              ToUnderlying(CommunicatorId::kCount)>
-      msg_queues_;
-  std::condition_variable msgs_cv_;
-  std::mutex msgs_cv_m;
+      app_messages_;
+  // Serialize access to app_messages_
+  std::condition_variable app_messages_cv_;
+  std::mutex app_messages_mtx_;
 
   std::unique_ptr<zmqpp::socket> parent_pipe_;  // Used by computation thread
   std::unique_ptr<zmqpp::socket> child_pipe_;   // Used by background thread
   std::string endpoint_;                        // Application endpoint
 
   std::unique_ptr<std::thread> thread_;  //  Background/Network thread
-  std::exception_ptr eptr_ = nullptr;    //  Holds the last exception
+  std::exception_ptr eptr_;              //  Holds the last exception
 
   Logger logger_;
 };
