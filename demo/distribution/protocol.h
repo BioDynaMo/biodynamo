af1f5ccc72be3147f98f435884becf7f9aecb914
Konstantinos Kanellis <kkanelli@hotmail.com>
2017-09-08 17:44:22 +0300

    Documentation for protocol header file

--- demo/distribution/protocol.h
+++ demo/distribution/protocol.h
@@ -11,11 +11,22 @@
 
 namespace bdm {
 
-const std::string PROTOCOL_CLIENT = "BDM/0.1C";
-const std::string PROTOCOL_WORKER = "BDM/0.1W";
+const std::string PROTOCOL_CLIENT = "BDM/0.1C";  //!< Client protocol version
+const std::string PROTOCOL_WORKER = "BDM/0.1W";  //!< Worker protocol version
 
+///
+/// @brief Utility class for network messages
+///
 class MessageUtil {
  public:
+  ///
+  /// @brief Pops and deserializes the first frame from a ZMQ message
+  ///
+  /// @param[in]  msg   Pointer to the ZMQ message
+  /// @tparam           The serialized object type
+  ///
+  /// @returns Unique pointer to the deserialized object
+  ///
   template <typename T>
   inline static std::unique_ptr<T> PopFrontObject(zmqpp::message* msg) {
     auto obj = MessageUtil::Deserialize<T>(msg->raw_data(0), msg->size(0));
@@ -24,6 +35,13 @@ class MessageUtil {
     return obj;
   }
 
+  ///
+  /// @brief Serializes and pushes an object at the beggining of a ZMQ message
+  ///
+  /// @param[in]  msg   Pointer to the ZMQ message
+  /// @param[in]  obj   The object to be serialized and added to the message
+  /// @tparam           Type of the object
+  ///
   template <typename T>
   inline static void PushFrontObject(zmqpp::message* msg, const T& obj) {
     size_t obj_sz;
@@ -32,6 +50,17 @@ class MessageUtil {
     msg->push_front(obj_bin.get(), obj_sz);
   }
 
+  ///
+  /// @brief  Deserializes an object from a binary string
+  ///
+  /// The binary string must have been previously serialized by ROOT I/O.
+  ///
+  /// @param[in]  data    Pointer to the binary string
+  /// @param[in]  size    Size of the binary string
+  /// @tparam             Type of the serialized object
+  ///
+  /// @returns  Unique pointer to the deserialized object
+  ///
   template <typename T>
   inline static std::unique_ptr<T> Deserialize(const void* data, size_t size) {
     TBufferFile buf(TBufferFile::EMode::kRead, size, const_cast<void*>(data),
@@ -40,12 +69,24 @@ class MessageUtil {
         reinterpret_cast<T*>(buf.ReadObjectAny(T::Class())));
   }
 
+  ///
+  /// @brief  Serializes an object to a binary string
+  ///
+  /// Serializes an object to a binary string ready for network transmission,
+  /// using the ROOT I/O framework.
+  ///
+  /// @param[in]  obj     Object to be serialized
+  /// @param[out] sz_out  Size of the resulting binary string
+  /// @tparam             Type of the object to be serialized
+  ///
+  /// @returns  Unique pointer to the serialized binary string
+  ///
   template <typename T>
   inline static std::unique_ptr<const char[]> Serialize(const T& obj,
                                                         size_t* sz_out) {
-    auto data_sz = TBuffer::kInitialSize;  //+ TBuffer::kExtraSpace;
+    auto data_sz = TBuffer::kInitialSize;
     std::unique_ptr<char[]> data(new char[data_sz]);
-    memset(data.get(), 0, data_sz);  // initialize bytes
+    memset(data.get(), 0, data_sz);  // Initialize bytes to zero
 
     TBufferFile buf(TBufferFile::EMode::kWrite, data_sz, data.get(), kFALSE,
                     TStorage::ReAllocChar);
@@ -60,6 +101,9 @@ class MessageUtil {
 // ###### Application level protocol definition ########## //
 // ------------------------------------------------------- //
 
+///
+/// @brief Application protocol commands enumerator
+///
 enum class AppProtocolCmd : std::uint8_t {
   kInvalid = 0,
   kDebug,
@@ -70,35 +114,58 @@ enum class AppProtocolCmd : std::uint8_t {
   kMinValue = kDebug,
   kMaxValue = kRequestHaloRegion,
 };
+/// @brief  String repr of the AppProtocolCmdStr enumerator
 const std::string AppProtocolCmdStr[] = {
     "[Invalid]", "[Debug]", "[RequestHaloRegion]", "[ReportHaloRegion]"};
 
+/// @brief  Overloaded stream operator for AppProtocolCmd
 inline std::ostream& operator<<(std::ostream& stream,
                                 const AppProtocolCmd& cmd) {
   stream << AppProtocolCmdStr[ToUnderlying(cmd)];
   return stream;
 }
 
+///
+/// @brief Application message header class
+///
+/// Holds information about the application level protocol
+///
 class AppMessageHeader {
  public:
+  ///
+  /// @brief  Create empty header
+  ///
   AppMessageHeader() {}
+
+  ///
+  /// @brief Create header with \p cmd command
+  ///
   explicit AppMessageHeader(AppProtocolCmd cmd) : cmd_(cmd) {}
+
+  ///
+  /// @brief Virtual destructor (needed by ROOT I/O)
+  ///
   virtual ~AppMessageHeader() {}
 
-  AppProtocolCmd cmd_;
+  AppProtocolCmd cmd_;  //!< Application command
 
+  /// @brief Overloaded stream operator for #AppMessageHeader
   inline friend std::ostream& operator<<(std::ostream& stream,
                                          const AppMessageHeader& header) {
     stream << "[Command]: " << header.cmd_ << std::endl;
     return stream;
   }
 
+  /// Directive to ROOT I/O to serialize this class
   ClassDef(AppMessageHeader, 1);
 };
 
 // ###### Middleware protocol definition ########## //
 // ------------------------------------------------------- //
 
+///
+/// @brief Client middleware protocol commands enumerator
+///
 enum class ClientProtocolCmd : std::uint8_t {
   kInvalid = 0,
   kRequest,
@@ -112,10 +179,12 @@ enum class ClientProtocolCmd : std::uint8_t {
   kMaxValue = kBrokerTerminate,
   kCount = kMaxValue
 };
+/// @brief  String repr of the #ClientProtocolCmd enumerator
 const std::string ClientProtocolCmdStr[] = {
     "[Invalid]", "[Request]",     "[Report]",     "[ACK]",
     "[NAK]",     "[CheckWorker]", "ReqTerminate]"};
 
+/// @brief  Overloaded stream operator for #ClientProtocolCmd
 inline std::ostream& operator<<(std::ostream& stream,
                                 const ClientProtocolCmd& cmd) {
   stream << ClientProtocolCmdStr[ToUnderlying(cmd)];
@@ -123,7 +192,9 @@ inline std::ostream& operator<<(std::ostream& stream,
 }
 
 // -------------------------------------------------------- //
-
+///
+/// @brief Worker middleware protocol commands enumerator
+///
 enum class WorkerProtocolCmd : std::uint8_t {
   kInvalid = 0,
   kReady,
@@ -136,15 +207,19 @@ enum class WorkerProtocolCmd : std::uint8_t {
   kMaxValue = kDisconnect,
   kCount = kMaxValue
 };
+/// @brief  String repr of the #WorkerProtocolCmd enumerator
 const std::string WorkerProtocolCmdStr[] = {"[Invalid]",   "[Ready]",
                                             "[Request]",   "[Report]",
                                             "[Heartbeat]", "[Disconnect]"};
 
+/// @brief  Overloaded stream operator for #WorkerProtocolCmd
 inline std::ostream& operator<<(std::ostream& stream,
                                 const WorkerProtocolCmd& cmd) {
   stream << WorkerProtocolCmdStr[ToUnderlying(cmd)];
   return stream;
 }
+
+// -------------------------------------------------------- //
 
 // Macro is used for named arguments/parameters
 #define optional_arg(class_, type, name, default_value) \
@@ -155,25 +230,43 @@ inline std::ostream& operator<<(std::ostream& stream,
   }                                                     \
   type name##_ = default_value
 
-// Curiously Recurring Template Pattern/Idiom (CRTP)
+///
+/// @brief  Middleware message header class
+///
+/// Holds information about the middleware level protocol. Makes use of the
+/// Curiously Recurring Template Pattern/Idiom (CRTP) to reduce code rewritting
+///
+/// @tparam Type of the derived message type
+///
 template <typename T>
 class MiddlewareMessageHeader {
  public:
+  /// @brief  Create empty header
   MiddlewareMessageHeader()
       : sender_(CommunicatorId::kUndefined),
         receiver_(CommunicatorId::kUndefined) {}
+
+  ///
+  /// @brief Create new and valid header
+  ///
+  /// @param[in]  sender    Message origin
+  /// @param[in]  receiver  Message final destination
+  ///
   MiddlewareMessageHeader(CommunicatorId sender, CommunicatorId receiver)
       : sender_(sender), receiver_(receiver) {}
+
+  /// @brief Virtual destructor (needed by ROOT I/O)
   virtual ~MiddlewareMessageHeader() {}
 
   // Required fields
-  CommunicatorId sender_;
-  CommunicatorId receiver_;
+  CommunicatorId sender_;    //!< Message origin
+  CommunicatorId receiver_;  //!< Message final destination
 
   // Optional fields
-  optional_arg(T, std::string, client_id, "");
-  optional_arg(T, std::string, worker_id, "");
+  optional_arg(T, std::string, client_id, "");  //!< Identifier of the client
+  optional_arg(T, std::string, worker_id, "");  //!< Identifier of the worker
 
+  /// @brief  Overloaded stream operator for #MiddlewareMessageHeader
   inline friend std::ostream& operator<<(
       std::ostream& stream, const MiddlewareMessageHeader& header) {
     stream << "[Sender    ]: " << header.sender_ << std::endl
@@ -184,20 +277,36 @@ class MiddlewareMessageHeader {
     return stream;
   }
 
+  /// Directive to ROOT I/O to serialize this class
   ClassDef(MiddlewareMessageHeader, 1);
 };
 
+///
+/// @brief Middleware message header used for client -- broker communication
+///
 class ClientMiddlewareMessageHeader
     : public MiddlewareMessageHeader<ClientMiddlewareMessageHeader> {
  public:
+  /// @brief  Create empty & invalid header
   ClientMiddlewareMessageHeader() : cmd_(ClientProtocolCmd::kInvalid) {}
+
+  ///
+  /// @brief Create new valid header
+  ///
+  /// @param[in]  cmd       Command of this message
+  /// @param[in]  sender    Message origin
+  /// @param[in]  receiver  Message final destination
+  ///
   ClientMiddlewareMessageHeader(ClientProtocolCmd cmd, CommunicatorId sender,
                                 CommunicatorId receiver)
       : MiddlewareMessageHeader(sender, receiver), cmd_(cmd) {}
+
+  /// @brief Virtual destructor (needed by ROOT I/O)
   virtual ~ClientMiddlewareMessageHeader() {}
 
-  ClientProtocolCmd cmd_;
+  ClientProtocolCmd cmd_;  //!< Message command for the middleware
 
+  /// @brief  Overloaded stream operator for #ClientMiddlewareMessageHeader
   inline friend std::ostream& operator<<(
       std::ostream& stream, const ClientMiddlewareMessageHeader& header) {
     stream << std::endl << "{ClientMiddlewareMessageHeader}" << std::endl;
@@ -208,20 +317,36 @@ class ClientMiddlewareMessageHeader
     return stream;
   }
 
+  /// Directive to ROOT I/O to serialize this class
   ClassDef(ClientMiddlewareMessageHeader, 1);
 };
 
+///
+/// @brief Middleware message header used for client -- broker communication
+///
 class WorkerMiddlewareMessageHeader
     : public MiddlewareMessageHeader<WorkerMiddlewareMessageHeader> {
  public:
+  /// @brief  Create empty & invalid header
   WorkerMiddlewareMessageHeader() : cmd_(WorkerProtocolCmd::kInvalid) {}
+
+  ///
+  /// @brief Create new valid header
+  ///
+  /// @param[in]  cmd       Command of this message
+  /// @param[in]  sender    Message origin
+  /// @param[in]  receiver  Message final destination
+  ///
   WorkerMiddlewareMessageHeader(WorkerProtocolCmd cmd, CommunicatorId sender,
                                 CommunicatorId receiver)
       : MiddlewareMessageHeader(sender, receiver), cmd_(cmd) {}
+
+  /// @brief Virtual destructor (needed by ROOT I/O)
   virtual ~WorkerMiddlewareMessageHeader() {}
 
-  WorkerProtocolCmd cmd_;
+  WorkerProtocolCmd cmd_;  //!< Message command for the middleware
 
+  /// @brief  Overloaded stream operator for #WorkerMiddlewareMessageHeader
   inline friend std::ostream& operator<<(
       std::ostream& stream, const WorkerMiddlewareMessageHeader& header) {
     stream << std::endl << "{WorkerMiddlewareMessageHeader}" << std::endl;
@@ -232,6 +357,7 @@ class WorkerMiddlewareMessageHeader
     return stream;
   }
 
+  /// Directive to ROOT I/O to serialize this class
   ClassDef(WorkerMiddlewareMessageHeader, 1);
 };
 }  // namespace bdm
