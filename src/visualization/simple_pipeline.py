4acfb40375b2b4769142787f3b2ee9a0bb9f98d3
Ahmad Hesam <hesam212@gmail.com>
2017-09-15 16:53:47 +0200

    Added possibility to write to file for post-simulation visualization
    Writing the visualization state to file can be done by setting the
    `write_to_file` flag to True. The frequency can be set by setting
    `outputfrequency` to the desired value.

--- src/visualization/simple_pipeline.py
+++ src/visualization/simple_pipeline.py
@@ -5,6 +5,11 @@ from paraview import coprocessing
 #Code generated from cpstate.py to create the CoProcessor.
 #ParaView 5.3.0 64 bits
 
+# the frequency to output everything
+outputfrequency = 1
+# flag to write to file or not
+write_to_file = False
+
 #CoProcessor definition
 def CreateCoProcessor():
     def _CreatePipeline(coprocessor, datadescription):
@@ -16,21 +21,28 @@ def CreateCoProcessor():
             #disable automatic camera reset on 'Show'
             paraview.simple._DisableFirstRenderCameraReset()
 
-            #create a new 'XML Unstructured Grid Reader'
             #create a producer from a simulation input
             diffusion_grid_data = coprocessor.CreateProducer(datadescription, 'diffusion_grid_data')
             cells_data = coprocessor.CreateProducer(datadescription, 'cells_data')
 
             #create a new 'Glyph'
             #glyph1 = Glyph(Input = propDiameterFilter, GlyphType = 'Sphere')
-            glyph1 = Glyph(guiName= '* Cells', Input= cells_data, GlyphType = 'Sphere')
+            glyph1 = Glyph(guiName= 'Cells', Input= cells_data, GlyphType = 'Sphere')
             glyph1.Scalars = [ 'POINTS', 'Cell Diameters' ]
             glyph1.Vectors = [ 'POINTS', 'None' ]
             glyph1.ScaleMode = 'scalar'
             glyph1.GlyphMode = 'All Points'
             glyph1.GlyphTransform = 'Transform2'
 
-            #finally, restore active source
+            timestep = datadescription.GetTimeStep()
+
+            if write_to_file == True:
+                diffusion_grid_writer = servermanager.writers.XMLPImageDataWriter(Input=diffusion_grid_data)
+                diffusion_cell_Writer = servermanager.writers.XMLPUnstructuredGridWriter(Input=cells_data)
+
+                coprocessor.RegisterWriter(diffusion_grid_writer, "diffusion_grid_%t.pvti", freq=outputfrequency)
+                coprocessor.RegisterWriter(diffusion_cell_Writer, "diffusion_cells_%t.pvtu", freq=outputfrequency)
+
             SetActiveSource(glyph1)
 
         return Pipeline()
@@ -59,18 +71,25 @@ coprocessor.EnableLiveVisualization(True, 1)
 
 def RequestDataDescription(datadescription):
     "Callback to populate the request for current timestep"
+    timestep = datadescription.GetTimeStep()
+    
+    datadescription.GetInputDescriptionByName('diffusion_grid_data').AllFieldsOn()
+    datadescription.GetInputDescriptionByName('cells_data').AllFieldsOn()
+    datadescription.GetInputDescriptionByName('diffusion_grid_data').GenerateMeshOn()
+    datadescription.GetInputDescriptionByName('cells_data').GenerateMeshOn()
+    
     global coprocessor
 
 
     if datadescription.GetForceOutput() == True:
-#We are just going to request all fields and meshes from the simulation code / adaptor.
+        #We are just going to request all fields and meshes from the simulation code / adaptor.
         for i in range(datadescription.GetNumberOfInputDescriptions()):
             datadescription.GetInputDescription(i).AllFieldsOn()
             datadescription.GetInputDescription(i).GenerateMeshOn()
         return
 
-#setup requests for all inputs based on the requirements of the
-#pipeline.
+    #setup requests for all inputs based on the requirements of the
+    #pipeline.
     coprocessor.LoadRequestedData(datadescription)
 
 #--------------------------------- Processing method ----------------------------------
