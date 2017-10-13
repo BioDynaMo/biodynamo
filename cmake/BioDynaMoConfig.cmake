1694242fbd07792bcfc375f0cfa6391960a3a59c
Lukas Johannes Breitwieser <lukas.johannes.breitwieser@cern.ch>
2017-10-13 22:35:54 +0200

    Add BioDynaMoConfig.cmake to support find_package(BioDynaMo)

---
+++ cmake/BioDynaMoConfig.cmake
@@ -0,0 +1,33 @@
+# BioDynaMo configuration file for external projects
+#
+# Script defines the following variables
+#   BioDynaMo_FOUND      - set to true of BioDynaMo has been found
+#   BioDynaMo_USE_FILE   - cmake script that will setup the build
+#
+# If an external project can't locate this file emitting the following error
+# message:
+#      Could not find a package configuration file provided by "BioDynaMo" with
+#        any of the following names:
+#
+#          BioDynaMoConfig.cmake
+#          biodynamo-config.cmake
+#
+# make sure the folder containing this file is added to the PATH environment
+# variable or use`cmake -DCMAKE_FIND_DEBUG_MODE=ON ..` to debug the issue.
+
+find_file(BioDynaMo_USE_FILE
+          UseBioDynaMo.cmake
+          PATHS "/usr/share/biodynamo/cmake"
+          ENV "BDM_CMAKE_DIR")
+
+if(NOT BioDynaMo_USE_FILE AND BioDynaMo_FIND_REQUIRED)
+  message(FATAL_ERROR "BioDynaMo not found! Try to set BDM_CMAKE_DIR environment variable \
+                       pointing to the directory that contains the file UseBioDynaMo.cmake \
+                       or add -DBDM_CMAKE_DIR=... to the cmake command")
+elseif(NOT BioDynaMo_USE_FILE AND NOT BioDynaMo_FIND_QUIETLY)
+  message(WARNING "BioDynaMo not found! Try to set BDM_CMAKE_DIR environment variable \
+                   pointing to the directory that contains the file UseBioDynaMo.cmake \
+                   or add -DBDM_CMAKE_DIR=... to the cmake command")
+endif()
+
+set(BioDynaMo_FOUND TRUE)
--- cmake/Installation.cmake
+++ cmake/Installation.cmake
@@ -1,18 +1,20 @@
 # This file contains configuration for the install step
 
 # set install directories
-set(CMAKE_INSTALL_BINDIR      "bin"                            CACHE PATH "user executables (bin)")
-set(CMAKE_INSTALL_INCLUDEDIR  "include"                        CACHE PATH "C/C++ header files (include)")
-set(CMAKE_INSTALL_LIBDIR      "lib"                            CACHE PATH "object code libraries (lib)")
-set(CMAKE_INSTALL_DATADIR     "share"                          CACHE PATH "read-only architecture-independent data (share)")
-set(CMAKE_INSTALL_CMAKEDIR    "${CMAKE_INSTALL_DATADIR}/cmake" CACHE PATH "Build related files (DATADIR/cmake)")
+set(CMAKE_INSTALL_BINDIR        "bin"                            CACHE PATH "User executables (bin)")
+set(CMAKE_INSTALL_INCLUDEDIR    "include"                        CACHE PATH "C/C++ header files (include)")
+set(CMAKE_INSTALL_LIBDIR        "lib"                            CACHE PATH "Object code libraries (lib)")
+set(CMAKE_INSTALL_CMAKEDIR      "cmake"                          CACHE PATH "CMake files required from external projects")
+set(CMAKE_INSTALL_DATADIR       "share"                          CACHE PATH "Read-only architecture-independent data (share)")
+set(CMAKE_INSTALL_CMAKEDATADIR  "${CMAKE_INSTALL_DATADIR}/cmake" CACHE PATH "Build related files (DATADIR/cmake)")
 
 # hide them from configuration tools
 mark_as_advanced(${CMAKE_INSTALL_BINDIR}
                  ${CMAKE_INSTALL_INCLUDEDIR}
                  ${CMAKE_INSTALL_LIBDIR}
+                 ${CMAKE_INSTALL_CMAKEDIR}
                  ${CMAKE_INSTALL_DATADIR}
-                 ${CMAKE_INSTALL_CMAKEDIR})
+                 ${CMAKE_INSTALL_CMAKEDATADIR})
 
 # libbiodynamo.so
 install(TARGETS biodynamo LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR})
@@ -26,7 +28,9 @@ install(FILES third_party/cpp_magic.h DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
 install(FILES third_party/OptionParser.h DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
 # build files
 file(GLOB SELECTION_FILES cmake/*.xml)
-install(FILES ${SELECTION_FILES} DESTINATION ${CMAKE_INSTALL_CMAKEDIR})
-install(FILES cmake/SetCompilerFlags.cmake DESTINATION ${CMAKE_INSTALL_CMAKEDIR})
-install(FILES cmake/BioDynaMo.cmake DESTINATION ${CMAKE_INSTALL_CMAKEDIR})
-install(FILES ${CMAKE_BINARY_DIR}/UseBioDynaMo.cmake DESTINATION ${CMAKE_INSTALL_CMAKEDIR})
+install(FILES ${SELECTION_FILES} DESTINATION ${CMAKE_INSTALL_CMAKEDATADIR})
+install(FILES cmake/SetCompilerFlags.cmake DESTINATION ${CMAKE_INSTALL_CMAKEDATADIR})
+install(FILES cmake/BioDynaMo.cmake DESTINATION ${CMAKE_INSTALL_CMAKEDATADIR})
+install(FILES ${CMAKE_BINARY_DIR}/UseBioDynaMo.cmake DESTINATION ${CMAKE_INSTALL_CMAKEDATADIR})
+# CMake files required from external projects
+install(FILES cmake/BioDynaMoConfig.cmake DESTINATION ${CMAKE_INSTALL_CMAKEDIR})
