220f2de2f4299cd7cd65e0e97b956a53c277a83d
Lukas Johannes Breitwieser <lukas.johannes.breitwieser@cern.ch>
2017-10-21 17:58:00 +0200

    Add make target (snap-package) to build snap package

---
+++ cmake/build_snap_package.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# OPTIONS:
+#   $1 install directory
+
+if [[ $# -ne 1 ]]; then
+  echo "This script requires one argument: "
+  echo "OPTIONS: "
+  echo "  $1 install directory"
+  exit
+fi
+
+make install
+mkdir build-snap && cd build-snap
+mv ../snapcraft.yaml .
+# create run command
+mkdir bin
+echo '#!/bin/bash' > bin/run
+echo '# execute command given as first parameter' >> bin/run
+echo '$1' >> bin/run
+chmod +x bin/run
+
+sudo docker pull snapcore/snapcraft
+echo "Start building snap package"
+sudo docker run --net=host -v $PWD:$PWD -v $1:$1 -v /snap/core/:/snap/core/ -w $PWD snapcore/snapcraft snapcraft
