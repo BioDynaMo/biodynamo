73f8bba593cb2f1cd804dfa92712e3b4c0f025fa
Lukas Johannes Breitwieser <lukas.johannes.breitwieser@cern.ch>
2017-10-26 15:30:26 +0200

    Finish bdm cli new command and improve help messages

--- cli/biodynamo.py
+++ cli/biodynamo.py
@@ -1,31 +1,55 @@
 #!/usr/bin/env python3
 
-# from optparse import OptionParser
 import argparse
-# import new_command
 from new_command import NewCommand
 from build_command import BuildCommand
 from run_command import RunCommand
 from assist_command import AssistCommand
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(prog='biodynamo')
-	sp = parser.add_subparsers(dest='cmd')
-	for cmd in ['build', 'clean', 'run', 'assist']:
-		sp.add_parser(cmd)
-	for cmd in ['new']:
-		spp = sp.add_parser(cmd)
-		spp.add_argument('SIMULATION_NAME', type=str)
+    parser = argparse.ArgumentParser(prog='biodynamo',
+        description='This is the BioDynaMo command line interface. It guides '
+                    'you during the whole simulation workflow. From starting '
+                    'a new project, to compiling and executing, all the way '
+                    'to requesting assistance from the BioDynaMo developers.',
+        epilog='')
 
-	args = parser.parse_args()
+    sp = parser.add_subparsers(dest='cmd')
 
-	if args.cmd == 'new':
-		NewCommand(args.SIMULATION_NAME)
-	if args.cmd == 'build':
-		BuildCommand()
-	if args.cmd == 'clean':
-		BuildCommand(clean=True, build=False)
-	if args.cmd == 'run':
-		RunCommand()
-	if args.cmd == 'assist':
-		AssistCommand()
+    sp.add_parser('assist', help='Use this command if you need help from the '
+                                 'BiodynaMo developers. This command helps you '
+                                 'to gather information which is required to '
+                                 'reproduce and debug your issue. First, '
+                                 'it will ask you to commit all uncommited changes. '
+                                 'Afterwards, it will attempt to build and run your '
+                                 'simulation. During this process it will collect '
+                                 'logs which will be commited and uploaded in a '
+                                 'seperate git branch. In the end it will output '
+                                 'a link that you should add to your e-mail or '
+                                 'slack message when you describe your issue.' )
+
+    sp.add_parser('build', help='Builds the simulation binary')
+
+    sp.add_parser('clean', help='Removes all build files')
+
+    spp = sp.add_parser('new', help='Creates a new simulation project. Downloads '
+    'a template project from BioDynaMo, renames it to the given simulation name, '
+    'creates a new Github repository and configures git.')
+    spp.add_argument('SIMULATION_NAME', type=str, help='simulation name help')
+
+    sp.add_parser('run', help='Executes the simulation')
+
+    args = parser.parse_args()
+
+    if args.cmd == 'new':
+    	NewCommand(args.SIMULATION_NAME)
+    elif args.cmd == 'build':
+    	BuildCommand()
+    elif args.cmd == 'clean':
+    	BuildCommand(clean=True, build=False)
+    elif args.cmd == 'run':
+    	RunCommand()
+    elif args.cmd == 'assist':
+    	AssistCommand()
+    else:
+        parser.print_help()
--- cli/new_command.py
+++ cli/new_command.py
@@ -4,24 +4,57 @@ import re
 import requests
 import subprocess as sp
 import sys
+import time
+
+class Print:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
+    @staticmethod
+    def success(message):
+       print(Print.BOLD + Print.GREEN + str(message) + Print.END)
+
+    @staticmethod
+    def error(message):
+        print(Print.RED + str(message) + Print.END)
+
+    @staticmethod
+    def new_step(message):
+       print('\n' + Print.BOLD + Print.BLUE + str(message) + Print.END)
+
 
 def ValidateSimName(sim_name):
-	pattern = re.compile("^[a-zA-Z]+[a-zA-Z0-9\-_]+$")
-	if not pattern.match(sim_name):
-		print("Error: simulation name '{0}' is not valid.".format(sim_name))
-		print("       Allowed characters are a-z A-Z 0-9 - and _")
-		print("       Must start with a-z or A-Z")
-		sys.exit(1)
+    pattern = re.compile("^[a-zA-Z]+[a-zA-Z0-9\-_]+$")
+    if not pattern.match(sim_name):
+        Print.error("Error: simulation name '{0}' is not valid.".format(sim_name))
+        Print.error("       Allowed characters are a-z A-Z 0-9 - and _")
+        Print.error("       Must start with a-z or A-Z")
+        sys.exit(1)
 
-def CloneTemplateRepository(sim_name):
-	try:
-		sp.check_output(["git", "clone", "https://github.com/BioDynaMo/simulation-templates.git", sim_name])
-	except sp.CalledProcessError as err:
-		print("Error while checking out the template project from BioDynaMo git repository")
-		sys.exit(1)
+## Removes any created files during NewCommand and exits the program
+def CleanupOnError(sim_name):
+    try:
+        sp.check_output(["rm", "-rf", sim_name])
+    except:
+        Print.error("Error: Failed to remove folder {0}".format(sim_name))
+    sys.exit(1)
 
-def RemoveGitFolder(sim_name):
-	sp.check_output(["rm", "-rf", sim_name+"/.git"])
+def DownloadTemplateRepository(sim_name):
+    Print.new_step("Download template repository")
+    try:
+        sp.check_output(["git", "clone", "https://github.com/BioDynaMo/simulation-templates.git", sim_name])
+        sp.check_output(["rm", "-rf", sim_name+"/.git"])
+    except sp.CalledProcessError as err:
+        Print.error("Error while downloading the template project from BioDynaMo")
+        CleanupOnError(sim_name)
 
 def ModifyFileContent(filename, fn):
 	with open(filename) as f:
@@ -33,83 +66,87 @@ def ModifyFileContent(filename, fn):
 		f.write(content)
 
 def CustomizeFiles(sim_name):
-	# README.md
-	ModifyFileContent(sim_name + "/README.md", lambda content: "# " + sim_name + "\n")
+    Print.new_step("Customize files")
+    try:
+        # README.md
+        ModifyFileContent(sim_name + "/README.md", lambda content: "# " + sim_name + "\n")
 
-	# CMakelists.txt
-	ModifyFileContent(sim_name + "/CMakeLists.txt", lambda content: content.replace("my-simulation", sim_name))
+        # CMakelists.txt
+        ModifyFileContent(sim_name + "/CMakeLists.txt", lambda content: content.replace("my-simulation", sim_name))
 
-	# source files
-	include_guard = sim_name.upper().replace("-", "_") + "_H_"
-	ModifyFileContent(sim_name + "/src/my-simulation.h", lambda c: c.replace("MY_SIMULATION_H_", include_guard))
-	ModifyFileContent(sim_name + "/src/my-simulation.cc", lambda c: c.replace("my-simulation", sim_name))
-	#   rename
-	os.rename(sim_name + "/src/my-simulation.h", sim_name + "/src/" + sim_name + ".h")
-	os.rename(sim_name + "/src/my-simulation.cc", sim_name + "/src/" + sim_name + ".cc")
+        # source files
+        include_guard = sim_name.upper().replace("-", "_") + "_H_"
+        ModifyFileContent(sim_name + "/src/my-simulation.h", lambda c: c.replace("MY_SIMULATION_H_", include_guard))
+        ModifyFileContent(sim_name + "/src/my-simulation.cc", lambda c: c.replace("my-simulation", sim_name))
+        #   rename
+        os.rename(sim_name + "/src/my-simulation.h", sim_name + "/src/" + sim_name + ".h")
+        os.rename(sim_name + "/src/my-simulation.cc", sim_name + "/src/" + sim_name + ".cc")
+    except:
+        Print.error("Error: File customizations failed")
+        CleanupOnError(sim_name)
 
 def InitializeNewGitRepo(sim_name):
-	sp.check_output(["git", "init"], cwd=sim_name)
+    Print.new_step("Initialize new git repository")
+    sp.check_output(["git", "init"], cwd=sim_name)
 
-	# check if user name and email are set
-	try:
-		out = sp.check_output(["git", "config", "user.name"], cwd=sim_name).decode("utf-8")
-	except sp.CalledProcessError as err:
-		# User name not set
-		print("Git user name is not set.")
-		response = input("Please enter your name: ")
-		out = sp.check_output(["git", "config", "user.name", response], cwd=sim_name)
+    # check if user name and email are set
+    try:
+        out = sp.check_output(["git", "config", "user.name"], cwd=sim_name).decode("utf-8")
+    except sp.CalledProcessError as err:
+        # User name not set
+        print("Your git user name is not set.")
+        response = input("Please enter your name (e.g. Mona Lisa): ")
+        out = sp.check_output(["git", "config", "user.name", response], cwd=sim_name)
 
-	try:
-		out = sp.check_output(["git", "config", "user.email"], cwd=sim_name).decode("utf-8")
-	except sp.CalledProcessError as err:
-		# User name not set
-		print("Git user e-mail is not set.")
-		response = input("Please enter your e-mail: ")
-		out = sp.check_output(["git", "config", "user.email", response], cwd=sim_name)
+    try:
+        out = sp.check_output(["git", "config", "user.email"], cwd=sim_name).decode("utf-8")
+    except sp.CalledProcessError as err:
+        # User name not set
+        print("Your git user e-mail is not set.")
+        response = input("Please enter your e-mail address that you have used to sign-up for github: ")
+        out = sp.check_output(["git", "config", "user.email", response], cwd=sim_name)
 
-	sp.check_output(["git", "add", "."], cwd=sim_name)
-	sp.check_output(["git", "commit", "-m", "\"Initial commit\""], cwd=sim_name)
+    sp.check_output(["git", "add", "."], cwd=sim_name)
+    sp.check_output(["git", "commit", "-m", "\"Initial commit\""], cwd=sim_name)
 
 def CreateNewGithubRepository(sim_name):
-	gh_user = input("Username for 'https://github.com': ")
-	repo_url = "https://github.com/" + gh_user + "/" + sim_name + ".git"
-	try:
-		gh_pass = getpass.getpass("Password for 'https://" + gh_user + "@github.com':")
-		url     = 'https://api.github.com/user/repos'
-		r = requests.post(url, auth=(gh_user, gh_pass), data='{"name":"' + sim_name + '", "description": "Simulation powered by BioDynaMo"}')
-		gh_pass = "123456789abcdefgh"
-		if r.status_code != 201:
-			print("Github repository creation failed.")
-			print(r.status_code)
-			print(r.text)
-			sys.exit(1)
-	except sp.CalledProcessError as err:
-		print("Github repository creation failed.")
-		print(err)
-		sys.exit(1)
+    Print.new_step("Create Github repository")
+    gh_user = input("Please enter your Github username: ")
+    gh_pass = getpass.getpass("Please enter your Github password: ")
 
-	try:
-		sp.check_output(["git", "remote", "add", "origin", repo_url], cwd=sim_name)
-	except sp.CalledProcessError as err:
-		print("Setting remote to github url ({0}) failed.".format(repo_url))
-		sys.exit(1)
+    # create new github repo
+    try:
+        url = 'https://api.github.com/user/repos'
+        r = requests.post(url, auth=(gh_user, gh_pass), data='{"name":"' + sim_name + '", "description": "Simulation powered by BioDynaMo"}')
+        if r.status_code != 201:
+            Print.error("Github repository creation failed.")
+            Print.error(r.status_code)
+            Print.error(r.text)
+            CleanupOnError(sim_name)
+    except sp.CalledProcessError as err:
+        Print.error("Github repository creation failed.")
+        Print.error(err)
+        CleanupOnError(sim_name)
 
-def PushChanges(sim_name):
-	sp.check_output(["git", "push", "origin", "master"], cwd=sim_name)
+    # Connect github repository with local
+    try:
+        repo_url = "https://github.com/" + gh_user + "/" + sim_name + ".git"
+        sp.check_output(["git", "remote", "add", "origin", repo_url], cwd=sim_name)
+    except sp.CalledProcessError as err:
+        Print.error("Error: Setting remote github url ({0}) failed.".format(repo_url))
+        CleanupOnError(sim_name)
 
 def NewCommand(sim_name):
-	print("new " + sim_name)
-	# TODO Message that user needs an github account
+    print("Info: This command requires a Github.com account.")
+    print("      Please have your account details ready, or ")
+    print("      go over to https://github.com/join to sign up.")
 
-	ValidateSimName(sim_name)
-	CloneTemplateRepository(sim_name)
-	RemoveGitFolder(sim_name)
-	CustomizeFiles(sim_name)
-	InitializeNewGitRepo(sim_name)
-	CreateNewGithubRepository(sim_name)
-	PushChanges(sim_name)
+    ValidateSimName(sim_name)
+    DownloadTemplateRepository(sim_name)
+    CustomizeFiles(sim_name)
+    InitializeNewGitRepo(sim_name)
+    CreateNewGithubRepository(sim_name)
 
-	print(sim_name + " has been created successfully!")
-	print('Stepping into the directory "%s"' % (sim_name))
-	sp.check_output(["cd", sim_name])
-
+    Print.success(sim_name + " has been created successfully!")
+    print('Stepping into the directory "%s"' % (sim_name))
+    sp.check_output(["cd", sim_name])
