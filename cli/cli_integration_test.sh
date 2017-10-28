191d7dd857ab88af5735a0df7dd21cd24b3866ac
Lukas Johannes Breitwieser <lukas.johannes.breitwieser@cern.ch>
2017-10-28 21:26:23 +0200

    Add simulation parent directory as config file search path

--- cli/cli_integration_test.sh
+++ cli/cli_integration_test.sh
@@ -8,7 +8,7 @@ cd test-sim
 biodynamo build
 biodynamo run >actual 2>&1
 
-echo "Warning in <InitializeBioDynamo>: Config file bdm.toml not found." > expected
+echo "Warning in <InitializeBioDynamo>: Config file bdm.toml not found in `.` or `../` directory." > expected
 echo "Warning: No backup file name given. No backups will be made!" >> expected
 echo "Your simulation objects are getting near the edge of the simulation space. Be aware of boundary conditions that may come into play!" >> expected
 echo "Your simulation objects are getting near the edge of the simulation space. Be aware of boundary conditions that may come into play!" >> expected
--- src/biodynamo.cc
+++ src/biodynamo.cc
@@ -7,11 +7,16 @@ namespace bdm {
 void InitializeBioDynamo(int argc, const char** argv) {
   auto options = bdm::DefaultSimulationOptionParser(argc, argv);
   constexpr auto kConfigFile = "bdm.toml";
+  constexpr auto kConfigFileParentDir = "../bdm.toml";
   if (FileExists(kConfigFile)) {
     auto config = cpptoml::parse_file(kConfigFile);
     Param::AssignFromConfig(config);
+  } else if (FileExists(kConfigFileParentDir)) {
+    auto config = cpptoml::parse_file(kConfigFileParentDir);
+    Param::AssignFromConfig(config);
   } else {
-    Warning("InitializeBioDynamo", "Config file %s not found.", kConfigFile);
+    Warning("InitializeBioDynamo",
+            "Config file %s not found in `.` or `../` directory.", kConfigFile);
   }
   if (options.backup_file_ != "") {
     Param::backup_file_ = options.backup_file_;
