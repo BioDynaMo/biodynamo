6d4147dc2efcf1b7b1679accadbfb40a8c30be18
Lukas Johannes Breitwieser <lukas.johannes.breitwieser@cern.ch>
2017-11-29 15:10:44 +0100

    Measure total runtime of compiled cell division module

--- demo/cell_division_module.cc
+++ demo/cell_division_module.cc
@@ -1,3 +1,12 @@
 #include "cell_division_module.h"
 
-int main(int argc, const char** argv) { return bdm::Simulate(argc, argv); }
+int main(int argc, const char** argv) {
+  int ret_code = 0;
+  bdm::Param::output_op_runtime_ = true;
+  {
+    bdm::Timing simulation("simulate");
+    ret_code =  bdm::Simulate(argc, argv);
+  }
+
+  return ret_code;
+}
