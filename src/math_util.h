cc06f4adfc32a72820d23ec9e272dec2b7c21a9c
Ahmad Hesam <hesam212@gmail.com>
2017-12-20 16:06:07 +0100

    Fix normalization mistake
    Need to take square root after dot product of vector with itself.

--- src/math_util.h
+++ src/math_util.h
@@ -3,6 +3,8 @@
 
 #include <array>
 #include <cmath>
+#include <numeric>
+#include <vector>
 
 namespace bdm {
 
@@ -11,7 +13,7 @@ struct Math {
   static constexpr double kPi = 3.141592653589793238462643383279502884;
 
   static double Norm(const std::array<double, 3>& a) {
-    double norm = a[0] * a[0] + a[1] * a[1] + a[2] * a[2];
+    double norm = std::sqrt(a[0] * a[0] + a[1] * a[1] + a[2] * a[2]);
     if (norm == 0.0) {
       norm = 1.0;
     }
@@ -34,8 +36,7 @@ struct Math {
     dist_array[0] = pos2[0] - pos1[0];
     dist_array[1] = pos2[1] - pos1[1];
     dist_array[2] = pos2[2] - pos1[2];
-    double l2_norm = Norm(dist_array);
-    return sqrt(l2_norm);
+    return Norm(dist_array);
   }
 };
 
--- test/unit/math_util_test.cc
+++ test/unit/math_util_test.cc
@@ -9,7 +9,7 @@ TEST(MathUtilTest, Norm) {
   std::array<double, 3> vector = {1.1, 2.2, 3.3};
   auto result = Math::Norm(vector);
 
-  EXPECT_NEAR(16.94, result, abs_error<double>::value);
+  EXPECT_NEAR(4.115823125451335, result, abs_error<double>::value);
 }
 
 TEST(MathUtilTest, NormZero) {
@@ -32,9 +32,9 @@ TEST(MathUtilTest, Normalize) {
   std::array<double, 3> vector = {1.1, 2.2, 3.3};
   auto result = Math::Normalize(vector);
 
-  EXPECT_NEAR(0.0649350649350649351, result[0], abs_error<double>::value);
-  EXPECT_NEAR(0.1298701298701298701, result[1], abs_error<double>::value);
-  EXPECT_NEAR(0.1948051948051948052, result[2], abs_error<double>::value);
+  EXPECT_NEAR(0.2672612419124244187, result[0], abs_error<double>::value);
+  EXPECT_NEAR(0.5345224838248488374, result[1], abs_error<double>::value);
+  EXPECT_NEAR(0.8017837257372732561, result[2], abs_error<double>::value);
 }
 
 TEST(MathUtilTest, L2Distance) {
