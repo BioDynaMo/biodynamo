8c3354bd8b9a2c01f4ffe381ec55a9927535e279
Lukas Johannes Breitwieser <lukas.johannes.breitwieser@cern.ch>
2018-01-16 14:21:54 +0100

    WIP

--- src/neuroscience/neuron.h
+++ src/neuroscience/neuron.h
@@ -1,6 +1,7 @@
 #ifndef NEUROSCIENCE_NEURON_H_
 #define NEUROSCIENCE_NEURON_H_
 
+#include <unordered_map>
 #include <typeinfo>  // TODO remove
 #include "cell.h"
 #include "simulation_object_util.h"
@@ -10,18 +11,11 @@ namespace bdm {
 namespace neuroscience {
 
 BDM_SIM_OBJECT(Neuron, Cell) {
-  BDM_SIM_OBJECT_HEADER(NeuronExt, 1, daughters_, foo_);
+  BDM_SIM_OBJECT_HEADER(NeuronExt, 1, daughters_, daughters_coord_, foo_);
 
  public:
   using TNeurite = typename TCompileTimeParam::TNeurite;
   using TNeuriteSoPtr = ToSoPtr<TNeurite>;
-
-  // TODO move to header macro
-  //
-  template <typename TResourceManager = ResourceManager<>>
-  TResourceManager* Rm() {
-    return TResourceManager::Get();
-  }
 
   NeuronExt() {
     std::cout << typeid(MostDerived).name() << std::endl;
@@ -41,11 +35,24 @@ BDM_SIM_OBJECT(Neuron, Cell) {
 
   void RemoveDaughter(const ToSoPtr<TNeurite> daughter);
 
+  /// Returns the absolute coordinates of the location where the dauther is
+  /// attached.
+  /// @param daughter_element_idx element_idx of the daughter
+  /// @return the coord
+  std::array<double, 3> OriginOf(uint32_t daughter_element_idx) const;
+
  private:
   // vec<SoPointer<typename ToBackend<TNeurite, SimBackend>::type, SimBackend>>
   // daughters_;
   // vec<SoPointer<ToBackend<TNeurite, SimBackend>, SimBackend>> daughters_;
   vec<std::vector<ToSoPtr<TNeurite>>> daughters_ = {{}};
+
+  /// Daughter attachment points in local coordinates
+  /// Key: element index of neurite segement
+  /// Value: position
+  /// FIXME update if elemement index changes.
+  vec<std::unordered_map<uint32_t, std::array<double, 3>>> daughters_coord_ = {{}};
+
   // TNeurite* bar_;
 
   // vec<SoPointer<typename TNeurite::template Self<SimBackend>, SimBackend>> aa_;
@@ -68,10 +75,11 @@ BDM_SO_DEFINE(inline void NeuronExt)::RemoveDaughter(const ToSoPtr<typename TCom
 BDM_SO_DEFINE(inline typename NeuronExt<TCompileTimeParam, TDerived, TBase>::TNeuriteSoPtr NeuronExt)::ExtendNewNeurite(const std::array<double, 3>& direction) {
   auto dir = Matrix::Add(direction, Base::position_[kIdx]);
   auto angles = Base::TransformCoordinatesGlobalToPolar(dir);
-  return ExtendNewNeurite(Base::diameter_[kIdx], angles[1], angles[2]);
+  return ExtendNewNeurite(Param::kNeuriteDefaultDiameter, angles[1], angles[2]);
 }
 
 BDM_SO_DEFINE(inline typename NeuronExt<TCompileTimeParam, TDerived, TBase>::TNeuriteSoPtr NeuronExt)::ExtendNewNeurite(double diameter, double phi, double theta) {
+  std::cout << "ExtendNewNeurite called with: " << diameter << ", " << phi << ", " << theta << std::endl;
   auto neurite = Rm()->template New<TNeurite>();
   // TODO copy biological modules
   // for (auto module : local_biology_modules_) {
@@ -93,6 +101,7 @@ BDM_SO_DEFINE(inline typename NeuronExt<TCompileTimeParam, TDerived, TBase>::TNe
       + z_coord * Base::kZAxis[2] };
 
   // positions & axis in cartesian coord
+  // TODO rename variables
   auto new_cyl_begin_location = Matrix::Add(Base::position_[kIdx], Matrix::ScalarMult(radius, axis_direction));
   auto new_cyl_spring_axis = Matrix::ScalarMult(new_length, axis_direction);
 
@@ -102,8 +111,16 @@ BDM_SO_DEFINE(inline typename NeuronExt<TCompileTimeParam, TDerived, TBase>::TNe
 
   // set attributes of new neurite segment
   neurite.SetDiameter(diameter);
+  neurite.UpdateVolume();
   neurite.SetSpringAxis(new_cyl_spring_axis);
-  neurite.SetPosition(new_position);
+  auto& foo = neurite.GetSpringAxis();
+  std::cout << "nspa: " << foo[0] << ", " << foo[1] << ", " << foo[2] << std::endl;
+  std::cout << "ncml: " << new_position[0] << ", " << new_position[1] << ", " << new_position[2] << std::endl;
+  std::cout << "ml: " << Base::position_[kIdx][0] << ", " << Base::position_[kIdx][1] << ", " << Base::position_[kIdx][2] << std::endl;
+  std::cout << "ncbl: " << new_cyl_begin_location[0] << ", " << new_cyl_begin_location[1] << ", " << new_cyl_begin_location[2] << std::endl;
+
+  // neurite.SetPosition(new_position);
+  neurite.SetMassLocation(new_position); // TODO rename variable
   neurite.SetActualLength(new_length);
   neurite.SetRestingLengthForDesiredTension(Param::kNeuriteDefaultTension);
   neurite.UpdateLocalCoordinateAxis();
@@ -112,7 +129,7 @@ BDM_SO_DEFINE(inline typename NeuronExt<TCompileTimeParam, TDerived, TBase>::TNe
   auto neurite_soptr = neurite.GetSoPtr();
   daughters_[kIdx].push_back(neurite_soptr);
   neurite.SetMother(GetSoPtr());
-  // TODO daughters_coord_[cyl.get()] = {x_coord, y_coord, z_coord};
+  daughters_coord_[kIdx][neurite.GetElementIdx()] = {x_coord, y_coord, z_coord};
 
   // SpaceNode
   // TODO can we remove that?
@@ -123,7 +140,21 @@ BDM_SO_DEFINE(inline typename NeuronExt<TCompileTimeParam, TDerived, TBase>::TNe
   return neurite_soptr;
 }
 
+BDM_SO_DEFINE(inline std::array<double, 3> NeuronExt)::OriginOf(uint32_t daughter_element_idx) const {
+  std::array<double, 3> xyz = daughters_coord_[kIdx][daughter_element_idx];
 
+  double radius = Base::diameter_[kIdx] * .5;
+  xyz = Matrix::ScalarMult(radius, xyz);
+
+  const auto& pos = Base::position_[kIdx];
+
+
+  return {
+    pos[0] + xyz[0] * Base::kXAxis[0] + xyz[1] * Base::kYAxis[0] + xyz[2] * Base::kZAxis[0],
+    pos[1] + xyz[0] * Base::kXAxis[1] + xyz[1] * Base::kYAxis[1] + xyz[2] * Base::kZAxis[1],
+    pos[2] + xyz[0] * Base::kXAxis[2] + xyz[1] * Base::kYAxis[2] + xyz[2] * Base::kZAxis[2]
+  };
+}
 
 
 BDM_SIM_OBJECT(SpecializedNeuron, Neuron) {
