7a04b36e0bc97ca399989f9dc999711dd74a5745
Sadyksaj <Sadyk.sajfullin64@gmail.com>
2018-02-14 15:53:35 +0300

    obsolete code removed

--- demo/cell_gene_module.h
+++ demo/cell_gene_module.h
@@ -12,45 +12,9 @@ namespace bdm {
   //initial values for list of proteins
   const vector<array<double, Param::protein_amount>> init_vals = {{12.0, 1.7, 3.4}};
 
-  // BDM_SIM_OBJECT(GeneCell, Cell) {
-  //   BDM_SIM_OBJECT_HEADER(GeneCellExt, 1, substances_);
-
-  //  public:
-  //   vec<array<double, protein_amount>> substances_;
-  //   GeneCellExt() {}
-  //   explicit GeneCellExt(const array<double, 3>& position, array<double, protein_amount> init_substances)
-  //    : Base(position), substances_(init_substances){}
-
-  //   virtual ~GeneCellExt() {}
-
-  //   // In this method described functions. These functions determine changes in concentration of proteins.
-  //   // You should describe your own function and push it to the update_value vector.
-  //   // Value of protein concentration inside substances_ vector.
-  //   // update_value.push_back(<"your function">);
-  //   vector<double> evolve(double curr_time){
-  //     vector<double> update_value;
-  //     update_value.push_back(2*substances_[0][0] - curr_time);
-  //     update_value.push_back(1/substances_[0][1]);
-  //     update_value.push_back(substances_[0][2]);
-  //     assert(update_value.size() == protein_amount && "Amount of functions does not equal to amount of proteins\n");
-  //     return update_value;
-  //   }
-  //   void DivideImpl(void* daughter, double volume_ratio, double phi, double theta)
-  //     override {
-  //       auto daughter_cast = static_cast<Self<Scalar>*>(daughter);
-  //       for (int j =  0; j < protein_amount; j++){
-  //         daughter_cast->substances_[0][j] = substances_[0][j] * 0.4;
-  //         substances_[0][j] = substances_[0][j] * 0.6;
-  //       }
-  //       // forward call to implementation in CellExt
-  //       Base::DivideImpl(daughter_cast, volume_ratio, phi, theta);
-  //     }
-  // };
-
   struct GeneCalculation : public BaseBiologyModule {
     double time_step = Param::simulation_time_step_;
     vector<array<double, Param::protein_amount>> substances_;
-    // size_t& step = Param::step_global_;
 
     GeneCalculation() : BaseBiologyModule(gAllBmEvents), substances_(init_vals){}
 
