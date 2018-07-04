1bcb973b8afe9bcb979d6521a6f36ef1cf19e60e
Nam Nguyen <nam@cern.ch>
2018-07-04 11:25:38 +0200

    Install libzmq (not libzmqpp) as prerequisite.

--- util/installation/ubuntu-16.04/prerequisites.sh
+++ util/installation/ubuntu-16.04/prerequisites.sh
@@ -45,7 +45,7 @@ function InstallPackages {
   INSTALL_PACKAGES="freeglut3-dev  git valgrind python python3 python2.7-dev lcov \
   gcc g++ make cmake clang-3.9 clang-format-3.9 clang-tidy-3.9 libomp-dev \
   doxygen graphviz python-pip \
-  libzmq5"
+  libzmq5 libzmq3-dev"
 
   ADD_REPOSITORY='deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-3.9 main'
 
