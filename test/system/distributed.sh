8570ae336b449b2bca0b3cfe17745f3d05ecbbaa
Nam Nguyen <nam@cern.ch>
2018-08-06 17:19:39 +0200

    Test distributed run in Docker.
    There are a few gotchas.
    
    1. ROOT needs access to .h files. So we map the demo source
       and the BioDynaMo source directories in Docker.
    2. BDM framework creates an `output` directory. So we need to make
       sure that the working directory is writable.
    3. Since we build the demo with shared libraries from Ray (such as
       Arrow, Plasma) we need to also use the wheel we created. If we
       use the wheel from PyPI, the shared library won't be able to be
       loaded.

--- test/system/distributed.sh
+++ test/system/distributed.sh
@@ -15,13 +15,65 @@
 
 set -e -x
 
-tmp_dir=$(mktemp -d)
-trap "rm -rf \"${tmp_dir}\"" EXIT
+function test_local() {
+    tmp_dir=$(mktemp -d)
+    trap "rm -rf \"${tmp_dir}\"" EXIT
 
-biodynamo demo distributed "${tmp_dir}"
-cd "${tmp_dir}/distributed"
-mkdir build
-cd build
-cmake ..
-make
-python ../driver.py -l libdistributed-ray.so
+    biodynamo demo distributed "${tmp_dir}"
+    cd "${tmp_dir}/distributed"
+    mkdir build
+    cd build
+    cmake ..
+    make -j $(nproc)
+    python ../driver.py -l libdistributed-ray.so -m local
+    python ../driver.py -l libdistributed-ray.so -m ray
+}
+
+function test_remote() {
+    tmp_dir=$(mktemp -d)
+    trap "sudo docker rm -f bdmdev-ray || true; rm -rf \"${tmp_dir}\"" EXIT
+
+    biodynamo demo distributed "${tmp_dir}"
+    cd "${tmp_dir}/distributed"
+    mkdir build
+    cd build
+    cmake ..
+    make -j $(nproc)
+    cd ..
+    sudo docker rm -f bdmdev-ray || true
+    sudo docker build . \
+        --network host \
+        --build-arg USER="${USER}" \
+        --build-arg HOST_UID="$(id -u `whoami`)" \
+        --build-arg HOST_GID="$(id -g `whoami`)" \
+        --tag bdmdev-ray
+    port=$(($RANDOM % 50000 + 2000))
+    sudo docker run \
+        --name bdmdev-ray \
+        --net host \
+        --volume "${PWD}:${PWD}" \
+        --volume "${BDM_INSTALL_DIR}:${BDM_INSTALL_DIR}" \
+        --volume /tmp:/tmp \
+        --env BDM_INSTALL_DIR="${BDM_INSTALL_DIR}" \
+        --workdir "${PWD}" \
+        -dit \
+        bdmdev-ray \
+        bash
+    sudo docker exec \
+        -it \
+        bdmdev-ray \
+        sudo pip install "${BDM_INSTALL_DIR}/third_party/ray/ray-0.5.0-cp27-cp27mu-linux_x86_64.whl"
+    sudo docker exec \
+        bdmdev-ray \
+        bash -c "source ${BDM_INSTALL_DIR}/biodynamo-env.sh && ray start --head --redis-port ${port}" &
+    # Wait for Ray to start
+    sleep 5
+    python driver.py -l "${PWD}/build/libdistributed-ray.so" --redis-address "127.0.0.1:${port}"
+    sudo docker exec bdmdev-ray ray stop
+}
+
+test_local
+if [ ! -f /.dockerenv ] ; then
+  # Only run remote test if we are not already in a docker.
+  test_remote
+fi
