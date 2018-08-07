fb3af4180bda61d544f0af08b5acf40b9b2e1027
Nam Nguyen <nam@cern.ch>
2018-08-07 10:37:25 +0200

    Add license header to the driver.

--- demo/distributed/driver.py
+++ demo/distributed/driver.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python
+# -----------------------------------------------------------------------------
+#
+# Copyright (C) The BioDynaMo Project.
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+#
+# See the LICENSE file distributed with this work for details.
+# See the NOTICE file distributed with this work for additional information
+# regarding copyright ownership.
+#
+# -----------------------------------------------------------------------------
 
 import argparse
 import collections
