49247b31555ab9aeb3bd59ee82240751ee3f5553
Lukas Johannes Breitwieser <lukas.johannes.breitwieser@cern.ch>
2019-02-01 17:01:02 +0100

    WIP runBiodynamoTestsMain compiling AND linking

--- src/core/sim_object/cell.h
+++ src/core/sim_object/cell.h
@@ -388,32 +388,6 @@ class Cell : public SimObject {
   }
 };
 
-// ----------------------------------------------------------------------------
-// Implementation -------------------------------------------------------------
-constexpr std::array<double, 3> Cell::kXAxis;
-constexpr std::array<double, 3> Cell::kYAxis;
-constexpr std::array<double, 3> Cell::kZAxis;
-
-inline void Cell::ApplyDisplacement(
-    const std::array<double, 3>& displacement) {
-  UpdatePosition(displacement);
-  // Reset biological movement to 0.
-  SetTractorForce({0, 0, 0});
-}
-
-inline std::array<double, 3> Cell::
-    TransformCoordinatesGlobalToPolar(const std::array<double, 3>& pos) const {
-  auto vector_to_point = Math::Subtract(pos, position_);
-  std::array<double, 3> local_cartesian{Math::Dot(kXAxis, vector_to_point),
-                                        Math::Dot(kYAxis, vector_to_point),
-                                        Math::Dot(kZAxis, vector_to_point)};
-  double radius = std::sqrt(local_cartesian[0] * local_cartesian[0] +
-                            local_cartesian[1] * local_cartesian[1] +
-                            local_cartesian[2] * local_cartesian[2]);
-  return {radius, std::acos(local_cartesian[2] / radius),
-          std::atan2(local_cartesian[1], local_cartesian[0])};
-}
-
 }  // namespace bdm
 
 #endif  // CORE_SIM_OBJECT_CELL_H_
