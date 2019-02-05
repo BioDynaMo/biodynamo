ace6544077c363e92537b5f1d00aac54639cf355
Lukas Johannes Breitwieser <lukas.johannes.breitwieser@cern.ch>
2019-02-05 15:17:42 +0100

    Modify sim_object_test.*

--- test/unit/core/sim_object/cell_test.cc
+++ test/unit/core/sim_object/cell_test.cc
@@ -16,10 +16,14 @@
 #include <typeinfo>
 #include <gtest/gtest.h>
 #include "core/sim_object/cell.h"
+#include "unit/core/sim_object/sim_object_test.h"
 #include "unit/test_util/test_util.h"
 
 namespace bdm {
 namespace cell_test_internal {
+
+using sim_object_test_internal::GrowthModule;
+using sim_object_test_internal::MovementModule;
 
 TEST(CellTest, TransformCoordinatesGlobalToPolar) {
   TestCell cell;
@@ -160,99 +164,6 @@ TEST(CellTest, DivideVolumeRatioAxis) {
 }
 
 // TODO move all biology module tests to sim_object tests
-
-TEST(CellTest, BiologyModule) {
-  Simulation simulation(TEST_NAME);
-
-  TestCell cell;
-  double diameter = cell.GetDiameter();
-  auto position = cell.GetPosition();
-
-  cell.AddBiologyModule(new MovementModule({1, 2, 3}));
-  cell.AddBiologyModule(new GrowthModule());
-
-  cell.RunBiologyModules();
-
-  EXPECT_NEAR(diameter + 0.5, cell.GetDiameter(), abs_error<double>::value);
-  EXPECT_NEAR(position[0] + 1, cell.GetPosition()[0], abs_error<double>::value);
-  EXPECT_NEAR(position[1] + 2, cell.GetPosition()[1], abs_error<double>::value);
-  EXPECT_NEAR(position[2] + 3, cell.GetPosition()[2], abs_error<double>::value);
-}
-
-TEST(CellTest, GetBiologyModulesTest) {
-  Simulation simulation(TEST_NAME);
-
-  // create cell and add biology modules
-  TestCell cell;
-  cell.AddBiologyModule(new GrowthModule());
-  cell.AddBiologyModule(new GrowthModule());
-  cell.AddBiologyModule(new MovementModule({1, 2, 3}));
-
-  uint64_t growth_module_cnt = 0;
-  uint64_t movement_module_cnt = 0;
-  for(auto* bm : cell.GetAllBiologyModules()) {
-    if (dynamic_cast<GrowthModule*>(bm)) {
-      growth_module_cnt++;
-    } else if (MovementModule* mm = dynamic_cast<MovementModule*>(bm)) {
-      movement_module_cnt++;
-      EXPECT_ARR_NEAR(mm->velocity_, {1, 2, 3});
-    }
-  }
-
-  EXPECT_EQ(2u, growth_module_cnt);
-  EXPECT_EQ(1u, movement_module_cnt);
-}
-
-TEST(CellTest, BiologyModuleEventHandler) {
-  Simulation simulation(TEST_NAME);
-
-  TestCell cell;
-
-  cell.AddBiologyModule(new MovementModule({1, 2, 3}));
-  cell.AddBiologyModule(new GrowthModule());
-
-  CellDivisionEvent event(1, 2, 3);
-  TestCell copy;
-  copy.EventConstructor(event, &cell, 0);
-  cell.EventHandler(event, &copy);
-
-  const auto& bms = cell.GetAllBiologyModules();
-  ASSERT_EQ(1u, bms.size());
-  EXPECT_TRUE(dynamic_cast<GrowthModule*>(bms[0]) != nullptr);
-
-  const auto& copy_bms = copy.GetAllBiologyModules();
-  ASSERT_EQ(1u, copy_bms.size());
-  EXPECT_TRUE(dynamic_cast<GrowthModule*>(copy_bms[0]) != nullptr);
-}
-
-TEST(CellTest, RemoveBiologyModule) {
-  Simulation simulation(TEST_NAME);
-
-  TestCell cell;
-
-  // add RemoveModule as first one! If removal while iterating over it is not
-  // implemented correctly, MovementModule will not be executed.
-  cell.AddBiologyModule(new RemoveModule());
-  cell.AddBiologyModule(new MovementModule({1, 2, 3}));
-  cell.AddBiologyModule(new GrowthModule());
-
-  // RemoveModule should remove itself
-  cell.RunBiologyModules();
-
-  const auto& bms = cell.GetAllBiologyModules();
-  ASSERT_EQ(2u, bms.size());
-  EXPECT_TRUE(dynamic_cast<MovementModule*>(bms[0]) != nullptr);
-  EXPECT_TRUE(dynamic_cast<GrowthModule*>(bms[1]) != nullptr);
-  // check if MovementModule and GrowthModule have been executed correctly.
-  EXPECT_ARR_NEAR({1, 2, 3}, cell.GetPosition());
-  EXPECT_NEAR(0.5, cell.GetDiameter(), abs_error<double>::value);
-
-  cell.AddBiologyModule(new RemoveModule());
-  ASSERT_EQ(3u, bms.size());
-  auto* to_be_removed = dynamic_cast<RemoveModule*>(bms[2]);
-  cell.RemoveBiologyModule(to_be_removed);
-  ASSERT_EQ(2u, bms.size());
-}
 
 TEST(CellTest, IO) { RunIOTest(); }
 
--- test/unit/core/sim_object/cell_test.h
+++ test/unit/core/sim_object/cell_test.h
@@ -17,93 +17,18 @@
 
 #include <vector>
 
-#include "core/biology_module/biology_module.h"
 #include "core/event/cell_division_event.h"
 #include "core/sim_object/cell.h"
 #include "core/util/io.h"
 #include "gtest/gtest.h"
 #include "unit/test_util/test_util.h"
+#include "unit/core/sim_object/sim_object_test.h"
+
 
 #define ROOTFILE "bdmFile.root"
 
 namespace bdm {
 namespace cell_test_internal {
-
-struct GrowthModule : public BaseBiologyModule {
-  double growth_rate_ = 0.5;
-  GrowthModule() : BaseBiologyModule(CellDivisionEvent::kEventId) {}
-
-  virtual ~GrowthModule() {}
-
-  /// Create a new instance of this object using the default constructor.
-  BaseBiologyModule* GetInstance() const { return new GrowthModule(); }
-
-  void EventConstructor(const Event& event, BaseBiologyModule* other, uint64_t new_oid = 0) override {
-    BaseBiologyModule::EventConstructor(event, other, new_oid);
-    if(GrowthModule* gbm = dynamic_cast<GrowthModule*>(other)) {
-      growth_rate_ = gbm->growth_rate_;
-    } else {
-      Log::Fatal("GrowthModule::EventConstructor", "other was not of type GrowthModule");
-    }
-  }
-
-  /// Default event handler (exising biology module won't be modified on
-  /// any event)
-  void EventHandler(const Event &event, BaseBiologyModule *other1, BaseBiologyModule* other2 = nullptr) override {
-    BaseBiologyModule::EventHandler(event, other1, other2);
-  }
-
-  void Run(SimObject* t) override {
-    t->SetDiameter(t->GetDiameter() + growth_rate_);
-  }
-
-  BDM_CLASS_DEF(GrowthModule, 1);
-};
-
-struct MovementModule : public BaseBiologyModule {
-  std::array<double, 3> velocity_;
-
-  MovementModule() : BaseBiologyModule(0, CellDivisionEvent::kEventId), velocity_({{0, 0, 0}}) {}
-  explicit MovementModule(const std::array<double, 3>& velocity)
-      : BaseBiologyModule(0, CellDivisionEvent::kEventId), velocity_(velocity) {}
-
-  /// Create a new instance of this object using the default constructor.
-  BaseBiologyModule* GetInstance() const { return new MovementModule(); }
-
-  void EventConstructor(const Event& event, BaseBiologyModule* other, uint64_t new_oid = 0) override {
-    BaseBiologyModule::EventConstructor(event, other, new_oid);
-    if(MovementModule* mbm = dynamic_cast<MovementModule*>(other)) {
-      velocity_ = mbm->velocity_;
-    } else {
-      Log::Fatal("MovementModule::EventConstructor", "other was not of type MovementModule");
-    }
-  }
-
-  /// Default event handler
-  void EventHandler(const Event &event, BaseBiologyModule *other1, BaseBiologyModule* other2 = nullptr) override {
-    BaseBiologyModule::EventHandler(event, other1, other2);
-  }
-
-  void Run(SimObject* so) override {
-    const auto& position = so->GetPosition();
-    so->SetPosition(Math::Add(position, velocity_));
-  }
-
-  BDM_CLASS_DEF(MovementModule, 1);
-};
-
-/// This biology module removes itself the first time it is executed
-struct RemoveModule : public BaseBiologyModule {
-  RemoveModule() {}
-
-  BaseBiologyModule* GetInstance() const { return new RemoveModule(); }
-
-  void Run(SimObject* sim_object) override {
-    sim_object->RemoveBiologyModule(this);
-  }
-
-  BDM_CLASS_DEF(RemoveModule, 1);
-};
 
 /// Class used to get access to protected members
 class TestCell : public Cell {
@@ -150,6 +75,8 @@ class TestCell : public Cell {
 namespace cell_test_internal {
 
 inline void RunIOTest() {
+  using GrowthModule = sim_object_test_internal::GrowthModule;
+  using MovementModule = sim_object_test_internal::MovementModule;
   remove(ROOTFILE);
 
   TestCell cell;
