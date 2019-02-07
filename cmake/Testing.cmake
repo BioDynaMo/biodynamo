a25fdd21d16747e9f04b8c263c47be7c9fa35718
Lukas Johannes Breitwieser <lukas.johannes.breitwieser@cern.ch>
2019-02-07 19:09:28 +0100

    Create util/valgrind.sh
    This makes it easier to call a binary with valgrind with its many options

--- cmake/Testing.cmake
+++ cmake/Testing.cmake
@@ -83,7 +83,7 @@ function(bdm_add_test_executable TEST_TARGET)
       AND NOT ${TEST_TARGET} STREQUAL "runBiodynamoTests_resource_manager_aos_test"
       AND NOT ${TEST_TARGET} STREQUAL "runBiodynamoTests_so_pointer_aos_test")
     # filter out SchedulerTest.Backup because of timing issue
-    add_test(NAME "valgrind_${TEST_TARGET}" COMMAND valgrind --track-origins=yes --leak-resolution=high --tool=memcheck --leak-check=full --show-leak-kinds=all --gen-suppressions=all --show-reachable=no --suppressions=${CMAKE_BINARY_DIR}/../util/valgrind-bdm.supp --error-exitcode=1 ./${TEST_TARGET} -- --gtest_filter=-*DeathTest.*:IOTest.InvalidRead:SchedulerTest.Backup:ResourceManagerTest.SortAndApplyOnAllElementsParallel*)
+    add_test(NAME "valgrind_${TEST_TARGET}" COMMAND ${CMAKE_SOURCE_DIR}/util/valgrind.sh ./${TEST_TARGET} -- --gtest_filter=-*DeathTest.*:IOTest.InvalidRead:SchedulerTest.Backup:ResourceManagerTest.SortAndApplyOnAllElementsParallel*)
   endif()
 
   add_dependencies(check ${TEST_TARGET})
