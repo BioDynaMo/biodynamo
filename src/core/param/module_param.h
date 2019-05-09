bd1440e10e95cfe390c4b67222bd46096522dfe3
Lukas Johannes Breitwieser <lukas.johannes.breitwieser@cern.ch>
2019-05-09 10:26:11 +0200

    Fix clang compiler warning

--- src/core/param/module_param.h
+++ src/core/param/module_param.h
@@ -52,7 +52,7 @@ struct ModuleParam {
   virtual void AssignFromConfig(const std::shared_ptr<cpptoml::table>&) = 0;
 
  private:
-  friend class Param;
+  friend struct Param;
   BDM_CLASS_DEF(ModuleParam, 1);
 };
 
