9d8121d484303ff65b9783e0b3e5679232786d48
Giovanni De Toni <giovanni.det@gmail.com>
2019-06-27 16:31:39 +0200

    Improve cmake's install procedure.

--- cmake/Installation.cmake
+++ cmake/Installation.cmake
@@ -29,42 +29,18 @@ endif()
 
 # Install biodynamo in its final directory
 install(DIRECTORY ${CMAKE_BIODYNAMO_ROOT}/biodynamo
-        DESTINATION .)
+        DESTINATION .
+        USE_SOURCE_PERMISSIONS)
 
 # Install third party directories
 install(DIRECTORY ${CMAKE_BIODYNAMO_ROOT}/third_party
         DESTINATION .
+        USE_SOURCE_PERMISSIONS
         OPTIONAL
-        PATTERN "*.tar.gz" EXCLUDE
-        PATTERN "root/bin" EXCLUDE
-        PATTERN "paraview/bin" EXCLUDE
-        PATTERN "qt/bin" EXCLUDE)
-
-# Copy root executables and make them executable
-install(DIRECTORY ${CMAKE_BIODYNAMO_ROOT}/third_party/root/bin
-        DESTINATION third_party/root
-        OPTIONAL
-        FILE_PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ
-        GROUP_EXECUTE GROUP_READ WORLD_EXECUTE WORLD_READ
-        PATTERN "${CMAKE_BIODYNAMO_ROOT}/third_party/root/bin/*")
-
-# Copy paraviews executable and make them executable
-install(DIRECTORY ${CMAKE_BIODYNAMO_ROOT}/third_party/paraview/bin
-        DESTINATION third_party/paraview
-        OPTIONAL
-        FILE_PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ
-        GROUP_EXECUTE GROUP_READ WORLD_EXECUTE WORLD_READ
-        PATTERN "${CMAKE_BIODYNAMO_ROOT}/third_party/paraview/bin/*")
-
-# Copy qt executable and make them executable
-install(DIRECTORY ${CMAKE_BIODYNAMO_ROOT}/third_party/qt/bin
-        DESTINATION third_party/qt
-        OPTIONAL
-        FILE_PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ
-        GROUP_EXECUTE GROUP_READ WORLD_EXECUTE WORLD_READ
-        PATTERN "${CMAKE_BIODYNAMO_ROOT}/third_party/qt/bin/*"
-        )
+        PATTERN "*.tar.gz" EXCLUDE)
 
 # Copy the environment file
 install(FILES ${CMAKE_BIODYNAMO_ROOT}/biodynamo-env.sh
-        DESTINATION .)
+        DESTINATION .
+        PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ
+        GROUP_EXECUTE GROUP_READ WORLD_EXECUTE WORLD_READ)
