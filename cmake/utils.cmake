6f2ae0e11c5a7248ce42a1e2c553ee4e1fc84b8e
Lukas Johannes Breitwieser <lukas.johannes.breitwieser@cern.ch>
2019-09-10 10:40:24 +0200

    Fix grammar

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -58,7 +58,7 @@ endfunction()
 function(verify_ROOT)
     if(NOT ROOT_FOUND)
         print_warning()
-        message("We did not found any ROOT installed in the system. We will proceed to download it "
+        message("We did not find any ROOT installed in the system. We will proceed to download it "
         "once the build process has started. ROOT will be then installed to the location ${CMAKE_THIRD_PARTY_DIR}/root.")
         print_line()
         include(external/ROOT)
