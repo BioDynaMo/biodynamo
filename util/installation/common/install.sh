4c4b2ae222d492ae17fb7d41756d15de547be7c1
Ahmad Hesam <hesam212@gmail.com>
2019-09-11 18:03:31 +0200

    Update success message with right install directory

--- util/installation/common/install.sh
+++ util/installation/common/install.sh
@@ -78,4 +78,5 @@ CleanBuild $BUILD_DIR
 
 # print final steps
 EchoSuccess "Installation of BioDynaMo finished successfully!"
-EchoFinishThisStep $HOME/biodynamo
+# TODO(ahmad): stay consistent with the configured install directory in CMake
+EchoFinishThisStep $HOME/bdm
