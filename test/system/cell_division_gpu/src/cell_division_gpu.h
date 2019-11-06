127cb071237b423c34b5a447f92d6358c2d0e124
Ahmad Hesam <hesam212@gmail.com>
2019-11-06 19:38:36 +0100

    Add command line arguments to enable GPU benchmarking
    E.g. `cell_division_gpu --opencl=1 --cells-per-dim=64` to run on
    GPU using OpenCL.

--- test/system/cell_division_gpu/src/cell_division_gpu.h
+++ test/system/cell_division_gpu/src/cell_division_gpu.h
@@ -18,7 +18,9 @@
 #include <array>
 
 #include "biodynamo.h"
+#include "core/param/command_line_options.h"
 #include "core/util/math.h"
+#include "core/util/timing.h"
 
 namespace bdm {
 
@@ -29,10 +31,10 @@ namespace bdm {
 // -----------------------------------------------------------------------------
 
 inline void ExpectArrayNear(const Double3& actual, const Double3& expected,
-                            bool* ret) {
+                            bool* wrong) {
   for (size_t i = 0; i < actual.size(); i++) {
     if (std::fabs(expected[i] - actual[i]) > 1e-9) {
-      *ret = false;
+      *wrong = true;
       std::cout << "Wrong result! Expected " << expected[i]
                 << ", but instead got " << actual[i]
                 << ", which is a difference of "
@@ -44,19 +46,31 @@ inline void ExpectArrayNear(const Double3& actual, const Double3& expected,
 
 enum ExecutionMode { kCpu, kCuda, kOpenCl };
 
-inline void RunTest(bool* result, ExecutionMode mode) {
-  std::cout << "Running test case " << mode << std::endl;
+inline void RunTest(bool* wrong, ExecutionMode mode, bool verify,
+                    uint64_t timesteps, uint64_t cells_per_dim) {
+  std::cout << "Running simulation on ";
   auto set_param = [&](auto* param) {
     switch (mode) {
       case kCpu:
+        std::cout << "CPU (single core)\n";
         break;
       case kOpenCl:
+        std::cout << "GPU (OpenCL)\n";
         param->use_gpu_ = true;
         param->use_opencl_ = true;
+        break;
       case kCuda:
+        std::cout << "GPU (CUDA)\n";
         param->use_gpu_ = true;
+        break;
     }
   };
+
+  // Results below are with 2 cells per dimension and after 10 timesteps
+  if (verify) {
+    cells_per_dim = 2;
+    timesteps = 10;
+  }
 
   Simulation simulation("cell_division_gpu", set_param);
   auto* rm = simulation.GetResourceManager();
@@ -68,7 +82,6 @@ inline void RunTest(bool* result, ExecutionMode mode) {
 #pragma omp parallel
   simulation.GetRandom()->SetSeed(1);
 
-  size_t cells_per_dim = 2;
   auto construct = [](const Double3& position) {
     auto* cell = new Cell(position);
     cell->SetDiameter(30);
@@ -89,62 +102,85 @@ inline void RunTest(bool* result, ExecutionMode mode) {
     }
   }
 
-  // Run for 10 timesteps. In step 2 a division should take place. In step 3
-  // these new cells are instantiated
-  simulation.GetScheduler()->Simulate(10);
+  {
+    Timing timer("Execution time");
+    simulation.GetScheduler()->Simulate(timesteps);
+  }
 
-  ExpectArrayNear(
-      rm->GetSimObject(0)->GetPosition(),
-      {4.1399071506916413909, -5.9871942139195297727, 2.8344890446256703065},
-      result);
-  ExpectArrayNear(
-      rm->GetSimObject(1)->GetPosition(),
-      {-2.4263219149482031511, -1.4202336557809887019, 29.769029317615839147},
-      result);
-  ExpectArrayNear(
-      rm->GetSimObject(2)->GetPosition(),
-      {-4.9118212650644856865, 23.156656083480623209, -9.1231684411316447125},
-      result);
-  ExpectArrayNear(
-      rm->GetSimObject(3)->GetPosition(),
-      {4.3076765979041251597, 15.615300607043293368, 25.657658447555828474},
-      result);
-  ExpectArrayNear(
-      rm->GetSimObject(4)->GetPosition(),
-      {28.139314619772036963, -0.20987998233654170388, 4.6381417441282613012},
-      result);
-  ExpectArrayNear(
-      rm->GetSimObject(5)->GetPosition(),
-      {24.417550786690171094, 3.347525366344008102, 28.067824703341415216},
-      result);
-  ExpectArrayNear(
-      rm->GetSimObject(6)->GetPosition(),
-      {16.614520566718258721, 15.828015607618416638, -4.8357284569095106974},
-      result);
-  ExpectArrayNear(
-      rm->GetSimObject(7)->GetPosition(),
-      {14.446017269290647889, 22.250832446808978204, 20.180438615017894932},
-      result);
+  if (verify) {
+    ExpectArrayNear(
+        rm->GetSimObject(0)->GetPosition(),
+        {4.1399071506916413909, -5.9871942139195297727, 2.8344890446256703065},
+        wrong);
+    ExpectArrayNear(
+        rm->GetSimObject(1)->GetPosition(),
+        {-2.4263219149482031511, -1.4202336557809887019, 29.769029317615839147},
+        wrong);
+    ExpectArrayNear(
+        rm->GetSimObject(2)->GetPosition(),
+        {-4.9118212650644856865, 23.156656083480623209, -9.1231684411316447125},
+        wrong);
+    ExpectArrayNear(
+        rm->GetSimObject(3)->GetPosition(),
+        {4.3076765979041251597, 15.615300607043293368, 25.657658447555828474},
+        wrong);
+    ExpectArrayNear(
+        rm->GetSimObject(4)->GetPosition(),
+        {28.139314619772036963, -0.20987998233654170388, 4.6381417441282613012},
+        wrong);
+    ExpectArrayNear(
+        rm->GetSimObject(5)->GetPosition(),
+        {24.417550786690171094, 3.347525366344008102, 28.067824703341415216},
+        wrong);
+    ExpectArrayNear(
+        rm->GetSimObject(6)->GetPosition(),
+        {16.614520566718258721, 15.828015607618416638, -4.8357284569095106974},
+        wrong);
+    ExpectArrayNear(
+        rm->GetSimObject(7)->GetPosition(),
+        {14.446017269290647889, 22.250832446808978204, 20.180438615017894932},
+        wrong);
+    if (!wrong) {
+      std::cout << "Simulation was verified correctly!" << std::endl;
+    }
+  }
 }
 
 inline int Simulate(int argc, const char** argv) {
-  bool result = true;
+  auto parser = CustomCLOParser(argc, argv);
+  bool cuda = parser.GetValue("cuda", false);
+  bool opencl = parser.GetValue("opencl", false);
+  bool verify = parser.GetValue("verify", false);
+  uint64_t cells_per_dim = parser.GetValue("cells-per-dim", 2);
+  uint64_t timesteps = parser.GetValue("timesteps", 10);
+
+  bool wrong = true;
 
   // TODO(ahmad): after Trello card ("Fix inconsistency in cell state due to
-  // direct updates in Biology Modules")
-  // enable multithreading, and adjust results if necessary
+  // direct updates in Biology Modules") enable multithreading, and adjust
+  // results if necessary
   omp_set_num_threads(1);
 
-  // Run CPU version
-  RunTest(&result, kCpu);
+  if (!cuda && !opencl) {
+    // Run CPU version
+    RunTest(&wrong, kCpu, verify, timesteps, cells_per_dim);
+  }
 
-  // Run GPU (CUDA) version
-  RunTest(&result, kCuda);
+#ifdef USE_CUDA
+  if (cuda) {
+    // Run GPU (CUDA) version
+    RunTest(&wrong, kCuda, verify, timesteps, cells_per_dim);
+  }
+#endif  // USE_CUDA
 
-  // Run GPU (OpenCL) version
-  RunTest(&result, kOpenCl);
+#ifdef USE_OPENCL
+  if (opencl) {
+    // Run GPU (OpenCL) version
+    RunTest(&wrong, kOpenCl, verify, timesteps, cells_per_dim);
+  }
+#endif  // USE_OPENCL
 
-  return !result;
+  return !wrong;
 }
 
 }  // namespace bdm
