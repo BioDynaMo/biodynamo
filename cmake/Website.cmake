4d1322bd6068d9ed1d11cfc566a618990a97530d
Ahmad Hesam <ahmad.hesam@cern.ch>
2019-12-16 11:25:25 +0100

    Add website option in CMake (#91)
    Check if Doxygen is installed before attempting to build the website

--- cmake/Website.cmake
+++ cmake/Website.cmake
@@ -29,16 +29,15 @@ function(GenerateStaticFiles TARGET FLAGS)
   add_dependencies(${TARGET} doc)
 endfunction()
 
-# Generate static website files
-GenerateStaticFiles(website "--api")
-# Generate staic website files and launches website on localhost for live editing
-GenerateStaticFiles(website-live "--develop")
-
-# For website-live the Doxygen files are not generated because we expect users
-# to mostly be interested in how the markdown files will look like. There is
-# also some bug in trying to show the API files if you were to enable to it
-# (i.e. it doesn't resolve the path correctly). For those that want to see the
-# Doxygen output, you can open the index.html in the build/doc/api directory with
-# your browser
+if (website)
+  if (NOT DOXYGEN_FOUND)
+    message(FATAL "You cannot build the website without Doxygen. Please make
+    sure that Doxygen is installed before attempting to build the website!")
+  endif()
+  # Generate static website files
+  GenerateStaticFiles(website "--api")
+  # Generate staic website files and launches website on localhost for live editing
+  GenerateStaticFiles(website-live "--develop")
+endif()
 
 # ------------------------------------------------------------------------------
