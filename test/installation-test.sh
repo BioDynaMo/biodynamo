c906796cd6f11601885dcb9b7cb4b0268427513a
Ahmad Hesam <hesam212@gmail.com>
2020-01-21 17:32:46 +0100

    Invert flag logic

--- test/installation-test.sh
+++ test/installation-test.sh
@@ -37,13 +37,13 @@ cd $BDM_PROJECT_DIR
 
 BDM_OS=$(DetectOs)
 
-NOTEBOOK_FLAG="-Dnotebooks=on"
+NOTEBOOK_FLAG="-Dnotebooks=off"
 
 # speed-up build by disabling tests and demos
 # Currently SBML and notebooks is not supported on osx
 if [ "$BDM_OS" != "osx" ] && [ "$BDM_OS" != "travis-osx" ]; then
   SBML_FLAG="-Dsbml=on"
-  NOTEBOOK_FLAG="-Dnotebooks=off"
+  NOTEBOOK_FLAG="-Dnotebooks=on"
 fi
 export BDM_CMAKE_FLAGS="-Dtest=off ${NOTEBOOK_FLAG} ${SBML_FLAG}"
 
