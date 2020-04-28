ba74d9a0e45a581d0fd82dca5a9da9b382d9eff8
Jack <44051158+BJackal@users.noreply.github.com>
2020-04-28 10:37:00 +0100

    Runge Kutta update
    Fixing some issues, such as forcing divisions to take doubles.

--- src/core/diffusion_grid.h
+++ src/core/diffusion_grid.h
@@ -578,8 +578,8 @@ class DiffusionGrid {
 
     #define YBF 16
     #pragma omp parallel for collapse(2)
-    for( size_t i = 5; i < 100; i += 5){ 
-    for (size_t order = 0; order < 3; order++ ){
+    for( size_t i = 1; i < 2; i += 1){ 
+    for (size_t order = 0; order < 4; order++ ){
     for (size_t yy = 0; yy < ny; yy += YBF) {
       for (size_t z = 0; z < nz; z++) {
         size_t ymax = yy + YBF;
@@ -588,7 +588,7 @@ class DiffusionGrid {
         }
         for (size_t y = yy; y < ymax; y++) {
           size_t x = 0;
-          int c, cp, cm, n, s, b, t;
+          int c, n, s, b, t;
           c = x + y * nx + z * nx * ny;
     #pragma omp simd
           for (x = 1; x < nx - 1; x++) {
@@ -597,74 +597,50 @@ class DiffusionGrid {
             ++s;
             ++b;
             ++t;
-            /* if ( y==0 || y== ny-1 || z==0 || z=nz-1 ){
+             if ( y==0 || y== ny-1 || z==0 || z=nz-1 ){
                       continue;
-                      }*/
-            // TODO (Jack) This is to be used in the case of an insulated box
-            // for Thermo and is a WIP.
-
-            cm = c - 1;
-            cp = c + 1;
+             }
+            
             n = c - nx;
             s = c + nx;
             b = c - nx * ny;
             t = c + nx * ny;
-
-            /* X axis */
-            if (x == 1) {
-              cm = c + 1;
-            } else if (x == nx - 2) {
-              cp = c - 1;
-            }
-
-            /* Y axis */
-            if (y == 0) {
-              n = s;
-            } else if (y == ny - 1) {
-              s = n;
-            }
-
-            /* Z axis */
-            if (z == 0) {
-              b = t;
-            } else if (z == nz - 1) {
-              t = b;
-            }
             
-            double h = 1.0* (i/20.0);
+            double h = 1.0;
+            double h2 = 2.0;
+            double h3 = 6.0;
 
                 if(order == 0){ /* for k1 */
 
-                k_[order] = (d * (r1_[cm] - 2 * r1_[c] + r1_[cp]) * ibl2 +
-                                d  * (r1_[s] - 2 * r1_[c] + r1_[n]) * ibl2 +
-                                d  * (r1_[b] - 2 * r1_[c] + r1_[t]) * ibl2);
-                y_[order] = c1_[c] + k_[order] * 1.0*((1.0 *h)/2.0);
+                k_[order] = (d * (c1_[c - 1] - 2 * c1_[c] + c1_[c + 1]) * ibl2 +
+                                d  * (c1_[s] - 2 * c1_[c] + c1_[n]) * ibl2 +
+                                d  * (c1_[b] - 2 * c1_[c] + c1_[t]) * ibl2);
+                y_[order] = c1_[c] + (k_[order] * (((double)h)/((double)h2)));
                 r1_[c] = y_[order];  
 
                 } else if (order == 1){ /* for k2 */
     
-                k_[order] = (d * 1.0*((1.0 *h)/2.0) * (r1_[cm] - 2 * r1_[c] + r1_[cp]) * ibl2 +
-                                d * 1.0*((1.0 *h)/2.0) * (r1_[s] - 2 * r1_[c] + r1_[n]) * ibl2 +
-                                d * 1.0*((1.0 *h)/2.0) * (r1_[b] - 2 * r1_[c] + r1_[t]) * ibl2);
-                y_[order] = c1_[c] + k_[order]* 1.0*((1.0 *h)/2.0);
+                k_[order] = (d * (((double)h)/((double)h2)) * (r1_[c - 1] - 2 * r1_[c] + r1_[c + 1]) * ibl2 +
+                                d * (((double)h)/((double)h2)) * (r1_[s] - 2 * r1_[c] + r1_[n]) * ibl2 +
+                                d * (((double)h)/((double)h2)) * (r1_[b] - 2 * r1_[c] + r1_[t]) * ibl2);
+                y_[order] = c1_[c] + (k_[order]* (((double)h)/((double)h2)));
                 r2_[c] = y_[order];             
                  
                 } else if (order == 2){ /* for k3 */
     
-                k_[order] = (d * 1.0*((1.0 *h)/2.0) * (r2_[cm] - 2 * r2_[c] + r2_[cp]) * ibl2 +
-                                d * 1.0*((1.0 *h)/2.0) * (r2_[s] - 2 * r2_[c] + r2_[n]) * ibl2 +
-                                d * 1.0*((1.0 *h)/2.0) * (r2_[b] - 2 * r2_[c] + r2_[t]) * ibl2);
-                y_[order] = c1_[c] + k_[order] * (1.0 * h);
+                k_[order] = (d * (((double)h)/((double)h2)) * (r2_[c - 1] - 2 * r2_[c] + r2_[c + 1]) * ibl2 +
+                                d * (((double)h)/((double)h2)) * (r2_[s] - 2 * r2_[c] + r2_[n]) * ibl2 +
+                                d * (((double)h)/((double)h2)) * (r2_[b] - 2 * r2_[c] + r2_[t]) * ibl2);
+                y_[order] = c1_[c] + (k_[order] * (double)h);
                 r3_[c] = y_[order];             
           
-                }else{ /* for k4 */
-                k_[order] = (d * dt_ * (r3_[cm] - 2 * r3_[c] + r3_[cp]) * ibl2 +
-                            d * dt_ * (r3_[s] - 2 * r3_[c] + r3_[n]) * ibl2 +
-                            d * dt_ * (r3_[b] - 2 * r3_[c] + r3_[t]) * ibl2);
+                }else if (order == 3){ /* for k4 */
+                k_[order] = (d * ((double)h) * (r3_[c - 1] - 2 * r3_[c] + r3_[c + 1]) * ibl2 +
+                            d * ((double)h) * (r3_[s] - 2 * r3_[c] + r3_[n]) * ibl2 +
+                            d * ((double)h) * (r3_[b] - 2 * r3_[c] + r3_[t]) * ibl2);
 
                         c2_[c] =
-                        (c1_[c] + 1.0*((1.0*h)/6.0)*(k_[0] + 2.0*k_[1] + 2.0*k_[2] + k_[3])*
-                (1 - mu_);
+                        (c1_[c] + ((((double)h)/((double)h3)))*(k_[0] + 2.0*k_[1] + 2.0*k_[2] + k_[3]) * (1 - mu_);
                 }
 
             }
