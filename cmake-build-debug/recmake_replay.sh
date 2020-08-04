be6dc787975ee3419e3487b4fb0bcccff1f75006
Dorukhan Arslan <dorukhan.ars.css@gmail.com>
2020-08-04 18:25:30 +0200

    thisbdm.sh: back to function exports for bash, autoloads instead of aliases for zsh, mockup quiet mode, temporarily removed env indicator, lsb dependency removed

--- cmake-build-debug/recmake_replay.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-rm -f CMakeCache.txt
-/Applications/CLion.app/Contents/bin/cmake/mac/bin/cmake  -DCMAKE_BUILD_TYPE="Debug" /Users/dorukhanarslan/CLionProjects/biodynamo 
-/Applications/CLion.app/Contents/bin/cmake/mac/bin/cmake  -DCMAKE_BUILD_TYPE="Debug" /Users/dorukhanarslan/CLionProjects/biodynamo
-/Applications/CLion.app/Contents/bin/cmake/mac/bin/cmake  -DCMAKE_BUILD_TYPE="Debug" /Users/dorukhanarslan/CLionProjects/biodynamo
-/Applications/CLion.app/Contents/bin/cmake/mac/bin/cmake  -DCMAKE_BUILD_TYPE="Debug" /Users/dorukhanarslan/CLionProjects/biodynamo
