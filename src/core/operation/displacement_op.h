0c70a991ecf64a2fca31483382f4c40483d5a656
Ahmad Hesam <hesam212@gmail.com>
2020-09-07 14:59:38 +0200

    Test style guid checks with some mistakes

--- src/core/operation/displacement_op.h
+++ src/core/operation/displacement_op.h
@@ -26,9 +26,9 @@
 #include "core/operation/operation_registry.h"
 #include "core/param/param.h"
 #include "core/scheduler.h"
+#include "core/util/math.h"
 #include "core/sim_object/sim_object.h"
 #include "core/simulation.h"
-#include "core/util/math.h"
 #include "core/util/thread_info.h"
 
 namespace bdm {
