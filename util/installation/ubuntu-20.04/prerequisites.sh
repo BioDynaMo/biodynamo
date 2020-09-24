b08a3ffac4fe42454d1aa5066cf2761d53c13075
Ahmad Hesam <hesam212@gmail.com>
2020-09-24 13:13:31 +0200

    Add llvm repo to install clang-tidy The package from the ubuntu repo was failing

--- util/installation/ubuntu-20.04/prerequisites.sh
+++ util/installation/ubuntu-20.04/prerequisites.sh
@@ -29,6 +29,11 @@ set -e
 
 BDM_PROJECT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/../../.."
 
+wget https://apt.llvm.org/llvm-snapshot.gpg.key
+sudo apt-key add llvm-snapshot.gpg.key
+sudo add-apt-repository "deb http://apt.llvm.org/focal/ llvm-toolchain-focal-10 main"
+sudo apt update
+
 # use ubuntu-16.04 prerequisites script
 . $BDM_PROJECT_DIR/util/installation/ubuntu-16.04/prerequisites.sh $1
 
