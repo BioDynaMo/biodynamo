BioDynaMo/biodynamo

commit 2bd71f5986c801f4614b85c39fc9e5735cbd40c6

Print simulation summary at the end of simulation To be enabled with Param::statistics_

src/core/scheduler.cc
@@ -28,7 +28,6 @@
 #include "core/simulation_backup.h"
 #include "core/util/log.h"
 #include "core/visualization/root/adaptor.h"
-#include "core/visualization/visualization_adaptor.h"
 
 namespace bdm {
 
@@ -38,19 +37,29 @@ Scheduler::Scheduler() {
   if (backup_->RestoreEnabled()) {
     restore_point_ = backup_->GetSimulationStepsFromBackup();
   }
-  visualization_ = VisualizationAdaptor::Create(param->visualization_engine_);
   root_visualization_ = new RootAdaptor();
 
-  default_ops_ = {"first op",     "bound space",    "biology module",
-                  "displacement", "discretization", "diffusion",
-                  "last op"};
+  // Operations are scheduled in the following order (sub categorated by their
+  // operation implementation type, so that actual order may vary)
+  default_ops_ = {"set up exec context",
+                  "visualize",
+                  "update environment",
+                  "first op",
+                  "bound space",
+                  "biology module",
+                  "displacement",
+                  "discretization",
+                  "diffusion",
+                  "last op",
+                  "tear down exec context"};
 
   // Schedule the default operations
   for (auto& def_op : default_ops_) {
     ScheduleOp(NewOperation(def_op));
   }
 
-  protected_ops_ = {"first op", "biology module", "discretization", "last op"};
+  protected_ops_ = {"set up exec context", "tear down exec context", "first op",
+                    "biology module",      "discretization",         "last op"};
 }
 
 Scheduler::~Scheduler() {
@@ -71,13 +80,6 @@ Scheduler::~Scheduler() {
   }
   delete backup_;
   delete root_visualization_;
-  if (visualization_) {
-    delete visualization_;
-  }
-  auto* param = Simulation::GetActive()->GetParam();
-  if (param->statistics_) {
-    std::cout << gStatistics << std::endl;
-  }
 }
 
 void Scheduler::Simulate(uint64_t steps) {
@@ -95,6 +97,8 @@ void Scheduler::Simulate(uint64_t steps) {
 }
 
 uint64_t Scheduler::GetSimulatedSteps() const { return total_steps_; }
+
+TimingAggregator* Scheduler::GetOpTimes() { return &op_times_; }
 
 void Scheduler::ScheduleOp(Operation* op) { ops_to_add_.push_back(op); }
 
@@ -199,43 +203,27 @@ void Scheduler::RunScheduledOps() {
     }
   }
   RunAllScheduledOps functor(row_wise_operations);
-  rm->ApplyOnAllElementsParallelDynamic(batch_size, functor);
+
+  Timing::Time("sim object ops", [&]() {
+    rm->ApplyOnAllElementsParallelDynamic(batch_size, functor);
+  });
 
   // Run the column-wise operations
   for (auto* op : scheduled_standalone_ops_) {
     if (total_steps_ % op->frequency_ == 0) {
-      (*op)();
+      Timing::Time(op->name_, [&]() { (*op)(); });
     }
   }
 }
 
 void Scheduler::Execute() {
-  auto* sim = Simulation::GetActive();
-  auto* env = sim->GetEnvironment();
-
   ScheduleOps();
 
-  Timing::Time("Set up exec context", [&]() {
-    const auto& all_exec_ctxts = sim->GetAllExecCtxts();
-    all_exec_ctxts[0]->SetupIterationAll(all_exec_ctxts);
-  });
-
-  Timing::Time("visualize", [&]() {
-    if (visualization_ != nullptr) {
-      visualization_->Visualize();
-    }
-  });
-  Timing::Time("neighbors", [&]() { env->Update(); });
-
   SetUpOps();
-  RunScheduledOps();
-  TearDownOps();
 
-  // finish updating sim objects
-  Timing::Time("Tear down exec context", [&]() {
-    const auto& all_exec_ctxts = sim->GetAllExecCtxts();
-    all_exec_ctxts[0]->TearDownIterationAll(all_exec_ctxts);
-  });
+  RunScheduledOps();
+
+  TearDownOps();
 }
 
 void Scheduler::Backup() {

src/core/scheduler.h
@@ -21,7 +21,9 @@
 #include <string>
 #include <unordered_map>
 #include <vector>
+
 #include "core/operation/operation.h"
+#include "core/util/timing_aggregator.h"
 
 namespace bdm {
 
@@ -83,6 +85,8 @@ class Scheduler {
 
   RootAdaptor* GetRootVisualization() { return root_visualization_; }
 
+  TimingAggregator* GetOpTimes();
+
  protected:
   uint64_t total_steps_ = 0;
 
@@ -94,7 +98,6 @@ class Scheduler {
   SimulationBackup* backup_ = nullptr;
   uint64_t restore_point_;
   std::chrono::time_point<Clock> last_backup_ = Clock::now();
-  VisualizationAdaptor* visualization_ = nullptr;  //!
   RootAdaptor* root_visualization_ = nullptr;      //!
 
   /// List of operations that are to be added in the upcoming timestep
@@ -114,6 +117,8 @@ class Scheduler {
   std::vector<std::string> protected_ops_;  //!
   /// Flag to check if scheduler has beeen initialized at least once
   bool initialized_ = false;
+  /// Tracks operations' execution times
+  TimingAggregator op_times_;
 
   /// Backup the simulation. Backup interval based on `Param::backup_interval_`
   void Backup();

src/core/diffusion_grid.h
@@ -938,6 +938,14 @@ class DiffusionGrid {
     return grid_dimensions_;
   }
 
+  std::array<int32_t, 3> GetGridSize() const {
+    std::array<int32_t, 3> ret;
+    ret[0] = grid_dimensions_[1] - grid_dimensions_[0];
+    ret[1] = grid_dimensions_[3] - grid_dimensions_[2];
+    ret[2] = grid_dimensions_[5] - grid_dimensions_[4];
+    return ret;
+  }
+
   const std::array<double, 7>& GetDiffusionCoefficients() const { return dc_; }
 
   bool IsInitialized() const { return initialized_; }

src/core/operation/default_ops.cc
@@ -5,6 +5,7 @@
 #include "core/operation/displacement_op_opencl.h"
 #include "core/operation/dividing_cell_op.h"
 #include "core/operation/operation.h"
+#include "core/visualization/visualization_adaptor.h"
 
 namespace bdm {
 
@@ -58,5 +59,69 @@ struct DiscretizationOp : public SimObjectOperationImpl {
 
 BDM_REGISTER_OP(DiscretizationOp, "discretization", kCpu);
 
+struct SetUpIterationOp : public StandaloneOperationImpl {
+  BDM_OP_HEADER(SetUpIterationOp);
+
+  void operator()() override {
+    auto* sim = Simulation::GetActive();
+    const auto& all_exec_ctxts = sim->GetAllExecCtxts();
+    all_exec_ctxts[0]->SetupIterationAll(all_exec_ctxts);
+  }
+};
+
+BDM_REGISTER_OP(SetUpIterationOp, "set up exec context", kCpu);
+
+struct TearDownIterationOp : public StandaloneOperationImpl {
+  BDM_OP_HEADER(TearDownIterationOp);
+
+  void operator()() override {
+    auto* sim = Simulation::GetActive();
+    const auto& all_exec_ctxts = sim->GetAllExecCtxts();
+    all_exec_ctxts[0]->TearDownIterationAll(all_exec_ctxts);
+  }
+};
+
+BDM_REGISTER_OP(TearDownIterationOp, "tear down exec context", kCpu);
+
+struct UpdateEnvironmentOp : public StandaloneOperationImpl {
+  BDM_OP_HEADER(UpdateEnvironmentOp);
+
+  void operator()() override {
+    auto* sim = Simulation::GetActive();
+    auto* env = sim->GetEnvironment();
+    env->Update();
+  }
+};
+
+BDM_REGISTER_OP(UpdateEnvironmentOp, "update environment", kCpu);
+
+struct VisualizationOp : public StandaloneOperationImpl {
+  BDM_OP_HEADER(VisualizationOp);
+
+  ~VisualizationOp() {
+    if (visualization_) {
+      delete visualization_;
+    }
+  }
+
+  void operator()() override {
+    // Cannot be done in constructor because no Simulation object will be
+    // created statically
+    if (!initialized_) {
+      auto* param = Simulation::GetActive()->GetParam();
+      visualization_ =
+          VisualizationAdaptor::Create(param->visualization_engine_);
+      initialized_ = true;
+    }
+    if (visualization_ != nullptr) {
+      visualization_->Visualize();
+    }
+  }
+
+  VisualizationAdaptor* visualization_ = nullptr;
+  bool initialized_ = false;
+};
+
+BDM_REGISTER_OP(VisualizationOp, "visualize", kCpu);
 
 }  // namespace bdm

src/core/operation/op_timer.h
@@ -31,8 +31,9 @@ struct OpTimer {
   template <typename Container>
   void operator()(Container* cells, uint16_t numa_node, uint16_t type_idx) {
     auto* param = Simulation::GetActive()->GetParam();
+    auto* agg = Simulation::GetActive()->GetScheduler()->GetOpTimes();
     if (param->statistics_) {
-      Timing timer(timer_msg_, &gStatistics);
+      Timing timer(timer_msg_, agg);
       operation_(cells, numa_node, type_idx);
     } else {
       operation_(cells, numa_node, type_idx);

src/core/resource_manager.cc
@@ -294,16 +294,7 @@ void ResourceManager::SortAndBalanceNumaNodes() {
   ApplyOnAllElementsParallel(functor);
 
   if (Simulation::GetActive()->GetParam()->debug_numa_) {
-    DebugNuma();
-  }
-}
-
-void ResourceManager::DebugNuma() const {
-  std::cout << "ResourceManager size of sim object containers\n" << std::endl;
-  uint64_t cnt = 0;
-  for (auto& numa_sos : sim_objects_) {
-    std::cout << "  numa node " << cnt++ << " size " << numa_sos.size()
-              << std::endl;
+    std::cout << *this << std::endl;
   }
 }
 

src/core/resource_manager.h
@@ -386,8 +386,19 @@ class ResourceManager {
   ThreadInfo* thread_info_ = ThreadInfo::GetInstance();  //!
 
   friend class SimulationBackup;
+  friend std::ostream& operator<<(std::ostream& os, const ResourceManager& rm);
   BDM_CLASS_DEF_NV(ResourceManager, 1);
 };
+
+inline std::ostream& operator<<(std::ostream& os, const ResourceManager& rm) {
+  os << "\033[1mSimulation objects per numa node\033[0m"
+     << std::endl;
+  uint64_t cnt = 0;
+  for (auto& numa_sos : rm.sim_objects_) {
+    os << "numa node " << cnt++ << " -> size: " << numa_sos.size() << std::endl;
+  }
+  return os;
+}
 
 }  // namespace bdm
 

src/core/simulation.cc
@@ -19,6 +19,7 @@
 #include <algorithm>
 #include <cmath>
 #include <cstdlib>
+#include <ostream>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -36,6 +37,7 @@
 #include "core/util/log.h"
 #include "core/util/string.h"
 #include "core/util/thread_info.h"
+#include "core/util/timing.h"
 #include "core/visualization/root/adaptor.h"
 #include "version.h"
 
@@ -118,13 +120,71 @@ void Simulation::Restore(Simulation&& restored) {
   InitializeOutputDir();
 }
 
+inline std::ostream& operator<<(std::ostream& os, Simulation& sim) {
+  std::vector<std::string> dg_names;
+  std::vector<int> dg_resolutions;
+  std::vector<std::array<int32_t, 3>> dg_dimensions;
+
+  sim.GetResourceManager()->ApplyOnAllDiffusionGrids([&](auto* dg) {
+    dg_names.push_back(dg->GetSubstanceName());
+    dg_resolutions.push_back(dg->GetResolution());
+    dg_dimensions.push_back(dg->GetGridSize());
+  });
+
+  os << std::endl;
+
+  os << "***********************************************" << std::endl;
+  os << std::endl;
+  os << "\033[1mSimulation name\t\t\t: \033[0m" << sim.GetUniqueName()
+     << std::endl;
+  os << "\033[1mNumber of iterations executed\t: \033[0m"
+     << sim.GetScheduler()->GetSimulatedSteps() << std::endl;
+  os << "\033[1mNumber of simulation objects\t: \033[0m"
+     << sim.GetResourceManager()->GetNumSimObjects() << std::endl;
+
+  if (dg_names.size() != 0) {
+    os << "\033[1mDiffusion grids\033[0m" << std::endl;
+    for (size_t i = 0; i < dg_names.size(); ++i) {
+      os << "  " << dg_names[i] << ":" << std::endl;
+      auto& dim = dg_dimensions[i];
+      os << "\t"
+         << "Resolution\t\t: " << dg_resolutions[i] << std::endl;
+      os << "\t"
+         << "Size\t\t\t: " << dim[0] << " x " << dim[1] << " x " << dim[2]
+         << std::endl;
+    }
+  }
+
+  os << "\033[1mOutput directory\t\t: \033[0m" << sim.GetOutputDir()
+     << std::endl;
+  os << "  size\t\t\t\t: "
+     << gSystem->GetFromPipe(
+            Concat("du -sh ", sim.GetOutputDir(), " | cut -f1").c_str())
+     << std::endl;
+  os << std::endl;
+  os << "***********************************************" << std::endl;
+  os << *(sim.GetScheduler()->GetOpTimes()) << std::endl;
+  os << "***********************************************" << std::endl;
+  os << std::endl;
+  os << "\033[1mThread Info\033[0m" << std::endl;
+  os << *ThreadInfo::GetInstance();
+  os << std::endl;
+  os << "***********************************************" << std::endl;
+  os << std::endl;
+  os << *(sim.GetResourceManager());
+  os << std::endl;
+  os << "***********************************************" << std::endl;
+
+  return os;
+}
+
 Simulation::~Simulation() {
+  if (param_ != nullptr && param_->statistics_) {
+    std::cout << *this << std::endl;
+  }
+
   if (mem_mgr_) {
     mem_mgr_->SetIgnoreDelete(true);
-  }
-  if (param_ != nullptr && rm_ != nullptr && param_->debug_numa_) {
-    std::cout << "ThreadInfo:\n" << *ThreadInfo::GetInstance() << std::endl;
-    rm_->DebugNuma();
   }
   Simulation* tmp = nullptr;
   if (active_ != this) {
@@ -265,9 +325,9 @@ void Simulation::InitializeRuntimeParams(
   static bool read_env = false;
   if (!read_env) {
     // Read, only once, bdm.rootrc to set BioDynaMo-related settings for ROOT
-    std::stringstream ss;
-    ss << std::getenv("BDMSYS") << "/etc/bdm.rootrc";
-    gEnv->ReadFile(ss.str().c_str(), kEnvUser);
+    std::stringstream os;
+    os << std::getenv("BDMSYS") << "/etc/bdm.rootrc";
+    gEnv->ReadFile(os.str().c_str(), kEnvUser);
     read_env = true;
   }
 

commit 894912e036c14db1e111e58a6a9117fea1aa8a86

Fix execution order of visualization operation
To avoid artefacts, the visualization op must be called at the beginning
of a timestep or at the end after iteration tear down.
Otherwise, the ResourceManager doesn't contain simulation objects that
have been created during the iteration. This might cause artefacts.

Bug introduced in 2bd71f5986c801f4614b85c39fc9e5735cbd40c6

src/core/scheduler.cc
@@ -48,7 +48,6 @@ Scheduler::Scheduler() {
                                           "discretization",
                                           "distribute run displacement info",
                                           "diffusion",
-                                          "visualize",
                                           "load balancing"};
 
   // Schedule the default operations
@@ -56,6 +55,7 @@ Scheduler::Scheduler() {
     ScheduleOp(NewOperation(def_op));
   }
 
+  visualize_op_ = NewOperation("visualize");
   setup_iteration_op_ = NewOperation("set up iteration");
   teardown_iteration_op_ = NewOperation("tear down iteration");
   update_environment_op_ = NewOperation("update environment");
@@ -68,6 +68,7 @@ Scheduler::~Scheduler() {
   for (auto* op : all_ops_) {
     delete op;
   }
+  delete visualize_op_;
   delete update_environment_op_;
   delete setup_iteration_op_;
   delete teardown_iteration_op_;
@@ -208,6 +209,7 @@ void Scheduler::Execute() {
   TearDownOps();
   Timing::Time(teardown_iteration_op_->name_,
                [&]() { (*teardown_iteration_op_)(); });
+  Timing::Time(visualize_op_->name_, [&]() { (*visualize_op_)(); });
 }
 
 void Scheduler::Backup() {

src/core/scheduler.h
@@ -124,6 +124,7 @@ class Scheduler {
   std::vector<std::string> protected_ops_;  //!
   /// Tracks operations' execution times
   TimingAggregator op_times_;
+  Operation* visualize_op_ = nullptr;
   Operation* update_environment_op_ = nullptr;
   Operation* setup_iteration_op_ = nullptr;
   Operation* teardown_iteration_op_ = nullptr;

test/unit/core/scheduler_test.cc
@@ -360,7 +360,7 @@ TEST(SchedulerTest, GetOps) {
   sim.GetResourceManager()->push_back(new Cell(10));
   auto* scheduler = sim.GetScheduler();
 
-  std::vector<std::string> def_ops = {"visualize", "displacement", "diffusion",
+  std::vector<std::string> def_ops = {"displacement", "diffusion",
                                       "load balancing"};
 
   for (auto& def_op : def_ops) {
@@ -394,8 +394,7 @@ TEST(SchedulerTest, ScheduleOrder) {
       "update run displacement", "bound space",
       "biology module",          "displacement",
       "discretization",          "distribute run displacement info"};
-  std::vector<std::string> sa_ops = {"diffusion", "visualize",
-                                     "load balancing"};
+  std::vector<std::string> sa_ops = {"diffusion", "load balancing"};
 
   int i = 0;
   ASSERT_EQ(so_ops.size(), scheduler->GetListOfScheduledSimObjectOps().size());