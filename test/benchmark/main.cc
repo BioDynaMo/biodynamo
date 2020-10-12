6861d1a10462aaa28d66cf686a843f1343bfe8f3
RobertSparadrap <robert.harakaly@epitech.eu>
2020-10-12 11:46:57 +0200

    benchmarking for tumor_concept

--- test/benchmark/main.cc
+++ test/benchmark/main.cc
@@ -1,23 +1,4 @@
-#include <benchmark/benchmark.h>
-/*
-static void BM_StringCreation(benchmark::State& state) {
-  for (auto _ : state)
-    std::string empty_string;
-}
-// Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
-
-// Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
-  std::string x = "hello";
-  for (auto _ : state)
-    std::string copy(x);
-}
-BENCHMARK(BM_StringCopy);
-
-BENCHMARK_MAIN();
-*/
-
+#include <../../build/benchmark/src/gbench/include/benchmark/benchmark.h>
 #include "../../demo/tumor_concept/src/tumor_concept.h"
 //#include "../../demo/soma_clustering/src/soma_clustering.h"
 
