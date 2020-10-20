35d3ce6a34deef604e4caba0cabb3d2574b4eb7b
Lukas Breitwieser <lukas.johannes.breitwieser@cern.ch>
2020-10-20 10:24:33 +0200

    Update version logic (#136)
    * Add .mailmap file
    
    This file is used by git to coalesce together commits by the same person
    in the shortlog, where their name and/or email address was spelled differently.
    
    * Update version logic

--- util/version/version.h
+++ util/version/version.h
@@ -18,33 +18,32 @@
 #include <cstdint>
 
 /// Version string as obtained from `git describe --tags`
-/// e.g.: v0.1.0-10-g91aade4
+/// e.g.: v1.0.123-g1234abc
 /// vMAJOR.MINOR.PATH-ADDITIONAL_COMMITS-gCOMMIT_ID
 #define BDM_RELEASE "@VERSION@"
 
 /// These macros can be used in the following way:
 ///
-///    #if BDM_VERSION_CODE >= BDM_VERSION(1,0,0,0)
+///    #if BDM_VERSION_CODE >= BDM_VERSION(1,0,0)
 ///       #include <newheader.h>
 ///    #else
 ///       #include <oldheader.h>
 ///    #endif
-#define BDM_VERSION(major, minor, patch, additional_commits) \
-  (static_cast<uint64_t>(major) << 48) +                     \
-  (static_cast<uint64_t>(minor) << 32) +                     \
-  (static_cast<uint64_t>(patch) << 16) +                     \
-   static_cast<uint64_t>(additional_commits)
+#define BDM_VERSION(major, minor, patch) \
+  (static_cast<uint64_t>(major) << 32) +                     \
+  (static_cast<uint64_t>(minor) << 16) +                     \
+   static_cast<uint64_t>(patch)
+
 #define BDM_VERSION_CODE                                         \
-  BDM_VERSION(@VERSION_MAJOR@, @VERSION_MINOR@, @VERSION_PATCH@, \
-              @VERSION_ADDITIONAL_COMMITS@)
+  BDM_VERSION(@VERSION_MAJOR@, @VERSION_MINOR@, @VERSION_PATCH@)
 
 namespace bdm {
 
 class Version {
  public:
   /// Returns version string as obtained from `git describe --tags`
-  /// e.g.: v0.1.0-10-g91aade4`
-  /// vMAJOR.MINOR.PATCH-ADDITIONAL_COMMITS-gCOMMIT_ID
+  /// e.g.: v1.0.123-g1234abc
+  /// vMAJOR.MINOR-PATCH-gCOMMIT_ID
   static const char *String() { return BDM_RELEASE; }
   static uint64_t Code() { return BDM_VERSION_CODE; }
 };
