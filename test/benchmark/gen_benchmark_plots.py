ef71faebad7f2935c97d1944f7e4a8bae76ad6cb
RobertSparadrap <robert.harakaly@epitech.eu>
2020-10-23 16:26:54 +0200

    average

--- test/benchmark/gen_benchmark_plots.py
+++ test/benchmark/gen_benchmark_plots.py
@@ -6,21 +6,45 @@ import sys
 from math import *
 import json
 
+
+def data_cpu(name_demo):
+    nb = len(name_demo)
+    a = name_demo
+    cpu = [0]*7
+    i = 0
+    j = 0
+    while i != 27:
+        b = data["benchmarks"][i]["name"]
+        if a[:nb] == b[:nb]:
+            cpu[j] = data["benchmarks"][i]["cpu_time"]
+            j+=1
+        i+=1
+    return cpu
+
+def graph(name_demo):
+    cpu = data_cpu(name_demo)
+    moy = 0
+    h = 0
+    tmp = 0
+    while h < 7:
+        tmp = cpu[h] + tmp
+        h += 1
+    moy = tmp / 7
+#    plt.plot(range(7), cpu, 'bo-')
+    fig, ax = plt.subplots()
+ #   plt.plot(range(1), moy, 'bo-')
+    xlabels = ['str']
+    ax.plot([1], moy, 'bo-')
+    ax.set_title(name_demo)
+    ax.set_xticks([1])
+    ax.set_xticklabels(xlabels, rotation=40)
+    plt.show()
+    return
+
 with open("results.json", "r") as read_file:
     data = json.load(read_file)
 
-a = "SomaClustering1"
-cpu = [0]*7
-i = 0
-j = 0
-while i != 27:
-    b = data["benchmarks"][i]["name"]
-    print(b[:15])
-    if a[:15] == b[:15]:
-        plt.title("Soma_clustering1")
-        cpu[j] = data["benchmarks"][i]["cpu_time"]
-        j+=1
-    i+=1
-plt.plot(range(7), cpu, 'bo-')
-print(cpu)
-plt.show()
+graph("SomaClustering1")
+#graph("SomaClustering0")
+#graph("TumorConcept1")
+#graph("TumorConcept0")
