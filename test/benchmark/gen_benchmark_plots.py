bba20fe9a36960166b343d0a3f386013e782cef8
RobertSparadrap <robert.harakaly@epitech.eu>
2020-10-30 16:55:13 +0100

    iteration

--- test/benchmark/gen_benchmark_plots.py
+++ test/benchmark/gen_benchmark_plots.py
@@ -7,13 +7,13 @@ from math import *
 import json
 
 
-def data_cpu(name_demo):
-    cpu = [0]*7
+def data_cpu(name_demo, iteration):
+    cpu = [0]*iteration
     i = 0
     j = 0
     a = name_demo
     nb = len(name_demo)
-    while i != 7:
+    while i != iteration:
         b = data["benchmarks"][i]["name"]
         if a[:nb] == b[:nb]:
             cpu[j] = data["benchmarks"][i]["cpu_time"]
@@ -21,15 +21,15 @@ def data_cpu(name_demo):
         i+=1
     return cpu
 
-def graph(name_demo):
-    cpu = data_cpu(name_demo)
+def graph(name_demo, iteration):
+    cpu = data_cpu(name_demo, iteration)
     moy = 0
     h = 0
     tmp = 0
-    while h < 7:
+    while h < iteration:
         tmp = cpu[h] + tmp
         h += 1
-    moy = tmp / 7
+    moy = tmp / iteration
     fig, ax = plt.subplots()
     xlabels = ['str']
     ax.plot([1], moy, 'bo-')
@@ -39,10 +39,10 @@ def graph(name_demo):
     plt.show()
     return
 
-def plot(name_demo):
+def plot(name_demo, iteration):
     with open("results.json", "r") as read_file:
         data = json.load(read_file)
-    graph(name_demo)
+    graph(name_demo, iteration)
 
 def name(i):
     name_data = data["benchmarks"][i]["name"]
@@ -65,11 +65,26 @@ def names():
     name_datas = name_datas[:j]
     return name_datas, j
 
+def iteration():
+    tmp = [0]
+    i = 0
+    x = 0
+    j = 0
+    while i != len(data["benchmarks"]):
+        tmp = name(i)
+        if tmp == name(i+1):
+            j+=1
+            i+=1
+        else:
+            break
+    return j+1
+
 def main():
     i = 0
+    it = iteration()
     name_datas, j = names()
     while i < j:
-        plot(name_datas[i])
+        plot(name_datas[i], it)
         i += 1
     return
 
