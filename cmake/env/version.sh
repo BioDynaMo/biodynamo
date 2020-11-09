af7fbfd4048c1ce61ba33688456246d1aa02e0a0
Robert Harakaly <r.harakaly@cern.ch>
2020-11-09 12:22:43 +0100

    script that call the python code in build

--- cmake/env/version.sh
+++ cmake/env/version.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 VERSION=`git log | awk '/commit/ {print $2}' | awk "NR==1{print;exit}"`
-../test/benchmark/gen_benchmark_plots.py benchmark/results.json $VERSION
+./gen_benchmark_plots.py benchmark/results.json $VERSION
