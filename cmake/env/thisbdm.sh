846b7fc8ce38bbf0b3a9bb69189a8b188aab389f
Fons Rademakers <Fons.Rademakers@cern.ch>
2020-12-14 19:06:43 +0100

    Revert mistake (was not sourcing script and thrown off by return warning).

--- cmake/env/thisbdm.sh
+++ cmake/env/thisbdm.sh
@@ -590,11 +590,11 @@ _source_thisbdm()
 if _source_thisbdm; then
   _bdm_ok "[OK] You have successfully sourced BioDynaMo's environment."
   _thisbdm_cleanup
-  exit 0
+  return 0
 else
   _bdm_err "[ERR] BioDynaMo's environment could not be sourced."
   # traps don't particularly work well (for this use-case)
   # in zsh, so we'll repeat ourselves just this once
   _thisbdm_cleanup
-  exit 1
+  return 1
 fi
