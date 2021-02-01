896c7b6965ae482ca61c58b91010952c9258558e
Fons Rademakers <Fons.Rademakers@cern.ch>
2021-02-01 04:52:47 +0100

    Download the macOS versions of the paraview and qt pre-build tar files.

--- cmake/external/ParaView.cmake
+++ cmake/external/ParaView.cmake
@@ -4,13 +4,20 @@ SET(PARAVIEW_SOURCE_DIR "${CMAKE_THIRD_PARTY_DIR}/paraview")
 
 # Quickfix until the file on the CERN's repository is not renamed correctly
 if (APPLE)
-    SET(PARAVIEW_DOWNLOAD_NAME paraview.tar.gz)
+   EXECUTE_PROCESS(COMMAND sw_vers "-productVersion"
+                   COMMAND cut -d . -f 1-2
+                   OUTPUT_VARIABLE MACOS_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
+   EXECUTE_PROCESS(COMMAND arch
+                   OUTPUT_VARIABLE MACOS_ARCH OUTPUT_STRIP_TRAILING_WHITESPACE)
+   SET(DETECTED_OS_VERS ${DETECTED_OS}-${MACOS_VERSION}-${MACOS_ARCH})
+   SET(PARAVIEW_TAR_FILE paraview_v5.9.0_${DETECTED_OS_VERS}_default.tar.gz)
 ELSE()
-    SET(PARAVIEW_DOWNLOAD_NAME paraview-v5.8.0.tar.gz)
+   SET(DETECTED_OS_VERS ${DETECTED_OS})
+   SET(PARAVIEW_TAR_FILE ${DETECTED_OS_VERS}/paraview-v5.8.0.tar.gz)
 ENDIF()
 
 download_verify_extract(
-  http://cern.ch/biodynamo-lfs/third-party/${DETECTED_OS}/${PARAVIEW_DOWNLOAD_NAME}
+  http://cern.ch/biodynamo-lfs/third-party/${PARAVIEW_TAR_FILE}
   ${PARAVIEW_SOURCE_DIR}
   ${${DETECTED_OS}-ParaView}
 )
--- cmake/external/Qt.cmake
+++ cmake/external/Qt.cmake
@@ -2,8 +2,21 @@ include(utils)
 
 SET(QT_SOURCE_DIR "${CMAKE_THIRD_PARTY_DIR}/qt")
 
+IF (APPLE)
+   EXECUTE_PROCESS(COMMAND sw_vers "-productVersion"
+                   COMMAND cut -d . -f 1-2
+                   OUTPUT_VARIABLE MACOS_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
+   EXECUTE_PROCESS(COMMAND arch
+                   OUTPUT_VARIABLE MACOS_ARCH OUTPUT_STRIP_TRAILING_WHITESPACE)
+   set(DETECTED_OS_VERS ${DETECTED_OS}-${MACOS_VERSION}-${MACOS_ARCH})
+   set(QT_TAR_FILE qt_v5.12.10_${DETECTED_OS_VERS}.tar.gz)
+ELSE()
+   set(DETECTED_OS_VERS ${DETECTED_OS})
+   set(QT_TAR_FILE ${DETECTED_OS_VERS}/qt.tar.gz)
+ENDIF()
+
 download_verify_extract(
-  http://cern.ch/biodynamo-lfs/third-party/${DETECTED_OS}/qt.tar.gz
+  http://cern.ch/biodynamo-lfs/third-party/${QT_TAR_FILE}
   ${QT_SOURCE_DIR}
   ${${DETECTED_OS}-Qt}
 )
