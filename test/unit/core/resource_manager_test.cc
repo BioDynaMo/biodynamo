b7c0aa1c2c0677ac1c5b804c0b07fe1a82e407e4
Tobias Duswald <tobias.duswald@cern.ch>
2021-04-06 11:42:27 +0200

    Restructure while loops
    Observed Errors:
    resource_manager_test.cc:110:16: runtime error: unsigned integer overflow:
    0 - 1 cannot be represented in type 'uint64_t' (aka 'unsigned long long')
    resource_manager_test.cc:118:13: runtime error: unsigned integer overflow:
    0 - 1 cannot be represented in type 'uint64_t' (aka 'unsigned long long')
    
    These errors probably won't impact anything but these changes avoid
    error messages in the future.

--- test/unit/core/resource_manager_test.cc
+++ test/unit/core/resource_manager_test.cc
@@ -107,16 +107,18 @@ TEST(ResourceManagerTest, Defragmentation) {
 
   // remove enough agents to drop below the low watermark
   uint64_t remove = std::ceil(cnt * 0.7) + 1;
-  while (remove-- != 0) {
+  while (remove != 0) {
     rm->RemoveAgent(AgentUid(remove));
+    remove--;
   }
   rm->EndOfIteration();
   EXPECT_TRUE(agent_uid_generator->IsInDefragmentationMode());
 
   // add enough agents to exceed the high watermark
   uint64_t add = std::ceil(cnt * 0.5) + 1;
-  while (add-- != 0) {
+  while (add != 0) {
     rm->AddAgent(new TestAgent());
+    add--;
   }
   rm->EndOfIteration();
   EXPECT_FALSE(agent_uid_generator->IsInDefragmentationMode());
