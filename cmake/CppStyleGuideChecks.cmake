9118a5773e02e3ed30fa2255fac7169411fbc611
Tobias Duswald <tobias.duswald@cern.ch>
2021-04-13 12:41:56 +0200

    Remove repetitive function argument

--- cmake/CppStyleGuideChecks.cmake
+++ cmake/CppStyleGuideChecks.cmake
@@ -22,69 +22,71 @@ add_executable(clang-tidy-header-helper EXCLUDE_FROM_ALL ${clang_tidy_header_hel
 target_include_directories(clang-tidy-header-helper PUBLIC "${CMAKE_SOURCE_DIR}/test")
 
 # -------------------- "make format*", "make show-format*" and "make check-format*" targets ------------
-function(add_clang_format_target make_target_id get_files_cmd file_filter)
+# @param file_filter: Filter that is passed to get-src-files scirpt (all|staged|changed)
+function(add_clang_format_target make_target_id file_filter)
   if (${CLANG_FORMAT_FOUND})
     add_custom_target(${make_target_id}
       COMMAND ${BUILD_SUPPORT_DIR}/run-clang-format.sh ${CMAKE_CURRENT_SOURCE_DIR} 
-      ${CLANG_FORMAT_BIN} 1 `${get_files_cmd} ${PROJECT_SOURCE_DIR} ${file_filter} cc h`
+      ${CLANG_FORMAT_BIN} 1 `${GET_SRC_FILES} ${PROJECT_SOURCE_DIR} ${file_filter} cc h`
       WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
       COMMENT "Run clang-format on selected files and update them in-place")
 
     add_custom_target(show-${make_target_id}
       COMMAND ${BUILD_SUPPORT_DIR}/run-clang-format.sh ${CMAKE_CURRENT_SOURCE_DIR} 
-      ${CLANG_FORMAT_BIN} 2 `${get_files_cmd} ${PROJECT_SOURCE_DIR} ${file_filter} cc h`
+      ${CLANG_FORMAT_BIN} 2 `${GET_SRC_FILES} ${PROJECT_SOURCE_DIR} ${file_filter} cc h`
       WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
       COMMENT "Run clang-format on selected files and display differences")
 
     add_custom_target(check-${make_target_id}
       COMMAND ${BUILD_SUPPORT_DIR}/run-clang-format.sh ${CMAKE_CURRENT_SOURCE_DIR} 
-      ${CLANG_FORMAT_BIN} 0 `${get_files_cmd} ${PROJECT_SOURCE_DIR} ${file_filter} cc h`
+      ${CLANG_FORMAT_BIN} 0 `${GET_SRC_FILES} ${PROJECT_SOURCE_DIR} ${file_filter} cc h`
       WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
       COMMENT "Run clang-format on selected files. Fails if any file needs to be reformatted")
   endif()
 endfunction(add_clang_format_target)
 
 # -------------------- "make tidy*", "make show-tidy*" and "make check-tidy*" targets ------------
-function(add_clang_tidy_target make_target_id get_files_cmd file_filter)
+# @param file_filter: Filter that is passed to get-src-files scirpt (all|staged|changed)
+function(add_clang_tidy_target make_target_id file_filter)
   if (${CLANG_TIDY_FOUND})
     add_custom_target(${make_target_id}
       COMMAND ${BUILD_SUPPORT_DIR}/run-clang-tidy.sh ${CLANG_TIDY_BIN} ${clang_tidy_header_helper}  ${CMAKE_BINARY_DIR}/compile_commands.json 1
-              `${get_files_cmd} ${PROJECT_SOURCE_DIR} ${file_filter} cc h | grep -v -F -f ${PROJECT_SOURCE_DIR}/.clang-tidy-ignore`
+              `${GET_SRC_FILES} ${PROJECT_SOURCE_DIR} ${file_filter} cc h | grep -v -F -f ${PROJECT_SOURCE_DIR}/.clang-tidy-ignore`
       WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
       COMMENT "Run clang-tidy on selected files and attempt to fix any warning automatically")
 
     add_custom_target(show-${make_target_id}
       COMMAND ${BUILD_SUPPORT_DIR}/run-clang-tidy.sh ${CLANG_TIDY_BIN} ${clang_tidy_header_helper} ${CMAKE_BINARY_DIR}/compile_commands.json 2
-              `${get_files_cmd} ${PROJECT_SOURCE_DIR} ${file_filter} cc h | grep -v -F -f ${PROJECT_SOURCE_DIR}/.clang-tidy-ignore`
+              `${GET_SRC_FILES} ${PROJECT_SOURCE_DIR} ${file_filter} cc h | grep -v -F -f ${PROJECT_SOURCE_DIR}/.clang-tidy-ignore`
       WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
       COMMENT "Run clang-tidy on selected files and display errors.")
 
     add_custom_target(check-${make_target_id}
       COMMAND ${BUILD_SUPPORT_DIR}/run-clang-tidy.sh ${CLANG_TIDY_BIN} ${clang_tidy_header_helper} ${CMAKE_BINARY_DIR}/compile_commands.json 0
-              `${get_files_cmd} ${PROJECT_SOURCE_DIR} ${file_filter} cc h | grep -v -F -f ${PROJECT_SOURCE_DIR}/.clang-tidy-ignore`
+              `${GET_SRC_FILES} ${PROJECT_SOURCE_DIR} ${file_filter} cc h | grep -v -F -f ${PROJECT_SOURCE_DIR}/.clang-tidy-ignore`
       WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
       COMMENT "Run clang-tidy on selected files. Fails if errors are found.")
   endif()
 endfunction(add_clang_tidy_target)
 
 # # -------------------- "make yapf*", "make show-yapf*" and "make check-yapf*" targets ------------
-# function(add_yapf_target make_target_id get_files_cmd)
+# function(add_yapf_target make_target_id file_filter)
 #   if (${YAPF_FOUND})
 #     add_custom_target(${make_target_id}
 #       COMMAND ${BUILD_SUPPORT_DIR}/run-yapf.sh ${YAPF_BIN} ${yapf_header_helper}  ${CMAKE_BINARY_DIR}/compile_commands.json 1
-#               `${get_files_cmd} ${PROJECT_SOURCE_DIR} | grep -v -F -f ${PROJECT_SOURCE_DIR}/.yapf-ignore`
+#               `${GET_SRC_FILES} ${PROJECT_SOURCE_DIR} ${file_filter} py | grep -v -F -f ${PROJECT_SOURCE_DIR}/.yapf-ignore`
 #       WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
 #       COMMENT "Run yapf on selected files and attempt to fix any warning automatically")
 # 
 #     add_custom_target(show-${make_target_id}
 #       COMMAND ${BUILD_SUPPORT_DIR}/run-yapf.sh ${YAPF_BIN} ${yapf_header_helper} ${CMAKE_BINARY_DIR}/compile_commands.json 2
-#               `${get_files_cmd} ${PROJECT_SOURCE_DIR} | grep -v -F -f ${PROJECT_SOURCE_DIR}/.yapf-ignore`
+#               `${GET_SRC_FILES} ${PROJECT_SOURCE_DIR} ${file_filter} py | grep -v -F -f ${PROJECT_SOURCE_DIR}/.yapf-ignore`
 #       WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
 #       COMMENT "Run yapf on selected files and display errors.")
 # 
 #     add_custom_target(check-${make_target_id}
 #       COMMAND ${BUILD_SUPPORT_DIR}/run-yapf.sh ${YAPF_BIN} ${yapf_header_helper} ${CMAKE_BINARY_DIR}/compile_commands.json 0
-#               `${get_files_cmd} ${PROJECT_SOURCE_DIR} | grep -v -F -f ${PROJECT_SOURCE_DIR}/.yapf-ignore`
+#               `${GET_SRC_FILES} ${PROJECT_SOURCE_DIR} ${file_filter} py | grep -v -F -f ${PROJECT_SOURCE_DIR}/.yapf-ignore`
 #       WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
 #       COMMENT "Run yapf on selected files. Fails if errors are found.")
 #   endif()
@@ -92,9 +94,9 @@ endfunction(add_clang_tidy_target)
 
 # ------------------------------------------------------------------------------------------------
 
-function(add_cpplint_target make_target_id get_files_cmd file_filter)
+function(add_cpplint_target make_target_id file_filter)
   add_custom_target(${make_target_id}
-    COMMAND ${BUILD_SUPPORT_DIR}/run-cpplint.sh `${get_files_cmd} ${PROJECT_SOURCE_DIR} ${file_filter} cc h`
+    COMMAND ${BUILD_SUPPORT_DIR}/run-cpplint.sh `${GET_SRC_FILES} ${PROJECT_SOURCE_DIR} ${file_filter} cc h`
     WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
     COMMENT "Run cpplint on selected files. Fails if errors are found.")
 endfunction(add_cpplint_target)
@@ -117,24 +119,18 @@ if (GIT_FOUND)
   # - make tidy
   # - make check-cpplint-all
   if ("${HAVE_ORIGIN_REPOSITORY}" EQUAL "1")
-    # add_clang_format_target(format "${CHANGED_SRC_FILES_ORIGIN_MASTER}" )
-    # add_clang_tidy_target(tidy "${CHANGED_SRC_FILES_ORIGIN_MASTER}" )
-    # add_cpplint_target(check-cpplint "${CHANGED_SRC_FILES_ORIGIN_MASTER}" )
-    add_clang_format_target(format "${GET_SRC_FILES}" "changed")
-    add_clang_tidy_target(tidy "${GET_SRC_FILES}" "changed")
-    add_cpplint_target(check-cpplint "${GET_SRC_FILES}" "changed")
+    add_clang_format_target(format "changed")
+    add_clang_tidy_target(tidy "changed")
+    add_cpplint_target(check-cpplint "changed")
   else()
     MESSAGE(WARNING "\nWe did not detect any remote origin/master branch. Therefore, targets \
 like 'make format', 'make tidy' and 'make check-cpplint' will not be available. Use 'make format-all', 'make tidy-all' and 'make check-cpplint-all' instead. \
 If you wish to use 'make format'/'make tidy'/'make check-cpplint' try to add an origin/master branch using 'git remote add'.\n")
   endif()
 
-  # add_clang_format_target(format-staged "${STAGED_SRC_FILES}" )
-  # add_clang_tidy_target(tidy-staged "${STAGED_SRC_FILES}" )
-  # add_cpplint_target(check-cpplint-staged "${STAGED_SRC_FILES}" )
-  add_clang_format_target(format-staged "${GET_SRC_FILES}" "staged")
-  add_clang_tidy_target(tidy-staged "${GET_SRC_FILES}" "staged")
-  add_cpplint_target(check-cpplint-staged "${GET_SRC_FILES}" "staged")
+  add_clang_format_target(format-staged "staged")
+  add_clang_tidy_target(tidy-staged "staged")
+  add_cpplint_target(check-cpplint-staged "staged")
 
   # check submission
   add_custom_target(check-submission
@@ -148,10 +144,6 @@ If you wish to use 'make format'/'make tidy'/'make check-cpplint' try to add an 
              Failing tests and issues from cpplint must be fixed manually.")
 endif()
 
-#add_clang_format_target(format-all "${ALL_SRC_FILES}")
-#add_clang_tidy_target(tidy-all "${ALL_SRC_FILES}")
-#add_cpplint_target(check-cpplint-all "${ALL_SRC_FILES}")
-add_clang_format_target(format-all "${GET_SRC_FILES}" "all")
-add_clang_tidy_target(tidy-all "${GET_SRC_FILES}" "all")
-add_cpplint_target(check-cpplint-all "${GET_SRC_FILES}" "all")
-
+add_clang_format_target(format-all "all")
+add_clang_tidy_target(tidy-all "all")
+add_cpplint_target(check-cpplint-all "all")
