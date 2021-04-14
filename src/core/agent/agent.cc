BioDynaMo/biodynamo

commit 2a64431a1fef29dc1ba48ca6cffccc63b8e6569a

Fix undefined behavior continued (#164)
Reported by fsanitize flags using clang 12 on MacOS

Co-authored-by: Tobias Duswald <tobias.duswald@cern.ch>

src/core/agent/agent.cc
@@ -47,7 +47,6 @@ Agent::Agent(TRootIOCtor* io_ctor) {}
 Agent::Agent(const Agent& other)
     : uid_(other.uid_),
       box_idx_(other.box_idx_),
-      run_behavior_loop_idx_(other.run_behavior_loop_idx_),
       propagate_staticness_neighborhood_(
           other.propagate_staticness_neighborhood_),
       is_static_next_ts_(other.is_static_next_ts_) {
@@ -118,26 +117,43 @@ void Agent::SetBoxIdx(uint32_t idx) { box_idx_ = idx; }
 // ---------------------------------------------------------------------------
 // Behaviors
 
+/// Contains helper variables used to support removal of behaviors while
+/// `RunBehaviors` iterates over them.
+/// Iteration and removal must be performed by the *same* thread.
+struct RemoveBehaviorHelper {
+  thread_local static int idx;
+  thread_local static const Agent* agent;
+};
+thread_local int RemoveBehaviorHelper::idx = 0;
+thread_local const Agent* RemoveBehaviorHelper::agent = nullptr;
+
 void Agent::AddBehavior(Behavior* behavior) { behaviors_.push_back(behavior); }
 
 void Agent::RemoveBehavior(const Behavior* behavior) {
-  for (unsigned int i = 0; i < behaviors_.size(); i++) {
+  for (int i = 0; i < behaviors_.size(); i++) {
     if (behaviors_[i] == behavior) {
       delete behavior;
       behaviors_.erase(behaviors_.begin() + i);
-      // if behavior was before or at the current run_behavior_loop_idx_,
-      // correct it by subtracting one.
-      run_behavior_loop_idx_ -= i > run_behavior_loop_idx_ ? 0 : 1;
+      // only modify RemoveBehaviorHelper::idx if RemoveBehaviorHelper::agent
+      // matches
+      if (RemoveBehaviorHelper::agent == this) {
+        // if behavior was before or at the current RemoveBehaviorHelper::idx,
+        // correct it by subtracting one.
+        RemoveBehaviorHelper::idx -= i > RemoveBehaviorHelper::idx ? 0 : 1;
+      }
     }
   }
 }
 
 void Agent::RunBehaviors() {
-  for (run_behavior_loop_idx_ = 0; run_behavior_loop_idx_ < behaviors_.size();
-       ++run_behavior_loop_idx_) {
-    auto* behavior = behaviors_[run_behavior_loop_idx_];
+  RemoveBehaviorHelper::agent = this;
+  for (RemoveBehaviorHelper::idx = 0;
+       RemoveBehaviorHelper::idx < behaviors_.size();
+       ++RemoveBehaviorHelper::idx) {
+    auto* behavior = behaviors_[RemoveBehaviorHelper::idx];
     behavior->Run(this);
   }
+  RemoveBehaviorHelper::agent = nullptr;
 }
 
 const InlineVector<Behavior*, 2>& Agent::GetAllBehaviors() const {

src/core/agent/agent.h
@@ -249,10 +249,6 @@ class Agent {
  private:
   Spinlock lock_;  //!
 
-  /// Helper variable used to support removal of behaviors while
-  /// `RunBehaviors` iterates over them.
-  uint32_t run_behavior_loop_idx_ = 0;
-
   /// If an agent is static, we should not compute the mechanical forces
   bool is_static_ = false;  //!
   /// If an agent becomes non-static (i.e. it moved or grew), we should set this

src/core/diffusion/diffusion_grid.cc
@@ -102,17 +102,16 @@ void DiffusionGrid::Update() {
     }
   }
 
-  // Calculate by how many boxes each dimension has grown
+  // Calculate new_dimension_length and new_resolution
   int new_dimension_length = grid_dimensions_[1] - grid_dimensions_[0];
   int new_resolution = std::ceil(new_dimension_length / box_length_);
-  int growth = new_resolution - resolution_;
 
-  if (growth > 0) {
+  if (new_resolution > resolution_) {
     // Store the old number of boxes along each axis for comparison
     size_t tmp_resolution = resolution_;
 
-    // Increase number of boxes along axis accordingly
-    resolution_ += growth;
+    // Set new resolution
+    resolution_ = new_resolution;
 
     // We need to maintain the parity of the number of boxes along each
     // dimension, otherwise copying of the substances to the increases grid
@@ -136,10 +135,6 @@ void DiffusionGrid::Update() {
     total_num_boxes_ = resolution_ * resolution_ * resolution_;
 
     CopyOldData(tmp_c1, tmp_gradients, tmp_resolution);
-
-    assert(total_num_boxes_ >=
-               (tmp_resolution * tmp_resolution * tmp_resolution) &&
-           "The diffusion grid tried to shrink! It can only become larger");
   }
 }
 

src/core/environment/morton_order.cc
@@ -22,27 +22,24 @@ namespace bdm {
 template <typename T>
 class Stack {
  public:
-  Stack(uint64_t max_elements) {
-    data_.resize(max_elements);
-    top_--;
-  }
+  Stack(uint64_t max_elements) { data_.resize(max_elements); }
 
   void Push(const T& el) {
     data_[++top_] = el;
-    assert(top_ < data_.size());
+    assert(top_ < static_cast<int64_t>(data_.size()));
   }
 
   const T& Pop() {
-    assert(top_ < data_.size());
+    assert(top_ < static_cast<int64_t>(data_.size()));
     return data_[top_--];
   }
 
   T& Top() { return data_[top_]; }
-  uint64_t Size() const { return top_ + 1; }
+  uint64_t Size() const { return static_cast<uint64_t>(top_ + 1); }
 
  private:
   std::vector<T> data_;
-  uint64_t top_ = 0;
+  int64_t top_ = -1;
 };
 
 // -----------------------------------------------------------------------------

src/core/environment/uniform_grid_environment.h
@@ -570,8 +570,6 @@ class UniformGridEnvironment : public Environment {
 
   uint64_t GetNumBoxes() const { return boxes_.size(); }
 
-  uint32_t GetBoxLength() { return box_length_; }
-
   std::array<uint64_t, 3> GetBoxCoordinates(size_t box_idx) const {
     std::array<uint64_t, 3> box_coord;
     box_coord[2] = box_idx / num_boxes_xy_;
@@ -702,7 +700,7 @@ class UniformGridEnvironment : public Environment {
   /// This is used to decide if boxes should be reinitialized
   uint32_t timestamp_ = 0;
   /// Length of a Box
-  uint32_t box_length_ = 1;
+  int32_t box_length_ = 1;
   /// Stores the number of boxes for each axis
   std::array<uint64_t, 3> num_boxes_axis_ = {{0}};
   /// Number of boxes in the xy plane (=num_boxes_axis_[0] * num_boxes_axis_[1])

src/core/memory/memory_manager.cc
@@ -44,10 +44,11 @@ Node* List::PopFront() {
       tail_ = nullptr;
     }
     --size_;
-    --nodes_before_skip_list_;
     if (skip_list_.back() == ret) {
       skip_list_.pop_back();
       nodes_before_skip_list_ = n_;
+    } else if (size_ != 0) {
+      --nodes_before_skip_list_;
     }
     return ret;
   }
@@ -281,7 +282,8 @@ void NumaPoolAllocator::InitializeNPages(List* tl_list, char* block,
 uint64_t NumaPoolAllocator::RoundUpTo(uint64_t number, uint64_t multiple) {
   assert((multiple & (multiple - 1)) == 0 && multiple &&
          "multiple must be a power of two and non-zero");
-  return (number + multiple - 1) & -multiple;
+  return (number + multiple - 1) &
+         (std::numeric_limits<uint64_t>::max() - (multiple - 1));
 }
 
 // -----------------------------------------------------------------------------

src/core/operation/default_ops.cc
@@ -30,7 +30,7 @@ BDM_REGISTER_OP(DiffusionOp, "diffusion", kCpu);
 
 // By default run load balancing only in the first iteration.
 BDM_REGISTER_OP_WITH_FREQ(LoadBalancingOp, "load balancing", kCpu,
-                          std::numeric_limits<std::size_t>::max());
+                          std::numeric_limits<uint32_t>::max());
 
 BDM_REGISTER_OP(MechanicalForcesOp, "mechanical forces", kCpu);
 

src/core/operation/operation_registry.cc
@@ -25,7 +25,7 @@ Operation *OperationRegistry::GetOperation(const std::string &op_name) {
 bool OperationRegistry::AddOperationImpl(const std::string &op_name,
                                          OpComputeTarget target,
                                          OperationImpl *impl,
-                                         size_t frequency) {
+                                         uint32_t frequency) {
   auto op = operations_.find(op_name);
   // If operation doesn't exist yet, make a new operation under given name
   if (op == operations_.end()) {

commit e767d6fbe2ef4785a3e37ba51faa750a13e19892

Fix performance regression
introduced in 2a64431a1fef29dc1ba48ca6cffccc63b8e6569a

src/core/agent/agent.cc
@@ -47,6 +47,7 @@ Agent::Agent(TRootIOCtor* io_ctor) {}
 Agent::Agent(const Agent& other)
     : uid_(other.uid_),
       box_idx_(other.box_idx_),
+      run_behavior_loop_idx_(other.run_behavior_loop_idx_),
       propagate_staticness_neighborhood_(
           other.propagate_staticness_neighborhood_),
       is_static_next_ts_(other.is_static_next_ts_) {
@@ -116,43 +117,26 @@ void Agent::SetBoxIdx(uint32_t idx) { box_idx_ = idx; }
 // ---------------------------------------------------------------------------
 // Behaviors
 
-/// Contains helper variables used to support removal of behaviors while
-/// `RunBehaviors` iterates over them.
-/// Iteration and removal must be performed by the *same* thread.
-struct RemoveBehaviorHelper {
-  thread_local static int idx;
-  thread_local static const Agent* agent;
-};
-thread_local int RemoveBehaviorHelper::idx = 0;
-thread_local const Agent* RemoveBehaviorHelper::agent = nullptr;
-
 void Agent::AddBehavior(Behavior* behavior) { behaviors_.push_back(behavior); }
 
 void Agent::RemoveBehavior(const Behavior* behavior) {
-  for (int i = 0; i < behaviors_.size(); i++) {
+  for (unsigned int i = 0; i < behaviors_.size(); i++) {
     if (behaviors_[i] == behavior) {
       delete behavior;
       behaviors_.erase(behaviors_.begin() + i);
-      // only modify RemoveBehaviorHelper::idx if RemoveBehaviorHelper::agent
-      // matches
-      if (RemoveBehaviorHelper::agent == this) {
-        // if behavior was before or at the current RemoveBehaviorHelper::idx,
-        // correct it by subtracting one.
-        RemoveBehaviorHelper::idx -= i > RemoveBehaviorHelper::idx ? 0 : 1;
-      }
+      // if behavior was before or at the current run_behavior_loop_idx_,
+      // correct it by subtracting one.
+      run_behavior_loop_idx_ -= i > run_behavior_loop_idx_ ? 0 : 1;
     }
   }
 }
 
 void Agent::RunBehaviors() {
-  RemoveBehaviorHelper::agent = this;
-  for (RemoveBehaviorHelper::idx = 0;
-       RemoveBehaviorHelper::idx < behaviors_.size();
-       ++RemoveBehaviorHelper::idx) {
-    auto* behavior = behaviors_[RemoveBehaviorHelper::idx];
+  for (run_behavior_loop_idx_ = 0; run_behavior_loop_idx_ < behaviors_.size();
+       ++run_behavior_loop_idx_) {
+    auto* behavior = behaviors_[run_behavior_loop_idx_];
     behavior->Run(this);
   }
-  RemoveBehaviorHelper::agent = nullptr;
 }
 
 const InlineVector<Behavior*, 2>& Agent::GetAllBehaviors() const {

src/core/agent/agent.h
@@ -249,6 +249,10 @@ class Agent {
  private:
   Spinlock lock_;  //!
 
+  /// Helper variable used to support removal of behaviors while
+  /// `RunBehaviors` iterates over them.
+  uint16_t run_behavior_loop_idx_ = 0;
+
   /// If an agent is static, we should not compute the mechanical forces
   bool is_static_ = false;  //!
   /// If an agent becomes non-static (i.e. it moved or grew), we should set this