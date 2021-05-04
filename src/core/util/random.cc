d209f7094b4c5c2542df3f0dd28b2ac841e75841
Lukas Johannes Breitwieser <lukas.johannes.breitwieser@cern.ch>
2021-05-04 12:12:49 +0200

    Improve random number generation
    * Allow usage of different PRNG engine (default Mersenne Twister)
    * Add functionality to create PRNG following a user-defined
      distribution. (Based on ROOT)
    * Create common interface `DistributionRng` to allow simulation
      parameterization with different distributions.

--- src/core/util/random.cc
+++ src/core/util/random.cc
@@ -13,29 +13,222 @@
 // -----------------------------------------------------------------------------
 
 #include "core/util/random.h"
-
-#include <cmath>
-#include <random>
+#include <TF1.h>
+#include <TRandom3.h>
+#include "core/simulation.h"
 
 namespace bdm {
 
-Random::Random() {}
+// -----------------------------------------------------------------------------
+Random::Random() : generator_(new TRandom3()) {}
 
+// -----------------------------------------------------------------------------
+Random::Random(TRootIOCtor*) {}
+
+// -----------------------------------------------------------------------------
+Random::Random(const Random& other)
+    : generator_(static_cast<TRandom*>(other.generator_->Clone())) {}
+
+// -----------------------------------------------------------------------------
+Random::~Random() {
+  if (generator_) {
+    delete generator_;
+  }
+  for (auto& el : udd_tf1_map_) {
+    delete el.second;
+  }
+}
+
+// -----------------------------------------------------------------------------
 Random& Random::operator=(const Random& other) {
-  generator_ = other.generator_;
+  if (&other != this) {
+    if (generator_) {
+      delete generator_;
+    }
+    generator_ = static_cast<TRandom*>(other.generator_->Clone());
+  }
   return *this;
 }
 
-double Random::Uniform(double max) { return generator_.Uniform(max); }
+// -----------------------------------------------------------------------------
+double Random::Uniform(double max) { return generator_->Uniform(max); }
+
+// -----------------------------------------------------------------------------
 double Random::Uniform(double min, double max) {
-  return generator_.Uniform(min, max);
+  return generator_->Uniform(min, max);
 }
 
+// -----------------------------------------------------------------------------
 double Random::Gaus(double mean, double sigma) {
-  return generator_.Gaus(mean, sigma);
+  return generator_->Gaus(mean, sigma);
 }
 
-void Random::SetSeed(uint64_t seed) { generator_.SetSeed(seed); }
-uint64_t Random::GetSeed() const { return generator_.GetSeed(); }
+// -----------------------------------------------------------------------------
+double Random::Exp(double tau) { return generator_->Exp(tau); }
+
+// -----------------------------------------------------------------------------
+double Random::Landau(double mean, double sigma) {
+  return generator_->Landau(mean, sigma);
+}
+
+// -----------------------------------------------------------------------------
+double Random::PoissonD(double mean) { return generator_->PoissonD(mean); }
+
+// -----------------------------------------------------------------------------
+double Random::BreitWigner(double mean, double gamma) {
+  return generator_->BreitWigner(mean, gamma);
+}
+
+// -----------------------------------------------------------------------------
+unsigned Random::Integer(int max) { return generator_->Integer(max); }
+
+// -----------------------------------------------------------------------------
+int Random::Binomial(int ntot, double prob) {
+  return generator_->Binomial(ntot, prob);
+}
+
+// -----------------------------------------------------------------------------
+int Random::Poisson(double mean) { return generator_->Poisson(mean); }
+
+// -----------------------------------------------------------------------------
+MathArray<double, 2> Random::Circle(double r) {
+  MathArray<double, 2> ret;
+  generator_->Circle(ret[0], ret[1], r);
+  return ret;
+}
+
+// -----------------------------------------------------------------------------
+MathArray<double, 3> Random::Sphere(double r) {
+  MathArray<double, 3> ret;
+  generator_->Sphere(ret[0], ret[1], ret[2], r);
+  return ret;
+}
+
+// -----------------------------------------------------------------------------
+void Random::SetSeed(uint64_t seed) { generator_->SetSeed(seed); }
+
+// -----------------------------------------------------------------------------
+uint64_t Random::GetSeed() const { return generator_->GetSeed(); }
+
+// -----------------------------------------------------------------------------
+void Random::SetGenerator(TRandom* new_generator) {
+  if (generator_) {
+    delete generator_;
+  }
+  generator_ = new_generator;
+}
+
+// -----------------------------------------------------------------------------
+template <typename TSample>
+TSample DistributionRng<TSample>::Sample() {
+  auto* rng = Simulation::GetActive()->GetRandom()->generator_;
+  return SampleImpl(rng);
+}
+template double DistributionRng<double>::Sample();
+template int DistributionRng<int>::Sample();
+
+// -----------------------------------------------------------------------------
+UniformRng::UniformRng(double min, double max) : min_(min), max_(max) {}
+UniformRng::~UniformRng() {}
+double UniformRng::SampleImpl(TRandom* rng) { return rng->Uniform(min_, max_); }
+
+UniformRng Random::GetUniformRng(double min, double max) {
+  return UniformRng(min, max);
+}
+
+// -----------------------------------------------------------------------------
+GausRng::GausRng(double mean, double sigma) : mean_(mean), sigma_(sigma) {}
+GausRng::~GausRng() {}
+double GausRng::SampleImpl(TRandom* rng) { return rng->Gaus(mean_, sigma_); }
+
+GausRng Random::GetGausRng(double mean, double sigma) {
+  return GausRng(mean, sigma);
+}
+
+// -----------------------------------------------------------------------------
+ExpRng::ExpRng(double tau) : tau_(tau) {}
+ExpRng::~ExpRng() {}
+double ExpRng::SampleImpl(TRandom* rng) { return rng->Exp(tau_); }
+
+ExpRng Random::GetExpRng(double tau) { return ExpRng(tau); }
+
+// -----------------------------------------------------------------------------
+LandauRng::LandauRng(double mean, double sigma) : mean_(mean), sigma_(sigma) {}
+LandauRng::~LandauRng() {}
+double LandauRng::SampleImpl(TRandom* rng) {
+  return rng->Landau(mean_, sigma_);
+}
+
+LandauRng Random::GetLandauRng(double mean, double sigma) {
+  return LandauRng(mean, sigma);
+}
+
+// -----------------------------------------------------------------------------
+PoissonDRng::PoissonDRng(double mean) : mean_(mean) {}
+PoissonDRng::~PoissonDRng() {}
+double PoissonDRng::SampleImpl(TRandom* rng) { return rng->PoissonD(mean_); }
+
+PoissonDRng Random::GetPoissonDRng(double mean) { return PoissonDRng(mean); }
+
+// -----------------------------------------------------------------------------
+BreitWignerRng::BreitWignerRng(double mean, double gamma)
+    : mean_(mean), gamma_(gamma) {}
+BreitWignerRng::~BreitWignerRng() {}
+double BreitWignerRng::SampleImpl(TRandom* rng) {
+  return rng->BreitWigner(mean_, gamma_);
+}
+
+BreitWignerRng Random::GetBreitWignerRng(double mean, double gamma) {
+  return BreitWignerRng(mean, gamma);
+}
+
+// -----------------------------------------------------------------------------
+UserDefinedDistRng::UserDefinedDistRng(TF1* function) : function_(function) {}
+UserDefinedDistRng::~UserDefinedDistRng() {}
+
+// TODO(Lukas) after the update to ROOT 6.24 pass
+// rng to GetRandom to avoid performance issue.
+double UserDefinedDistRng::SampleImpl(TRandom* rng) {
+  auto min = function_->GetXmin();
+  auto max = function_->GetXmax();
+  return function_->GetRandom(min, max);
+}
+void UserDefinedDistRng::Draw() { function_->Draw(); }
+
+UserDefinedDistRng Random::GetUserDefinedDistRng(
+    double (*f)(const double*, const double*),
+    const FixedSizeVector<double, 10>& params, double min, double max) {
+  TF1* tf1 = nullptr;
+  UserDefinedDist udd{f, params, min, max};
+  auto it = udd_tf1_map_.find(udd);
+  if (it == udd_tf1_map_.end()) {
+    tf1 = new TF1("UserDefinedDistRng", f, min, max, params.size());
+    udd_tf1_map_[udd] = tf1;
+    tf1->SetParameters(params[0], params[1], params[2], params[3], params[3],
+                       params[4], params[5], params[6], params[7], params[8],
+                       params[9]);
+  } else {
+    tf1 = it->second;
+  }
+  return UserDefinedDistRng(tf1);
+}
+
+// -----------------------------------------------------------------------------
+BinomialRng::BinomialRng(int ntot, double prob) : ntot_(ntot), prob_(prob) {}
+BinomialRng::~BinomialRng() {}
+int BinomialRng::SampleImpl(TRandom* rng) {
+  return rng->Binomial(ntot_, prob_);
+}
+
+BinomialRng Random::GetBinomialRng(int ntot, double prob) {
+  return BinomialRng(ntot, prob);
+}
+
+// -----------------------------------------------------------------------------
+PoissonRng::PoissonRng(double mean) : mean_(mean) {}
+PoissonRng::~PoissonRng() {}
+int PoissonRng::SampleImpl(TRandom* rng) { return rng->Poisson(mean_); }
+
+PoissonRng Random::GetPoissonRng(double mean) { return PoissonRng(mean); }
 
 }  // namespace bdm
--- test/unit/core/util/random_test.cc
+++ test/unit/core/util/random_test.cc
@@ -13,71 +13,306 @@
 // -----------------------------------------------------------------------------
 
 #include "core/util/random.h"
+#include <Math/DistFunc.h>
+#include <TF1.h>
+#include <TRandom3.h>
 #include <gtest/gtest.h>
 #include "unit/test_util/io_test.h"
+#include "unit/test_util/test_util.h"
 
 namespace bdm {
 
 TEST(RandomTest, Uniform) {
-  Random random;
+  Simulation simulation(TEST_NAME);
+  auto* random = simulation.GetRandom();
   TRandom3 reference;
 
-  random.SetSeed(42);
+  random->SetSeed(42);
   reference.SetSeed(42);
 
   for (uint64_t i = 0; i < 10; i++) {
-    EXPECT_EQ(reference.Uniform(), random.Uniform());
+    EXPECT_EQ(reference.Uniform(), random->Uniform());
   }
 
   for (uint64_t i = 0; i < 10; i++) {
-    EXPECT_EQ(reference.Uniform(i), random.Uniform(i));
+    EXPECT_EQ(reference.Uniform(i), random->Uniform(i));
   }
 
   for (uint64_t i = 0; i < 10; i++) {
-    EXPECT_EQ(reference.Uniform(i, i + 2), random.Uniform(i, i + 2));
+    EXPECT_EQ(reference.Uniform(i, i + 2), random->Uniform(i, i + 2));
+  }
+
+  auto distrng = random->GetUniformRng(3, 4);
+  for (uint64_t i = 0; i < 10; i++) {
+    EXPECT_EQ(reference.Uniform(3, 4), distrng.Sample());
   }
 }
 
 TEST(RandomTest, UniformArray) {
-  Random random;
+  Simulation simulation(TEST_NAME);
+  auto* random = simulation.GetRandom();
   TRandom3 reference;
 
-  random.SetSeed(42);
+  random->SetSeed(42);
   reference.SetSeed(42);
 
-  auto result = random.UniformArray<5>();
+  auto result = random->UniformArray<5>();
   for (uint64_t i = 0; i < 5; i++) {
     EXPECT_EQ(reference.Uniform(), result[i]);
   }
 
-  auto result1 = random.UniformArray<2>(8.3);
+  auto result1 = random->UniformArray<2>(8.3);
   for (uint64_t i = 0; i < 2; i++) {
     EXPECT_EQ(reference.Uniform(8.3), result1[i]);
   }
 
-  auto result2 = random.UniformArray<12>(5.1, 9.87);
+  auto result2 = random->UniformArray<12>(5.1, 9.87);
   for (uint64_t i = 0; i < 12; i++) {
     EXPECT_EQ(reference.Uniform(5.1, 9.87), result2[i]);
   }
 }
 
 TEST(RandomTest, Gaus) {
-  Random random;
+  Simulation simulation(TEST_NAME);
+  auto* random = simulation.GetRandom();
   TRandom3 reference;
 
-  random.SetSeed(42);
+  random->SetSeed(42);
   reference.SetSeed(42);
 
   for (uint64_t i = 0; i < 10; i++) {
-    EXPECT_EQ(reference.Gaus(), random.Gaus());
+    EXPECT_EQ(reference.Gaus(), random->Gaus());
   }
 
   for (uint64_t i = 0; i < 10; i++) {
-    EXPECT_EQ(reference.Gaus(i), random.Gaus(i));
+    EXPECT_EQ(reference.Gaus(i), random->Gaus(i));
   }
 
   for (uint64_t i = 0; i < 10; i++) {
-    EXPECT_EQ(reference.Gaus(i, i + 2), random.Gaus(i, i + 2));
+    EXPECT_EQ(reference.Gaus(i, i + 2), random->Gaus(i, i + 2));
+  }
+
+  auto distrng = random->GetGausRng(3, 4);
+  for (uint64_t i = 0; i < 10; i++) {
+    EXPECT_EQ(reference.Gaus(3, 4), distrng.Sample());
+  }
+}
+
+TEST(RandomTest, Exp) {
+  Simulation simulation(TEST_NAME);
+  auto* random = simulation.GetRandom();
+  TRandom3 reference;
+
+  random->SetSeed(42);
+  reference.SetSeed(42);
+
+  for (uint64_t i = 0; i < 10; i++) {
+    EXPECT_EQ(reference.Exp(i), random->Exp(i));
+  }
+
+  for (uint64_t i = 0; i < 10; i++) {
+    EXPECT_EQ(reference.Exp(i + 2), random->Exp(i + 2));
+  }
+
+  auto distrng = random->GetExpRng(123);
+  for (uint64_t i = 0; i < 10; i++) {
+    EXPECT_EQ(reference.Exp(123), distrng.Sample());
+  }
+}
+
+TEST(RandomTest, Landau) {
+  Simulation simulation(TEST_NAME);
+  auto* random = simulation.GetRandom();
+  TRandom3 reference;
+
+  random->SetSeed(42);
+  reference.SetSeed(42);
+
+  for (uint64_t i = 0; i < 10; i++) {
+    EXPECT_EQ(reference.Landau(), random->Landau());
+  }
+
+  for (uint64_t i = 0; i < 10; i++) {
+    EXPECT_EQ(reference.Landau(i), random->Landau(i));
+  }
+
+  for (uint64_t i = 0; i < 10; i++) {
+    EXPECT_EQ(reference.Landau(i, i + 2), random->Landau(i, i + 2));
+  }
+
+  auto distrng = random->GetLandauRng(3, 4);
+  for (uint64_t i = 0; i < 10; i++) {
+    EXPECT_EQ(reference.Landau(3, 4), distrng.Sample());
+  }
+}
+
+TEST(RandomTest, PoissonD) {
+  Simulation simulation(TEST_NAME);
+  auto* random = simulation.GetRandom();
+  TRandom3 reference;
+
+  random->SetSeed(42);
+  reference.SetSeed(42);
+
+  for (uint64_t i = 0; i < 10; i++) {
+    EXPECT_EQ(reference.PoissonD(i), random->PoissonD(i));
+  }
+
+  for (uint64_t i = 0; i < 10; i++) {
+    EXPECT_EQ(reference.PoissonD(i + 2), random->PoissonD(i + 2));
+  }
+
+  auto distrng = random->GetPoissonDRng(123);
+  for (uint64_t i = 0; i < 10; i++) {
+    EXPECT_EQ(reference.PoissonD(123), distrng.Sample());
+  }
+}
+
+TEST(RandomTest, BreitWigner) {
+  Simulation simulation(TEST_NAME);
+  auto* random = simulation.GetRandom();
+  TRandom3 reference;
+
+  random->SetSeed(42);
+  reference.SetSeed(42);
+
+  for (uint64_t i = 0; i < 10; i++) {
+    EXPECT_EQ(reference.BreitWigner(), random->BreitWigner());
+  }
+
+  for (uint64_t i = 0; i < 10; i++) {
+    EXPECT_EQ(reference.BreitWigner(i), random->BreitWigner(i));
+  }
+
+  for (uint64_t i = 0; i < 10; i++) {
+    EXPECT_EQ(reference.BreitWigner(i, i + 2), random->BreitWigner(i, i + 2));
+  }
+
+  auto distrng = random->GetBreitWignerRng(3, 4);
+  for (uint64_t i = 0; i < 10; i++) {
+    EXPECT_EQ(reference.BreitWigner(3, 4), distrng.Sample());
+  }
+}
+
+TEST(RandomTest, UserDefinedDistRng) {
+  Simulation simulation(TEST_NAME);
+  auto* random = simulation.GetRandom();
+  auto function = [](const double* x, const double* params) {
+    return ROOT::Math::lognormal_pdf(*x, params[0], params[1]);
+  };
+  double min = 1;
+  double max = 4;
+  TF1 reference("", function, min, max, 2);
+  reference.SetParameters(1.1, 1.2);
+
+  // workaround until update to 6.24
+  gRandom->SetSeed(42);
+  std::vector<double> expected;
+  for (uint64_t i = 0; i < 10; i++) {
+    expected.push_back(reference.GetRandom(min, max));
+  }
+
+  gRandom->SetSeed(42);
+  auto distrng = random->GetUserDefinedDistRng(function, {1.1, 1.2}, min, max);
+  std::vector<double> actual;
+  for (uint64_t i = 0; i < 10; i++) {
+    actual.push_back(distrng.Sample());
+  }
+  for (size_t i = 0; i < actual.size(); i++) {
+    EXPECT_NEAR(expected[i], actual[i], abs_error<double>::value);
+  }
+}
+
+TEST(RandomTest, Binomial) {
+  Simulation simulation(TEST_NAME);
+  auto* random = simulation.GetRandom();
+  TRandom3 reference;
+
+  random->SetSeed(42);
+  reference.SetSeed(42);
+
+  for (uint64_t i = 0; i < 10; i++) {
+    EXPECT_EQ(reference.Binomial(i, i + 2), random->Binomial(i, i + 2));
+  }
+
+  auto distrng = random->GetBinomialRng(3, 4);
+  for (uint64_t i = 0; i < 10; i++) {
+    EXPECT_EQ(reference.Binomial(3, 4), distrng.Sample());
+  }
+}
+
+TEST(RandomTest, Poisson) {
+  Simulation simulation(TEST_NAME);
+  auto* random = simulation.GetRandom();
+  TRandom3 reference;
+
+  random->SetSeed(42);
+  reference.SetSeed(42);
+
+  for (uint64_t i = 0; i < 10; i++) {
+    EXPECT_EQ(reference.Poisson(i), random->Poisson(i));
+  }
+
+  for (uint64_t i = 0; i < 10; i++) {
+    EXPECT_EQ(reference.Poisson(i + 2), random->Poisson(i + 2));
+  }
+
+  auto distrng = random->GetPoissonRng(123);
+  for (uint64_t i = 0; i < 10; i++) {
+    EXPECT_EQ(reference.Poisson(123), distrng.Sample());
+  }
+}
+
+TEST(RandomTest, Integer) {
+  Simulation simulation(TEST_NAME);
+  auto* random = simulation.GetRandom();
+  TRandom3 reference;
+
+  random->SetSeed(42);
+  reference.SetSeed(42);
+
+  for (uint64_t i = 1; i < 10; i++) {
+    EXPECT_EQ(reference.Integer(i), random->Integer(i));
+  }
+}
+
+TEST(RandomTest, Circle) {
+  Simulation simulation(TEST_NAME);
+  auto* random = simulation.GetRandom();
+  TRandom3 reference;
+
+  random->SetSeed(42);
+  reference.SetSeed(42);
+
+  for (uint64_t i = 1; i < 10; i++) {
+    double expected_x = 0;
+    double expected_y = 0;
+    reference.Circle(expected_x, expected_y, i);
+
+    auto actual = random->Circle(i);
+    EXPECT_EQ(expected_x, actual[0]);
+    EXPECT_EQ(expected_y, actual[1]);
+  }
+}
+
+TEST(RandomTest, Sphere) {
+  Simulation simulation(TEST_NAME);
+  auto* random = simulation.GetRandom();
+  TRandom3 reference;
+
+  random->SetSeed(42);
+  reference.SetSeed(42);
+
+  for (uint64_t i = 1; i < 10; i++) {
+    double expected_x = 0;
+    double expected_y = 0;
+    double expected_z = 0;
+    reference.Sphere(expected_x, expected_y, expected_z, i);
+
+    auto actual = random->Sphere(i);
+    EXPECT_EQ(expected_x, actual[0]);
+    EXPECT_EQ(expected_y, actual[1]);
+    EXPECT_EQ(expected_z, actual[2]);
   }
 }
 
@@ -100,6 +335,33 @@ TEST_F(IOTest, Random) {
     EXPECT_EQ(reference.Uniform(i, i + 2), random.Uniform(i, i + 2));
   }
 }
+
+TEST_F(IOTest, UserDefinedDistRng) {
+  Simulation simulation(TEST_NAME);
+  auto* random = simulation.GetRandom();
+
+  auto ud_dist = [](const double* x, const double* param) { return sin(*x); };
+  auto udd_rng = random->GetUserDefinedDistRng(ud_dist, {}, 0, 3);
+
+  gRandom->SetSeed(42);
+  std::vector<double> expected;
+  for (int i = 0; i < 10; ++i) {
+    expected.push_back(udd_rng.Sample());
+  }
+
+  UserDefinedDistRng* restored;
+  BackupAndRestore(udd_rng, &restored);
+
+  gRandom->SetSeed(42);
+  std::vector<double> actual;
+  for (int i = 0; i < 10; ++i) {
+    actual.push_back(restored->Sample());
+  }
+  for (size_t i = 0; i < actual.size(); i++) {
+    EXPECT_NEAR(expected[i], actual[i], 1e-6);
+  }
+}
+
 #endif  // USE_DICT
 
 }  // namespace bdm
