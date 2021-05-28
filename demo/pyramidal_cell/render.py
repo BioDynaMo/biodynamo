f394440f25cc1154b3b1e76fff6bd32f89411e0b
Lukas Johannes Breitwieser <lukas.johannes.breitwieser@cern.ch>
2021-05-28 17:00:30 +0200

    Add pyramidal cell demo
    Co-authored-by: Jean de Montigny <J.De-Montigny2@newcastle.ac.uk>

---
+++ demo/pyramidal_cell/render.py
@@ -0,0 +1,112 @@
+import argparse
+from paraview.simple import *
+
+parser = argparse.ArgumentParser(description='Command line arguments')
+parser.add_argument('--raytracing', action='store_true', default=False)
+parser.add_argument('--screenshots', action='store_true', default=False)
+parser.add_argument('--animation', action='store_true', default=False)
+params = parser.parse_args()
+
+#### disable automatic camera reset on 'Show'
+paraview.simple._DisableFirstRenderCameraReset()
+
+# load state
+LoadState('output/pyramidal_cell/pyramidal_cell.pvsm')
+
+# find source
+mySomas = FindSource('NeuronSomas')
+
+# set active source
+SetActiveSource(mySomas)
+
+# Properties modified on mySomas.GlyphType
+mySomas.GlyphType.ThetaResolution = 20
+mySomas.GlyphType.PhiResolution = 20
+
+# get active view
+renderView1 = GetActiveViewOrCreate('RenderView')
+# uncomment following to set a specific view size
+# renderView1.ViewSize = [1452, 949]
+
+# update the view to ensure updated data information
+renderView1.Update()
+
+# reset view to fit data
+renderView1.ResetCamera()
+
+# get animation scene
+animationScene1 = GetAnimationScene()
+
+animationScene1.GoToLast()
+
+# reset view to fit data bounds
+renderView1.ResetCamera(-4.98292255402, 4.98292255402, -4.98292255402, 4.98292255402, -5.0, 5.0)
+
+# find source
+myNeurites = FindSource('NeuriteElements')
+
+# set active source
+SetActiveSource(myNeurites)
+
+# reset view to fit data bounds
+renderView1.ResetCamera(-57.3483390808, 91.8613815308, -83.2577438354, 27.554019928, -125.748809814, 451.121826172)
+
+# Hide orientation axes
+renderView1.OrientationAxesVisibility = 0
+
+# Properties modified on renderView1
+renderView1.Background = [0.14901960784313725, 0.14901960784313725, 0.14901960784313725]
+
+# Properties modified on myNeuritesDisplay
+myNeuritesDisplay = GetDisplayProperties(myNeurites, view=renderView1)
+myNeuritesDisplay.Luminosity = 10.0
+
+# get the material library
+materialLibrary1 = GetMaterialLibrary()
+
+if params.raytracing:
+    pm = paraview.servermanager.vtkSMProxyManager
+    if pm.GetVersionMajor() == 5 and pm.GetVersionMinor() < 7:
+        renderView1.EnableOSPRay = 1
+        renderView1.OSPRayRenderer = 'pathtracer'
+    else:
+        renderView1.EnableRayTracing = 1
+        renderView1.BackEnd = 'OSPRay pathtracer'
+        renderView1.Denoise = 1
+
+    # Properties modified on renderView1
+    renderView1.Shadows = 1
+
+    # Properties modified on renderView1
+    renderView1.SamplesPerPixel = 20
+
+#### saving camera placements for all active views
+
+# current camera placement for renderView1
+renderView1.CameraPosition = [-1024.8891945362295, 161.76612091064453, 154.97137451171875]
+renderView1.CameraFocalPoint = [149.75026893615723, 161.76612091064453, 154.97137451171875]
+renderView1.CameraViewUp = [0.0, 0.0, 1.0]
+renderView1.CameraParallelScale = 304.0190642756604
+
+# update the view to ensure updated data information
+renderView1.Update()
+
+if params.screenshots:
+    SaveScreenshot('pyramidal-cell-single.png', renderView1, ImageResolution=[1200, 4000], FontScaling='Scale fonts proportionally')
+    print("Rendered image pyramidal-cell-single.png")
+
+if params.animation:
+    timesteps = GetAnimationScene().TimeKeeper.TimestepValues
+    print(int(timesteps[-1]))
+
+    SaveAnimation("animation.png", renderView1, ImageResolution=[1000, 2160],
+        FontScaling='Scale fonts proportionally',
+        OverrideColorPalette='',
+        StereoMode='No change',
+        TransparentBackground=0,
+        FrameRate=1,
+        FrameWindow=[0, int(timesteps[-1])],
+        # PNG options
+        CompressionLevel='5',
+        SuffixFormat='.%04d')
+
---
+++ demo/pyramidal_cell/src/pyramidal_cell.h
@@ -0,0 +1,171 @@
+// -----------------------------------------------------------------------------
+//
+// Copyright (C) 2021 CERN & Newcastle University for the benefit of the
+// BioDynaMo collaboration. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// See the LICENSE file distributed with this work for details.
+// See the NOTICE file distributed with this work for additional information
+// regarding copyright ownership.
+//
+// -----------------------------------------------------------------------------
+#ifndef PYRAMIDAL_CELL_H_
+#define PYRAMIDAL_CELL_H_
+
+#include "biodynamo.h"
+#include "neuroscience/neuroscience.h"
+
+namespace bdm {
+
+enum Substances { kApical, kBasal };
+
+struct ApicalDendriteGrowth : public Behavior {
+  BDM_BEHAVIOR_HEADER(ApicalDendriteGrowth, Behavior, 1);
+  ApicalDendriteGrowth() { AlwaysCopyToNew(); }
+  virtual ~ApicalDendriteGrowth() {}
+  
+  void Initialize(const NewAgentEvent& event) override {
+    Base::Initialize(event);
+    can_branch_ = false;
+  }
+ 
+  void Run(Agent* agent) override { 
+    auto* sim = Simulation::GetActive();
+    auto* random = sim->GetRandom();
+    auto* rm = sim->GetResourceManager();
+
+    if (!init_) {
+      dg_guide_ = rm->GetDiffusionGrid(kApical);
+      init_ = true;
+    }
+
+    auto* dendrite = bdm_static_cast<NeuriteElement*>(agent);
+    if (dendrite->GetDiameter() > 0.575) {
+      Double3 gradient;
+      dg_guide_->GetGradient(dendrite->GetPosition(), &gradient);
+
+      double gradient_weight = 0.06;
+      double randomness_weight = 0.3;
+      double old_direction_weight = 4;
+
+      auto random_axis = random->template UniformArray<3>(-1, 1);
+      auto old_direction = dendrite->GetSpringAxis() * old_direction_weight;
+      auto grad_direction = gradient * gradient_weight;
+      auto random_direction = random_axis * randomness_weight;
+
+      Double3 new_step_direction =
+          old_direction + random_direction + grad_direction;
+
+      dendrite->ElongateTerminalEnd(100, new_step_direction);
+      dendrite->SetDiameter(dendrite->GetDiameter() - 0.00071);
+
+      if (can_branch_ && dendrite->IsTerminal() &&
+          dendrite->GetDiameter() > 0.55 && random->Uniform() < 0.038) {
+        auto rand_noise = random->template UniformArray<3>(-0.1, 0.1);
+        Double3 branch_direction =
+            Math::Perp3(dendrite->GetUnitaryAxisDirectionVector() + rand_noise,
+                        random->Uniform(0, 1)) +
+            dendrite->GetSpringAxis();
+        auto* dendrite_2 = dendrite->Branch(branch_direction);
+        dendrite_2->SetDiameter(0.65);
+      }
+    }
+  }
+
+ private:
+  bool init_ = false;
+  bool can_branch_ = true;
+  DiffusionGrid* dg_guide_ = nullptr;
+};
+
+struct BasalDendriteGrowth : public Behavior {
+  BDM_BEHAVIOR_HEADER(BasalDendriteGrowth, Behavior, 1);
+  BasalDendriteGrowth() { AlwaysCopyToNew(); }
+  virtual ~BasalDendriteGrowth() {}
+ 
+  void Run(Agent* agent) override {
+    auto* sim = Simulation::GetActive();
+    auto* random = sim->GetRandom();
+    auto* rm = sim->GetResourceManager();
+
+    if (!init_) {
+      dg_guide_ = rm->GetDiffusionGrid(kBasal);
+      init_ = true;
+    }
+
+    auto* dendrite = bdm_static_cast<NeuriteElement*>(agent);
+    if (dendrite->IsTerminal() && dendrite->GetDiameter() > 0.75) {
+      Double3 gradient;
+      dg_guide_->GetGradient(dendrite->GetPosition(), &gradient);
+
+      double gradient_weight = 0.03;
+      double randomness_weight = 0.4;
+      double old_direction_weight = 6;
+
+      auto random_axis = random->template UniformArray<3>(-1, 1);
+      auto old_direction = dendrite->GetSpringAxis() * old_direction_weight;
+      auto grad_direction = gradient * gradient_weight;
+      auto random_direction = random_axis * randomness_weight;
+
+      Double3 new_step_direction =
+          old_direction + random_direction + grad_direction;
+
+      dendrite->ElongateTerminalEnd(50, new_step_direction);
+      dendrite->SetDiameter(dendrite->GetDiameter() - 0.00085);
+
+      if (random->Uniform() < 0.006) {
+        dendrite->Bifurcate();
+      }
+    }
+  }
+
+ private:
+  bool init_ = false;
+  DiffusionGrid* dg_guide_ = nullptr;
+};
+
+inline void AddInitialNeuron(const Double3& position) {
+  auto* soma = new neuroscience::NeuronSoma(position);
+  soma->SetDiameter(10);
+  Simulation::GetActive()->GetResourceManager()->AddAgent(soma);
+
+  auto* apical_dendrite = soma->ExtendNewNeurite({0, 0, 1});
+  auto* basal_dendrite1 = soma->ExtendNewNeurite({0, 0, -1});
+  auto* basal_dendrite2 = soma->ExtendNewNeurite({0, 0.6, -0.8});
+  auto* basal_dendrite3 = soma->ExtendNewNeurite({0.3, -0.6, -0.8});
+  
+  apical_dendrite->AddBehavior(new ApicalDendriteGrowth());
+  basal_dendrite1->AddBehavior(new BasalDendriteGrowth());
+  basal_dendrite2->AddBehavior(new BasalDendriteGrowth());
+  basal_dendrite3->AddBehavior(new BasalDendriteGrowth());
+}
+
+/// Create and initialize substances for neurite attraction
+inline void CreateExtracellularSubstances(const Param* p) {
+  using MI = ModelInitializer;
+  MI::DefineSubstance(kApical, "substance_apical", 0, 0,
+                        p->max_bound / 80);
+  MI::DefineSubstance(kBasal, "substance_basal", 0, 0,
+                        p->max_bound / 80);
+  // initialize substance with gaussian distribution
+  auto a_initializer = GaussianBand(p->max_bound, 200, Axis::kZAxis); 
+  auto b_initializer = GaussianBand(p->min_bound, 200, Axis::kZAxis); 
+  MI::InitializeSubstance(kApical, a_initializer);
+  MI::InitializeSubstance(kBasal, b_initializer);
+}
+
+inline int Simulate(int argc, const char** argv) {
+  neuroscience::InitModule();
+  Simulation simulation(argc, argv);
+  AddInitialNeuron({150, 150, 0});
+  CreateExtracellularSubstances(simulation.GetParam());
+  simulation.GetScheduler()->Simulate(500);
+  std::cout << "Simulation completed successfully!" << std::endl;
+  return 0;
+}
+
+}  // namespace bdm
+
+#endif  // PYRAMIDAL_CELL_H_
