ba367ca6f362dfdfed912001f10df4394bf53972
Tobias Duswald <tobias.duswald@cern.ch>
2021-06-30 19:10:18 +0200

    Add callback to BuildCommand for TestCommand

--- cli/test_command.py
+++ cli/test_command.py
@@ -12,8 +12,10 @@
 #
 # -----------------------------------------------------------------------------
 
-import os
+import os, sys, shutil
 import subprocess as sp
+from print_command import Print
+from build_command import BuildCommand
 
 
 ## The BioDynaMo CLI command to execute the unit-tests created by default with
@@ -23,6 +25,19 @@ def TestCommand():
     if cwd.split("/")[-1] == "build":
         sp.run("ctest", shell=True)
     else:
+        # If there is no build, we first build the simulation and tests
+        if "build" not in os.listdir(cwd):
+            try:
+                BuildCommand()
+                Print.new_step("Testing")
+            except:
+                Print.error("No build directory found and \"biodynamo build\"")
+                Print.error("failed. Please build project first.")
+                # Clean up if BuildCommand() fails.
+                if "build" in os.listdir(cwd):
+                    shutil.rmtree("build")
+                sys.exit(1)
+        # As soon as there is a build, we use it
         os.chdir("build")
         sp.run("ctest", shell=True)
         os.chdir(cwd)
