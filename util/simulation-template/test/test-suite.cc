6aa3e5dcda063eef351f8ba5681e4e24e3eab672
Tobias Duswald <tobias.duswald@cern.ch>
2021-10-01 11:13:45 +0200

    Reduce example test-suite to minimum

--- util/simulation-template/test/test-suite.cc
+++ util/simulation-template/test/test-suite.cc
@@ -16,17 +16,28 @@
 #include "biodynamo.h"
 
 // Googletest in combination with the provided CMakeLists.txt allows you to
-// define tests in arbitrary .cc files in the `test/` folder. We propose
-// this file to test the agent. The examples below are not necessary because
-// they are tested in BioDynaMo itself. It should serve as an inspiration for
-// testing user-defined, customn behaviours or similar things. If you wish to
-// add tests for other aspects, you can either add them to the existing
-// test-*.cc files or create a new test-*.cc file in the test/folder. CMake will
-// handle it automatically.
+// define tests in arbitrary .cc files in the `test/` folder. This file should
+// serve as an inspiration for testing user-defined, custom behaviors, basic as
+// well as compicated functions, or similar things. If you wish to add tests for
+// specific aspects, you can either add them to the existing test-suite.cc file
+// or create a new *.cc file in the `test/` folder. CMake will handle it
+// automatically. For more information regarding testing with Googletest, please
+// consider the following sources:
+// * https://google.github.io/googletest/primer.html
+// * https://github.com/google/googletest
 
 #define TEST_NAME typeid(*this).name()
 
 namespace bdm {
+
+// A function to test
+int Compute42() { return 6 * 7; };
+
+// Show how to compare two numbers
+TEST(UtilTest, NumberTest) {
+  // Expect equality
+  EXPECT_EQ(Compute42(), 42);
+}
 
 // Test if we can add agents to the simulation
 TEST(AgentTest, AddAgentsToSimulation) {
@@ -44,31 +55,6 @@ TEST(AgentTest, AddAgentsToSimulation) {
   // Test if all 20 cells are in the simulation
   auto no_cells = rm->GetNumAgents();
   EXPECT_EQ(expected_no_cells, no_cells);
-}
-
-// Test if our GrothDevision behaviour increases the cell volume as expected
-TEST(AgentTest, CellGrowth) {
-  // Create simulation
-  Simulation simulation(TEST_NAME);
-
-  // Add one growing cell to the simulation
-  auto* rm = simulation.GetResourceManager();
-  auto* cell = new Cell(30);
-  cell->AddBehavior(new GrowthDivision());
-  const AgentPointer<Cell> cell_ptr = cell->GetAgentPtr<Cell>();
-  rm->AddAgent(cell);
-
-  // Get cell volume at the beginning
-  const double volume_1 = cell_ptr->GetVolume();
-
-  // Simulate for 3 timesteps
-  simulation.Simulate(3);
-
-  // Get cell volume at the beginning after 3 timesteps
-  const double volume_2 = cell_ptr->GetVolume();
-
-  // Test if cell volume has increased
-  EXPECT_LT(volume_1, volume_2);
 }
 
 }  // namespace bdm
