cda114c356c4cdeb68906be32280b2c752c17089
Lukas Johannes Breitwieser <lukas.johannes.breitwieser@cern.ch>
2021-10-26 16:43:41 +0200

    WIP selection

--- src/core/diffusion/diffusion_grid.h
+++ src/core/diffusion/diffusion_grid.h
@@ -187,7 +187,7 @@ class DiffusionGrid {
   double box_volume_ = 0;
   /// Lock for each voxel used to prevent race conditions between
   /// multiple threads
-  mutable ParallelResizeVector<Spinlock> locks_ = {};
+  mutable ParallelResizeVector<Spinlock> locks_ = {};  //!
   /// The array of concentration values
   ParallelResizeVector<double> c1_ = {};
   /// An extra concentration data buffer for faster value updating
