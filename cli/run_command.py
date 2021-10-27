aeb9ce21e39bb7c30ce9e6d670a8885ea11a9644
Tobias Duswald <tobias.duswald@cern.ch>
2021-10-27 23:49:48 +0200

    Fix scheduled GHA  (#219)
    The scheduled GHA are configured with -Dtest=off. 
    One of the shell scripts tried to execute ./build/test-sim-test which is not built when -Dtest=off. 
    This commit adds an if statement to check if build/test-sim-test exists and only if that's the case, we execute the "problematic" command.

--- cli/run_command.py
+++ cli/run_command.py
@@ -23,19 +23,41 @@ from util import GetBinaryName
 ## @param      sim_name  The simulation name
 ##
 def RunCommand(args):
+
+    # 1. attempt to change into project director if we are in build
+    cwd = os.getcwd()
+    if cwd.split("/")[-1] == "build":
+        os.chdir("..")
     sim_name = GetBinaryName()
     args_str = " ".join(args)
     cmd = "./build/" + sim_name
 
+    BuildCommand()
     try:
-        BuildCommand()
-        Print.new_step("Run " + sim_name + " " + args_str)
+        # 2. execute run command
+        Print.new_step(
+            "<bdm run> Running " + sim_name + " " + args_str + " ..."
+        )
         if platform.system() == "Darwin":
             launcher = os.environ["BDMSYS"] + "/bin/launcher.sh"
-            sp.run([launcher, cmd, args_str])
+            result = sp.run([launcher, cmd, args_str])
         else:
-            sp.run([cmd, args_str])
-        Print.success("Finished successfully")
+            result = sp.run([cmd, args_str])
+
+        # 3. go back to initial folder.
+        os.chdir(cwd)
+
+        # 4. return the return code of the ctest command:
+        if result.returncode != 0:
+            Print.error(
+                "<bdm run> Received return code {} from {}.".format(
+                    result.returncode, cmd
+                )
+            )
+            exit(result.returncode)
+        Print.success("<bdm run> Finished successfully.")
+
     except sp.CalledProcessError as err:
         print(err.output.decode("utf-8"))
-        Print.error("Error during execution of {0}".format(cmd))
+        Print.error("<bdm run> Error during execution of {0}".format(cmd))
+        exit(1)
