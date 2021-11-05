741d46a864d36d1477715150da4d28d3218e90e3
Lukas Johannes Breitwieser <lukas.johannes.breitwieser@cern.ch>
2021-11-05 17:20:15 +0100

    Check if notebooks are built

--- cmake/Website.cmake
+++ cmake/Website.cmake
@@ -24,14 +24,25 @@ function(GenerateStaticFiles TARGET FLAGS)
       COMMENT "Generate website"
       COMMAND ./build_website.sh --dir ${CMAKE_CURRENT_SOURCE_DIR} ${FLAGS}
       VERBATIM)
+
+  # API documentation dependency
+  if (NOT DOXYGEN_FOUND)
+    message(FATAL_ERROR "You cannot build the website without Doxygen. Please make
+    sure that Doxygen is installed before attempting to build the website!")
+  endif()
   add_dependencies(${TARGET} doc)
+
+  # BDM notebook dependency
+  if (notebooks)
+    add_dependencies(${TARGET} notebooks)
+  else()
+    message(FATAL_ERROR "You cannot build the website without building the BioDynaMo notebooks first.
+Enable the notebook feature by adding the following cmake parameter: -Dnotebooks=ON
+")
+  endif()
 endfunction()
 
 if (website)
-  if (NOT DOXYGEN_FOUND)
-    message(FATAL "You cannot build the website without Doxygen. Please make
-    sure that Doxygen is installed before attempting to build the website!")
-  endif()
   # Generate static website files
   GenerateStaticFiles(website "--api")
   # Generate staic website files and launches website on localhost for live editing
