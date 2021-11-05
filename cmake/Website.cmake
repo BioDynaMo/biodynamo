d7678e14140859afbb8245b6c3d9328e6c124075
Lukas Breitwieser <lukas.johannes.breitwieser@cern.ch>
2021-11-05 19:05:15 +0100

    Required updates to display all demos and notebooks on the website (#222)

--- cmake/Website.cmake
+++ cmake/Website.cmake
@@ -19,20 +19,30 @@ function(GenerateStaticFiles TARGET FLAGS)
   file(MAKE_DIRECTORY "${WEB_DIR}")
   add_custom_target(${TARGET}
       WORKING_DIRECTORY "${WEB_DIR}"
-      COMMAND rm -rf ${WEB_DIR} && mkdir ${WEB_DIR}
-      COMMAND git clone https://github.com/BioDynaMo/website.git .
-      COMMAND git checkout master
+      COMMAND [ -f build_website.sh ] || git clone https://github.com/BioDynaMo/website.git .
+      COMMAND git pull
       COMMENT "Generate website"
       COMMAND ./build_website.sh --dir ${CMAKE_CURRENT_SOURCE_DIR} ${FLAGS}
       VERBATIM)
+
+  # API documentation dependency
+  if (NOT DOXYGEN_FOUND)
+    message(FATAL_ERROR "You cannot build the website without Doxygen. Please make
+    sure that Doxygen is installed before attempting to build the website!")
+  endif()
   add_dependencies(${TARGET} doc)
+
+  # BDM notebook dependency
+  if (notebooks)
+    add_dependencies(${TARGET} notebooks)
+  else()
+    message(FATAL_ERROR "You cannot build the website without building the BioDynaMo notebooks first.
+Enable the notebook feature by adding the following cmake parameter: -Dnotebooks=ON
+")
+  endif()
 endfunction()
 
 if (website)
-  if (NOT DOXYGEN_FOUND)
-    message(FATAL "You cannot build the website without Doxygen. Please make
-    sure that Doxygen is installed before attempting to build the website!")
-  endif()
   # Generate static website files
   GenerateStaticFiles(website "--api")
   # Generate staic website files and launches website on localhost for live editing
