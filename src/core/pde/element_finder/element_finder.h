6d66a58cd23a0ec9a4b2d45612f786bb3e103165
Tobias Duswald <tobias.duswald@cern.ch>
2021-11-16 16:38:24 +0100

    Restructure class for Linux and Cling, fix warning
    React to build failure on Ubuntu and CentOS

--- src/core/pde/element_finder/element_finder.h
+++ src/core/pde/element_finder/element_finder.h
@@ -25,22 +25,6 @@ namespace bdm {
 /// The ElementFinder is a Octree based search engine to identify the closest
 /// center of an element relative to a given vector.
 class ElementFinder {
- private:
-  /// Struct to wrap the octree implementation (see best practices, e.g. in
-  /// OctreeEnvironment).
-  struct OctreeImplementation;
-  /// Octree for spatial searches
-  std::unique_ptr<OctreeImplementation> octree_;
-  /// mfem::Mesh
-  mfem::Mesh* mesh_;
-  /// Wraps the access to a mfem::Mesh for octree search
-  ElementContainer element_container_;
-  /// Vertex to Element Table from mfem mesh.
-  mfem::Table* vertex_to_element_;
-
-  /// Converts a Double3 to a corresponding MFEM vector.
-  mfem::Vector ConvertToMFEMVector(const Double3& point);
-
  public:
   /// Constructing a ElementFinder requires a mfem::Mesh. During this call, the
   /// Octree is build for fast searches.
@@ -48,6 +32,10 @@ class ElementFinder {
 
   /// Frees the heap memory allocated by the constructed octree.
   ~ElementFinder();
+
+  /// Struct to wrap the octree implementation (see best practices, e.g. in
+  /// OctreeEnvironment).
+  struct OctreeImplementation;
 
   /// Given a mfem::Vector, the function returns the element_id of the mesh
   /// whose center is the closest to the vector x (measured as L2 distance).
@@ -89,6 +77,19 @@ class ElementFinder {
   /// fast, bear in mind when using.
   bool IsInNeighborElements(const Double3& point, int& element_id,
                             mfem::IntegrationPoint* ip = nullptr);
+
+ private:
+  /// Octree for spatial searches
+  std::unique_ptr<OctreeImplementation> octree_;
+  /// mfem::Mesh
+  mfem::Mesh* mesh_;
+  /// Wraps the access to a mfem::Mesh for octree search
+  ElementContainer element_container_;
+  /// Vertex to Element Table from mfem mesh.
+  mfem::Table* vertex_to_element_;
+
+  /// Converts a Double3 to a corresponding MFEM vector.
+  mfem::Vector ConvertToMFEMVector(const Double3& point);
 };
 
 }  // namespace bdm
