01987a9619f5a02e5fad6ea21d4bdf8d12c2ccd3
Tobias Duswald <tobias.duswald@cern.ch>
2021-11-19 08:42:54 +0100

    Fix typo

--- cmake/external/ROOT.cmake
+++ cmake/external/ROOT.cmake
@@ -3,20 +3,20 @@ include(utils)
 # Directory in which ROOT will be downloaded
 SET(ROOT_SOURCE_DIR "${CMAKE_THIRD_PARTY_DIR}/root")
 
-set(ROOT_TAR_FILE http://cern.ch/biodynamo-lfs/third-party/root_v6.22.06_python3.9_${DETECTED_OS_VERS}.tar.gz)
+set(ROOT_TAR_FILE root_v6.22.06_python3.9_${DETECTED_OS_VERS}.tar.gz)
 if(APPLE)
   if("${DETECTED_OS_VERS}" MATCHES "^osx-12" OR
      "${DETECTED_OS_VERS}" STREQUAL "osx-11.6-arm64")
-    set(ROOT_TAR_FILE http://cern.ch/biodynamo-lfs/third-party/root_v6.25.01_cxx14_python3.9_${DETECTED_OS_VERS}.tar.gz)
+    set(ROOT_TAR_FILE root_v6.25.01_cxx14_python3.9_${DETECTED_OS_VERS}.tar.gz)
   elseif("${DETECTED_OS_VERS}" STREQUAL "osx-11.6-i386")
-    set(ROOT_TAR_FILE http://cern.ch/biodynamo-lfs/third-party/root_v6.25.01.macos-11.6-x86_64-clang120)
+    set(ROOT_TAR_FILE root_v6.25.01.macos-11.6-x86_64-clang120.tar.gz)
   elseif("${DETECTED_OS_VERS}" MATCHES "^osx-11")
     message(FATAL_ERROR "We officialy only support the latest macOS 11 version: 11.6.")
   endif()
 endif()
 
 download_verify_extract(
-  ${ROOT_TAR_FILE}
+  http://cern.ch/biodynamo-lfs/third-party/${ROOT_TAR_FILE}
   ${ROOT_SOURCE_DIR}
   ${${DETECTED_OS_VERS}-ROOT}
 )
