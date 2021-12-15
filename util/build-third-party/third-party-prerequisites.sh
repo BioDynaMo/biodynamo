0d1b720ceba3ae5dca7581508bb1ee5bb49bcebf
Tobias Duswald <tobias.duswald@cern.ch>
2021-12-15 20:12:21 +0100

    Update scripts for third party build procedure

--- util/build-third-party/third-party-prerequisites.sh
+++ util/build-third-party/third-party-prerequisites.sh
@@ -125,8 +125,13 @@ EOF'
   env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install 3.9.1
   pyenv shell 3.9.1
 
+# MacOS
 else
   xcode-select --install || true
   brew update --preinstall
   brew install bash git cmake ninja swig python@3.9 libomp open-mpi git-lfs
+  # On ARM based macOS systems we used the brew version of qt5
+  if [ "$(uname -p)" = 'arm' ]; then
+    brew install qt@5
+  fi
 fi
