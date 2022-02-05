33ec493e7f4763e9acc709cbbfe950a8576ae890
mhoghrab <88381107+mhoghrab@users.noreply.github.com>
2022-02-05 12:34:23 +0100

    copyright update

--- demo/flocking/src/sim_param.h
+++ demo/flocking/src/sim_param.h
@@ -1,14 +1,7 @@
 // -----------------------------------------------------------------------------
 //
-// Copyright (C) 2021 CERN & University of Surrey for the benefit of the
-// BioDynaMo collaboration. All Rights Reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-//
-// See the LICENSE file distributed with this work for details.
-// See the NOTICE file distributed with this work for additional information
-// regarding copyright ownership.
+// Copyright (C) Moritz Grabmann.
+// All Rights Reserved.
 //
 // -----------------------------------------------------------------------------
 
