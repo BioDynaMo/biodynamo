2bb6200146dd1beed23ae221c9cbab36def964d4
Lukas Johannes Breitwieser <lukas.johannes.breitwieser@cern.ch>
2022-03-18 14:09:30 +0100

    Fix epidemiology demo

--- demo/epidemiology/src/analytical-solution.cc
+++ demo/epidemiology/src/analytical-solution.cc
@@ -12,14 +12,14 @@
 
 namespace bdm {
 
-int Odes(real_t t, const real_t y[], real_t f[], void* params) {
-  auto* dparams = static_cast<real_t*>(params);
-  real_t beta = dparams[0];
-  real_t gamma = dparams[1];
-  real_t n = dparams[2];
+int Odes(double t, const double y[], double f[], void* params) {
+  auto* dparams = static_cast<double*>(params);
+  double beta = dparams[0];
+  double gamma = dparams[1];
+  double n = dparams[2];
 
-  real_t s = y[0];
-  real_t i = y[1];
+  double s = y[0];
+  double i = y[1];
 
   // dsdt
   f[0] = -beta * i * s / n;
@@ -33,21 +33,22 @@ int Odes(real_t t, const real_t y[], real_t f[], void* params) {
 void CalculateAnalyticalSolution(TimeSeries* result, real_t beta, real_t gamma,
                                  real_t susceptible, real_t infected,
                                  real_t tstart, real_t tend, real_t step_size) {
-  real_t n = susceptible + infected;
-  real_t params[3] = {beta, gamma, n};
+  double n = susceptible + infected;
+  double params[3] = {beta, gamma, n};
   gsl_odeiv2_system sys = {Odes, nullptr, 3, params};
 
   gsl_odeiv2_driver* d =
       gsl_odeiv2_driver_alloc_y_new(&sys, gsl_odeiv2_step_rk2, 1e-6, 1e-6, 0.0);
-  real_t y[3] = {susceptible, infected, 0.0};
+  double y[3] = {susceptible, infected, 0.0};
 
   std::vector<real_t> timevec;
   std::vector<real_t> susceptiblevec;
   std::vector<real_t> infectedvec;
   std::vector<real_t> recoveredvec;
 
-  for (real_t t = tstart; t < tend; t += step_size) {
-    int status = gsl_odeiv2_driver_apply(d, &tstart, t, y);
+  double tstart_d = static_cast<double>(tstart);
+  for (double t = tstart_d; t < tend; t += step_size) {
+    int status = gsl_odeiv2_driver_apply(d, &tstart_d, t, y);
 
     if (status != GSL_SUCCESS) {
       printf("error, return value=%d\n", status);
