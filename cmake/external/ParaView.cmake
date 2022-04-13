5b4ad33b0356b0cc4d8732f734233fcb448bdbce
Tobias Duswald <tobias.duswald@cern.ch>
2022-04-13 18:24:33 +0200

    Rename PV build files

--- cmake/external/ParaView.cmake
+++ cmake/external/ParaView.cmake
@@ -2,8 +2,12 @@ include(utils)
 
 SET(PARAVIEW_SOURCE_DIR "${CMAKE_THIRD_PARTY_DIR}/paraview")
 
-if(APPLE)
-  SET(PARAVIEW_TAR_FILE paraview_v5.10.1_${DETECTED_OS_VERS}_default.tar.gz)
+if(APPLE AND "${DETECTED_ARCH}" STREQUAL "i386")
+  # The release of cmake 3.23.0 broke our build of ParaView on MacOSX. 
+  # The build was fixed with a reupload and carries the additional tag cm233.
+  SET(PARAVIEW_TAR_FILE paraview_v5.10.0_cm323_${DETECTED_OS_VERS}_default.tar.gz)
+elseif(APPLE AND "${DETECTED_ARCH}" STREQUAL "arm64")
+  SET(PARAVIEW_TAR_FILE paraview_v5.10.0_${DETECTED_OS_VERS}_default.tar.gz)
 else()
   SET(PARAVIEW_TAR_FILE paraview_v5.9.0_${DETECTED_OS_VERS}_default.tar.gz)
 endif(APPLE)
