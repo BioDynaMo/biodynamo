b3fc69c910c9ae08bef0c6d1c6c99d717d077141
Lukas Johannes Breitwieser <lukas.johannes.breitwieser@cern.ch>
2022-04-20 11:18:34 +0200

    Format

--- src/core/agent/agent.h
+++ src/core/agent/agent.h
@@ -211,7 +211,8 @@ class Agent {
     if (gAgentPointerMode == AgentPointerMode::kIndirect) {
       return AgentPointer<TAgent>(uid_);
     } else {
-      return AgentPointer<TAgent>(const_cast<TAgent*>(Cast<const Agent, const TAgent>(this)));
+      return AgentPointer<TAgent>(
+          const_cast<TAgent*>(Cast<const Agent, const TAgent>(this)));
     }
   }
 
@@ -272,7 +273,7 @@ class Agent {
   uint32_t box_idx_ = std::numeric_limits<uint32_t>::max();
   /// collection of behaviors which define the internal behavior
   InlineVector<Behavior*, 2> behaviors_;
-  
+
   template <typename TFrom, typename TTo>
   typename std::enable_if<std::is_base_of<TFrom, TTo>::value, TTo*>::type Cast(
       TFrom* agent) const {
