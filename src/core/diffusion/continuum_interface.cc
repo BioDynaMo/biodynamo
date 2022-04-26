bc015cba31dfc48fee5fcb0ebc5f1362eb1b8edd
Tobias Duswald <tobias.duswald@cern.ch>
2022-04-26 16:18:01 +0200

    Add documentation

--- src/core/diffusion/continuum_interface.cc
+++ src/core/diffusion/continuum_interface.cc
@@ -18,17 +18,28 @@ namespace bdm {
 
 void Continuum::IntegrateTimeAsynchronously(double dt) {
   if (time_step_ != std::numeric_limits<double>::max()) {
+    // Uptdate the total time to simulate
     time_to_simulate_ += dt;
+    // Compute the number of time steps to simulate
     auto n_steps = static_cast<int>(std::floor(time_to_simulate_ / time_step_));
+    // Simulate for the appropriate number of time steps
     for (int i = 0; i < n_steps; i++) {
       Step(time_step_);
     }
+    // Update the total simulated time
     simulated_time_ += n_steps * time_step_;
+    // Keep track of time that has not been simulated yet
     time_to_simulate_ -= n_steps * time_step_;
   } else {
+    // If time_step_ is not set, we simply forward the time step to the Step
+    // method.
     Step(dt);
     simulated_time_ += dt;
   }
 }
+
+void Continuum::SetTimeStep(double dt) { time_step_ = dt; }
+
+double Continuum::GetTimeStep() const { return time_step_; }
 
 }  // namespace bdm
--- src/core/diffusion/continuum_interface.h
+++ src/core/diffusion/continuum_interface.h
@@ -22,56 +22,139 @@
 
 namespace bdm {
 
+/// @brief Continuum class to interface with BioDynaMo for hybrid simulations.
+///
+/// This class is used to interface arbitrary Continuum implementations with
+/// BioDynaMo. The continuum may be a single- or multi-threaded, or possibly
+/// even a GPU implementation. Furthermore, it can be of arbitrary
+/// dimension. We provide more specific interfaces for scalar and vector fields
+/// (see `ScalarField` and `VectorField`). The interface is pure virtual, thus,
+/// the user must implement the methods (`Initialize`, `Update`, `Step`) in the
+/// derived class. The `DiffusionGrid` class is an example of such a derived
+/// class implementing a continuum (scalar field) via a finite difference
+/// discretization for the diffusion equation. Another example is given in the
+/// demo `analytic_continuum` which attaches an analytic scalar field to the
+/// simulation.
+///
+/// The class is included into the BioDynaMo simulation scheme through the
+/// `ContinuumOp` class. This class is a stand-alone operation, i.e., it is
+/// called once per simulation step (if its frequency is 1). This call to the
+/// `ContinuumOp::operator()` method triggers the execution of the
+/// `IntegrateTimeAsynchronously` method, which in turn calls the `Step` method
+/// with appropriate time step(s). Note that there are two options to influence
+/// the synchronisation of the simulation: the frequency of the `ContinuumOp`
+/// and the time step of the `Continuum`. The former defines the synchronization
+/// frequency between ABM and the continuum. The latter defines the time step of
+/// the continuum.
 class Continuum {
  public:
   Continuum() = default;
   explicit Continuum(const TRootIOCtor *) {}
   virtual ~Continuum() = default;
 
+  /// Manages the time evolution of the continuum. The method is called by the
+  /// `ContinuumOp::operator()` method. The method calles the `Step` method with
+  /// the appropriate time step(s) as defined via the `time_step_` member. If
+  /// the time step is not set, `dt` is used.
   void IntegrateTimeAsynchronously(double dt);
 
+  /// Initializes the continuum. This method is called via
+  /// `Scheduler::Initialize`. For some implementations, this method may be
+  /// useful, other may not require it. A possibly use case is that agents move
+  /// in a stationary continuum that is the solution to a timeindependent PDE.
+  /// In this case, the `Initialize` method could be used to solve the PDE
+  /// once at the beginning of the simulation.
   virtual void Initialize() = 0;
+
+  /// Updates the continuum. Since this method is public, it can be called form
+  /// anyone and trigger an update of the continuum model. We envision that this
+  /// can be used for mesh refinements or for other purposes. The DiffusionGrid,
+  /// for instance, reconstructs the grid under certain conditions with this
+  /// member function.
   virtual void Update() = 0;
+
+  /// This method integrates the continuum model in time by `dt`. Typically, the
+  /// continuum is the solution of a partial differential equation (PDE). Thus,
+  /// the step method is used to advance the solution in time. If your numerical
+  /// scheme has requirements for it's parameters depending on `dt`, make sure
+  /// to verify them in this method.
   virtual void Step(double dt) = 0;
 
+  /// Returns the ID of the continuum.
   int GetContinuumId() const { return continuum_id_; }
+
+  /// Sets the ID of the continuum.
   void SetContinuumId(int id) { continuum_id_ = id; }
+
+  /// Returns the name of the continuum.
   const std::string &GetContinuumName() const { return continuum_name_; }
+
+  /// Sets the name of the continuum.
   void SetContinuumName(const std::string &name) { continuum_name_ = name; }
+
+  /// Returns the time simulated by the continuum.
   double GetSimulatedTime() const { return simulated_time_; }
-  void SetTimeStep(double dt) { time_step_ = dt; }
-  double GetTimeStep() const { return time_step_; }
+
+  /// Sets the time step for the continuum. WARNING: We recommend not to mix
+  /// the the different scheduling options for the continuum model, i.e. avoid
+  /// using both, the time step and the frequency of the diffusion operation,
+  /// at the same time.
+  void SetTimeStep(double dt);
+
+  /// Returns the time step for the continuum. If no time time step was set, it
+  /// returns std::numeric_limits<double>::max(). Note however, the continuum
+  /// uses the time step of the diffusion operation / simulation.
+  double GetTimeStep() const;
 
  private:
+  /// Name of the continuum.
   std::string continuum_name_ = "";
+
+  /// Time step of the continuum (may differ from the simulation time step).
   double time_step_ = std::numeric_limits<double>::max();
+
+  /// Passed simulation time for the continuum.
   double simulated_time_ = 0.0;
+
+  /// Time that the continuum (still) has to integrate.
   double time_to_simulate_ = 0.0;
+
+  /// Id of the continuum.
   int continuum_id_ = -1;
 
   BDM_CLASS_DEF(Continuum, 1);  // NOLINT
 };
 
+/// Interface for scalar fields. See `Continuum` for more information.
 class ScalarField : public Continuum {
  public:
   ScalarField() = default;
   explicit ScalarField(const TRootIOCtor *) {}
   ~ScalarField() override = default;
 
+  /// Returns the value of the scalar field at the given position.
   virtual double GetValue(const Double3 &position) const = 0;
+
+  /// Returns the gradient of the scalar field at the given position.
   virtual Double3 GetGradient(const Double3 &position) const = 0;
 
   BDM_CLASS_DEF_OVERRIDE(ScalarField, 1);  // NOLINT
 };
 
+/// Interface for vector fields. See `Continuum` for more information.
 class VectorField : public Continuum {
  public:
   VectorField() = default;
   explicit VectorField(const TRootIOCtor *) {}
   ~VectorField() override = default;
 
+  /// Returns the value of the vector field at the given position.
   virtual Double3 GetValue(const Double3 &position) const = 0;
+
+  /// Returns the divergence of the vector field at the given position.
   virtual double GetDiv(const Double3 &position) const = 0;
+
+  /// Returns the curl of the vector field at the given position.
   virtual double GetCurl(const Double3 &position) const = 0;
 
   BDM_CLASS_DEF_OVERRIDE(VectorField, 1);  // NOLINT
