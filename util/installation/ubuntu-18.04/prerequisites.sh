30f096a5bedcf83de12c3e75959529d27e0f956a
Tobias Duswald <tobias.duswald@cern.ch>
2022-09-16 14:56:47 +0200

    Attempt to fix notebooks on Ubuntu

--- util/installation/ubuntu-18.04/prerequisites.sh
+++ util/installation/ubuntu-18.04/prerequisites.sh
@@ -72,7 +72,7 @@ pyenv shell $PYVERS
 # Install optional packages
 if [ $1 == "all" ]; then
   # this updates pip, but installs the updated version in $HOME/.local/bin
-  PIP_PACKAGES="nbformat jupyter metakernel jupyterlab"
+  PIP_PACKAGES="jupyter metakernel jupyterlab nbformat==5.4.0 nbconvert==6.5.3 nbclient==0.6.6"
   # Don't install --user: the packages should end up in the PYENV_ROOT directory
   python -m pip install $PIP_PACKAGES
 
