3378d9ca85cfd94319da838ce791e1719e19c640
Jack <44051158+BJackal@users.noreply.github.com>
2022-09-28 23:30:58 +0100

    Update cycling_cell.h
    Added brief comments for what each Setter and Getter is for. Additionally, removed the Vrel setter and getter along with its data members as I believe this is an artefact from the cell cycle specific unit test.

--- demo/monolayer_growth/src/cycling_cell.h
+++ demo/monolayer_growth/src/cycling_cell.h
@@ -52,28 +52,33 @@ class CyclingCell : public Cell {  // our object extends the Cell object
   }
 
   // getter and setter for division
+  // A cell can either divide or not divide so is a simple bool.
   void SetCanDivide(bool d) { can_divide_ = d; }
   bool GetCanDivide() const { return can_divide_; }
 
   // getter and setter for cells current phase of cell cycle
+  // keeps track of cells current state
   void SetCycle(CellState c) { cycle_ = c; }
   CellState GetCycle() const { return cycle_; }
 
   // getter and setter for cells maximum volume
+  // in this case cells have a maximum volue they can reach before having to divide
+  // thus we set it here.
   void SetVmax(real_t vm) { vmax_ = vm; }
   real_t GetVmax() const { return vmax_; }
 
   // getter and setter for cells initial volume
+  // cells initial volume needs to be handle during simulation so is simply stored
+  // here and kept within the agents themselves. Especially important if one wishes
+  // to have multiple agents which divide differently/ different initial condtions.
   void SetVinit(real_t vi) { vinit_ = vi; }
   real_t GetVinit() const { return vinit_; }
 
   // getter and setter for delt t
+  // Delt is used for comparing against a cells maximum amount of time it can be within
+  // a given state and needs to be kept track of throughout the whole simulation.
   void SetDelt(real_t dt) { delt_ = dt; }
   real_t GetDelt() const { return delt_; }
-
-  // getter and setter for relative cell volume
-  void SetVrel(real_t vr) { vrel_ = vr; }
-  real_t GetVrel() const { return vrel_; }
 
  private:
   bool can_divide_;
@@ -81,7 +86,6 @@ class CyclingCell : public Cell {  // our object extends the Cell object
   real_t vmax_;
   real_t vinit_;
   real_t delt_;
-  real_t vrel_;
 };
 
 }  // namespace bdm
