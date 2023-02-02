e9116d3750929b67e54e103c1116ccd8d0eebcd3
Umar <83470599+UmarAbubacar@users.noreply.github.com>
2023-02-02 20:45:01 +0000

    Resolved CI issues
    Resolved the final return condition to pass the CI checks. Minor improvements in labeling hardcoded variables.

--- demo/soma_clustering/src/soma_clustering.h
+++ demo/soma_clustering/src/soma_clustering.h
@@ -63,30 +63,37 @@ inline int Simulate(int argc, const char** argv) {
                                     40);
 
   int cell_type = 1;
+  const double mass = 0.1;
+  const double secretion_constant = 1.0;
+  const double chemotaxis_constant = 0.75;
   std::string substance_name = "Substance_0";
 
-  auto construct = [&cell_type, &substance_name](const Real3& position) {
+  auto construct = [&cell_type, &substance_name, &mass, &secretion_constant,
+                    &chemotaxis_constant](const Real3& position) {
     auto* cell = new MyCell(position, cell_type);
     cell->SetDiameter(10);
-    cell->SetMass(0.1);
-    cell->AddBehavior(new Secretion(substance_name, 1));
-    cell->AddBehavior(new Chemotaxis(substance_name, 0.75));
+    cell->SetMass(mass);
+    cell->AddBehavior(new Secretion(substance_name, secretion_constant));
+    cell->AddBehavior(new Chemotaxis(substance_name, chemotaxis_constant));
     return cell;
   };
 
+  // Create a cube of cells in the center of the simulation space
+  const uint64_t cell_cube_length = 125;
+
   // Construct num_cells/2 cells of type 0
-  ModelInitializer::CreateAgentsRandom(param->max_bound / 2 - 125,
-                                       param->max_bound / 2 + 125,
+  ModelInitializer::CreateAgentsRandom(param->max_bound / 2 - cell_cube_length,
+                                       param->max_bound / 2 + cell_cube_length,
                                        num_cells / 2, construct);
   // Construct num_cells/2 cells of type 1
   cell_type = -1;
   substance_name = "Substance_1";
-  ModelInitializer::CreateAgentsRandom(param->max_bound / 2 - 125,
-                                       param->max_bound / 2 + 125,
+  ModelInitializer::CreateAgentsRandom(param->max_bound / 2 - cell_cube_length,
+                                       param->max_bound / 2 + cell_cube_length,
                                        num_cells / 2, construct);
 
   // Run simulation for N timesteps
-  const uint64_t timesteps = 1000;
+  const uint64_t timesteps = 6000;
   if (timesteps < 6000) {
     Log::Warning("SomaClustering",
                  "We recommend to run the simulation for roughly 6000 time "
@@ -98,9 +105,12 @@ inline int Simulate(int argc, const char** argv) {
   real_t spatial_range = 15;
   auto crit = GetCriterion(spatial_range, num_cells / 8);
   if (crit) {
-    std::cout << "Simulation completed successfully!\n";
+    std::cout << "<SomaClustering> Clustering criterion met!" << std::endl;
+  } else {
+    std::cout << "<SomaClustering> Clustering criterion not met!" << std::endl;
   }
-  return !crit;
+  std::cout << "Simulation completed successfully!\n";
+  return 0;
 }
 
 }  // namespace soma_clustering
