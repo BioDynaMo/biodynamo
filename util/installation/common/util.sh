a4c92971854cb81a2f0ae8613fb8fb3b3d0b5cf5
johnpapad24 <92296237+johnpapad24@users.noreply.github.com>
2025-07-26 08:40:30 +0000

    Update util.sh
    added preliminary support for ubuntu 24.10,  25 and 26

--- util/installation/common/util.sh
+++ util/installation/common/util.sh
@@ -52,6 +52,9 @@ function DetectOs {
 		OS="ubuntu-24.04"
     elif echo "$DISTRIBUTOR" | grep -Eiq 'buntu' ; then
     		OS="ubuntu-${RELEASE}"
+        if echo "$OS" | grep -Eiq 'buntu-24|buntu-25|buntu-26'; then
+          OS="ubuntu-24.04"
+        fi
     fi
     echo $OS | awk '{print tolower($0)}'
   elif [ `uname` = "Darwin" ]; then
