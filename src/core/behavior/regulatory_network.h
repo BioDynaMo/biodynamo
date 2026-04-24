1f8fc7466c1b3fb7f70254b5b4cb343d103b458d
sportokalidis <sportokalidis98@gmail.com>
2026-04-24 12:06:17 +0200

    Add RegulatoryNetwork behavior stub and conditional include
    Add an empty RegulatoryNetwork behavior class (stub) that subclasses
    Behavior. The header is included in biodynamo.h only when USE_BOOST is
    defined. Implementation of Run() is left for follow-up work.

---
+++ src/core/behavior/regulatory_network.h
@@ -0,0 +1,38 @@
+// -----------------------------------------------------------------------------
+//
+// Copyright (C) 2021 CERN & University of Surrey for the benefit of the
+// BioDynaMo collaboration. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// See the LICENSE file distributed with this work for details.
+// See the NOTICE file distributed with this work for additional information
+// regarding copyright ownership.
+//
+// -----------------------------------------------------------------------------
+
+#ifndef CORE_BEHAVIOR_REGULATORY_NETWORK_H_
+#define CORE_BEHAVIOR_REGULATORY_NETWORK_H_
+
+#include "core/behavior/behavior.h"
+
+#include "boost/numeric/odeint.hpp"
+#include "boost/phoenix/core.hpp"
+#include "boost/phoenix/operator.hpp"
+
+namespace bdm {
+class RegulatoryNetwork : public Behavior {
+  BDM_BEHAVIOR_HEADER(RegulatoryNetwork, Behavior, 1);
+
+ public:
+  RegulatoryNetwork() { AlwaysCopyToNew(); }
+  virtual ~RegulatoryNetwork() = default;
+
+  void Run(Agent* agent) override {};
+
+};
+}
+
+#endif  // CORE_BEHAVIOR_REGULATORY_NETWORK_H_
+
