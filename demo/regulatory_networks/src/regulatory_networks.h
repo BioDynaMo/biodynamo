c4c275123c6e4e58925ac3f2b2209229ac133e1b
Vasileios Vavourakis <vasvav@gmail.com>
2026-04-26 21:00:26 +0300

    Created a blank regulatory networks demo

---
+++ demo/regulatory_networks/src/regulatory_networks.h
@@ -0,0 +1,33 @@
+// -----------------------------------------------------------------------------
+//
+// Copyright (C) 2021 CERN & University of Surrey for the benefit of the
+// BioDynaMo collaboration. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+// See the LICENSE file distributed with this work for details.
+// See the NOTICE file distributed with this work for additional information
+// regarding copyright ownership.
+//
+// -----------------------------------------------------------------------------
+
+#ifndef DEMO_REGULATORY_NETWORKS_H_
+#define DEMO_REGULATORY_NETWORKS_H_
+
+#include <string>
+#include <vector>
+
+#include "biodynamo.h"
+
+namespace bdm {
+
+inline int Simulate(int argc, const char** argv) {
+
+  std::cout << "Simulation completed successfully!\n";
+  return 0;
+}
+
+}  // namespace bdm
+
+#endif  // DEMO_REGULATORY_NETWORKS_H_
